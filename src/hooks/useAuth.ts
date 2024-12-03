950af762a1b8e7bccd0a675d4b5269cbad5eb8de
Crisops <crialeperez1835@gmail.com>
2024-12-03 12:33:02 -0500

    Rename file name (useLogin -> useAuth)

---
+++ src/hooks/useAuth.ts
@@ -0,0 +1,14 @@
+import { create } from 'zustand'
+
+
+
+export const useLogin = create<Store>()((set) => ({
+  initialForm: { email: "", password: ""},
+  setInitialForm: (initialForm) => set(() => ({ initialForm })),
+}))
+
+export const useCreateNewAccount = create<StoreNewAccountForm>((set) => ({
+  initialForm: {fullName: "", username:"", email: "", password:"", birthday: { day: "", month: "", year: ""} },
+  setInitialForm: (initialForm) => set(() => ({ initialForm })),
+}))
+
