b957fcf020f89cf64616c15d8477d025b1468c0c
Crisops <crialeperez1835@gmail.com>
2024-12-03 16:29:06 -0500

    Add handler when submitting form and change button location

--- src/components/Login/FormLogin.tsx
+++ src/components/Login/FormLogin.tsx
@@ -10,7 +10,7 @@ export const FormLogin = () => {
     const idEmail = useId()
     const idPassword = useId()
 
-    const {registerField, watch, errors} = useFormAuthLogin()
+    const {registerField, handleSubmit, watch, errors} = useFormAuthLogin()
 
     const password = watch("password")
 
@@ -32,42 +32,48 @@ export const FormLogin = () => {
         setView(!view)
     }
 
+    const handleOnSubmit = handleSubmit(() => {
+        console.log(initialForm);
+        
+    })
+
+
     return (
       <div className="flex items-center flex-col w-full h-[calc(100%-4rem)]">
           <div className="flex flex-col max-w-xl w-full h-full">
               <div className="my-7">
                   <h1 className="text-4xl text-white font-semibold">Introduce tu contraseña</h1>
               </div>
-              <form className="flex flex-col w-full h-full">
-                <InputForm
-                    id={idEmail}
-                    name="email"
-                    label="Correo electrónico"
-                    defaultValue={initialForm.email?.toLocaleLowerCase()}
-                    type="email"
-                    valueInput=""
-                    disabled
-                />
-                <InputForm
-                    id={idPassword}
-                    type={view ? 'text' : 'password'}
-                    valueInput={password}
-                    label="Contraseña"
-                    handleInputRegister={{...registerField("password")}}
-                    errorMessage={errors.password?.message}
-                    IconSvg={view ? <IconEyeOff color="white" size={35}/>  : <IconEye color="white" size={35}/>}
-                    handleViewPassword={handleViewPassword}
-                    autoComplete="on"
-                />
+              <form onSubmit={handleOnSubmit} className="flex flex-col w-full h-full">
+                    <InputForm
+                        id={idEmail}
+                        name="email"
+                        label="Correo electrónico"
+                        defaultValue={initialForm.email?.toLocaleLowerCase()}
+                        type="email"
+                        valueInput=""
+                        disabled
+                    />
+                    <InputForm
+                        id={idPassword}
+                        type={view ? 'text' : 'password'}
+                        valueInput={password}
+                        label="Contraseña"
+                        handleInputRegister={{...registerField("password")}}
+                        errorMessage={errors.password?.message}
+                        IconSvg={view ? <IconEyeOff color="white" size={35}/>  : <IconEye color="white" size={35}/>}
+                        handleViewPassword={handleViewPassword}
+                        autoComplete="on"
+                    />
+                    <div className="flex flex-col mb-10 h-full justify-end">
+                        <div>
+                            <button type="submit" className={`text-black text-xl w-full rounded-full font-bold bg-white p-4 hover:bg-white/90 ${errors.password?.message || initialForm.password === '' ? 'cursor-not-allowed' : 'cursor-pointer'}`}>Iniciar sesión</button>
+                        </div>
+                        <div className="mt-5">
+                            <p className="text-lg text-zinc-500">¿No tienes una cuenta? <Link href={'/signup'} className="text-sky-500 hover:underline">Regístrate</Link></p>
+                        </div>
+                    </div>
                 </form>
-                <div className="mb-10">
-                    <div>
-                        <button type="submit" className={`text-black text-xl w-full rounded-full font-bold bg-white p-4 hover:bg-white/90 ${errors.password?.message || initialForm.password === '' ? 'cursor-not-allowed' : 'cursor-pointer'}`}>Iniciar sesión</button>
-                    </div>
-                    <div className="mt-5">
-                        <p className="text-lg text-zinc-500">¿No tienes una cuenta? <Link href={'/signup'} className="text-sky-500 hover:underline">Regístrate</Link></p>
-                    </div>
-                </div>
           </div>
       </div>
     )
