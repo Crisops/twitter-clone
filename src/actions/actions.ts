3ecc860063470660eb366f8b058ce4b916da685b
Crisops <crialeperez1835@gmail.com>
2024-12-04 18:22:41 -0500

    Create file action to sing up with google

---
+++ src/actions/actions.ts
@@ -0,0 +1,46 @@
+'use server'
+
+import { revalidatePath } from 'next/cache'
+import { redirect } from 'next/navigation'
+
+import { createClient } from '@/utils/supabase/server'
+
+// export async function login(formData: FormData) {
+//   const supabase = await createClient()
+
+//   // type-casting here for convenience
+//   // in practice, you should validate your inputs
+//   const data = {
+//     email: formData.get('email') as string,
+//     password: formData.get('password') as string,
+//   }
+
+//   const { error } = await supabase.auth.signInWithPassword(data)
+
+//   if (error) {
+//     redirect('/error')
+//   }
+
+//   revalidatePath('/', 'layout')
+//   redirect('/')
+// }
+
+export async function signup() {
+  const supabase = await createClient()
+
+  const { data, error } = await supabase.auth.signInWithOAuth({
+    provider: 'google',
+    options: {
+      redirectTo: 'http://localhost:3000/auth/callback'
+    },
+  })
+
+  if (error) {
+    redirect('/error')
+  }
+
+  if (data.url) {
+    revalidatePath('/home')
+    redirect(data.url) // use the redirect API for your server framework
+  }
+}
