d8eddf7365f8958e3e871bedc332a033ebec9556
Crisops <crialeperez1835@gmail.com>
2024-12-04 18:28:13 -0500

    Add sign up function to google button

--- src/components/shared/ButtonGoogle.tsx
+++ src/components/shared/ButtonGoogle.tsx
@@ -1,10 +1,18 @@
+'use client'
+
+import { signup } from "@/actions/actions"
 import { IconGoogle } from "../Icons"
 
 export const ButtonGoogle = ({textContent}: {textContent: string}) => {
+
+    const handleSignUpGoogleAuth = async () => {
+        await signup()
+    }
+
     return (
         <div className="relative bg-white flex items-center justify-center rounded-full gap-4">
             <IconGoogle/>
-            <button className="text-lg text-zinc-700 py-3 pr-3 font-medium">{textContent}</button>
+            <button onClick={handleSignUpGoogleAuth} className="text-lg text-zinc-700 py-3 pr-3 font-medium">{textContent}</button>
         </div>
     )
 }
