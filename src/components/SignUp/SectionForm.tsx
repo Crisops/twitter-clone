5c88b25beb708a40a91e8b43d300449d878abe1b
Crisops <crialeperez1835@gmail.com>
2024-12-04 18:45:48 -0500

    Rename Component

--- src/components/SignUp/SectionForm.tsx
+++ src/components/SignUp/SectionForm.tsx
@@ -1,9 +1,9 @@
-import FormSingUp from "./FormSingUp"
+import FormSignUp from "./FormSignUp"
 
 export const SectionForm = () => {
     return (
         <section className="w-screen h-screen md:w-[750px] md:h-[800px] bg-black md:rounded-3xl">
-            <FormSingUp/>
+            <FormSignUp/>
         </section>
     )
 }
