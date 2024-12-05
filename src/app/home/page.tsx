11941a5d042d84002f8fff4349d24bd656943e95
Crisops <crialeperez1835@gmail.com>
2024-12-05 12:56:04 -0500

    Layout page home

--- src/app/home/page.tsx
+++ src/app/home/page.tsx
@@ -1,6 +1,22 @@
 
 export default function HomePage() {
   return (
-    <div>HomePage</div>
+    <section className="relative w-screen">
+      <div className="mx-auto max-w-[100rem]">
+          <section className="grid grid-cols-[350px_1fr_500px]">
+              <aside className="sticky top-0 h-screen border-r-1 border-zinc-900">
+                <div className="">
+                  1
+                </div>
+              </aside>
+            <div className="h-[200vh]">2</div>
+            <aside className="sticky top-0 h-screen border-l-1 border-zinc-900">
+              <div className="">
+                3
+              </div>
+            </aside>
+          </section>
+      </div>
+    </section>
   )
 }
