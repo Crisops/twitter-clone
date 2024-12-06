0ccf0980a63fcadf11db1d9fb7e719e697675ee5
Crisops <crialeperez1835@gmail.com>
2024-12-05 21:25:46 -0500

    Add config images remotes example

--- next.config.mjs
+++ next.config.mjs
@@ -1,7 +1,15 @@
-// import type { NextConfig } from 'next'
+//import type { NextConfig } from 'next'
 
 const nextConfig = {
-  /* config options here */
+  images:{
+    remotePatterns: [
+      {
+        protocol: "https",
+        hostname: 'pbs.twimg.com',
+
+      }
+    ]
+  }
 }
 
 export default nextConfig
