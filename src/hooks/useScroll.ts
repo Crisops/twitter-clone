cf203156f4950cb1d2485d3bed2cbada019e1757
Crisops <crialeperez1835@gmail.com>
2024-12-08 17:24:02 -0500

    Create custom hook to handle scroll viewport

---
+++ src/hooks/useScroll.ts
@@ -0,0 +1,23 @@
+import { useEffect, useState } from "react"
+
+export const useScroll = () => {
+
+    const [scroll, setScroll] = useState<number>(0)
+
+
+    useEffect(() => {
+      const handleScrollViewPort = () => {
+        setScroll(window.scrollY)
+      }
+    
+
+      window.addEventListener("scroll", handleScrollViewPort)
+
+      return () => {
+        window.removeEventListener("scroll", handleScrollViewPort)
+      }
+    }, [scroll])
+    
+
+    return {scroll}
+}
