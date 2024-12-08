c0a41b1f0410bfb5035535a712d17977d04fe0c7
Crisops <crialeperez1835@gmail.com>
2024-12-08 14:04:23 -0500

    Styles and size icon and image

--- src/components/Home/AsideBarMenu.tsx
+++ src/components/Home/AsideBarMenu.tsx
@@ -6,33 +6,33 @@ import AsideNavigation from "./AsideNavigation";
 export default function AsideBarMenu() {
   return (
     <aside className="sticky top-0 h-screen border-r-1 border-zinc-900">
-        <div className="h-full px-2">
+        <div className="h-full px-1">
             <div className="flex flex-col justify-between h-full">
                 <div className="flex flex-col w-full h-full">
                     <header className="py-4 pl-4">
-                        <IconTwitter size="size-[2.5rem]"/>
+                        <IconTwitter size="size-[2rem]"/>
                     </header>
                     <div className="h-full">
-                        <div className="pl-2 h-full">
+                        <div className="pl-1 h-full">
                             <AsideNavigation/>
                         </div>
                     </div>
                 </div>
-                <div className="py-5 pl-2">
-                    <div className="flex items-center justify-between p-3 rounded-full cursor-pointer transition-colors duration-150 hover:bg-white/10">
+                <div className="py-3 pl-1">
+                    <div className="flex items-center justify-between pl-2 py-2 pr-2 rounded-full cursor-pointer transition-colors duration-150 hover:bg-white/10">
                         <div className="flex items-center">
                             <div>
-                                <Image className="rounded-full" src="https://pbs.twimg.com/profile_images/1841249485810245632/0VcVbOE8_normal.jpg" width={50} height={50} alt="Nombre de usuario"/>
+                                <Image className="rounded-full" src="https://pbs.twimg.com/profile_images/1841249485810245632/0VcVbOE8_normal.jpg" width={40} height={40} alt="Nombre de usuario"/>
                             </div>
-                            <div className="ml-4">
+                            <div className="ml-3">
                                 <div className="flex flex-col justify-between">
-                                    <h4 className="text-white font-medium text-xl">Alejandro Pérez</h4>
-                                    <span className="text-zinc-500 text-xl font-normal">@CrisopsDev</span>
+                                    <h4 className="text-white font-medium text-base">Alejandro Pérez</h4>
+                                    <span className="text-zinc-500 text-base font-normal">@CrisopsDev</span>
                                 </div>
                             </div>
                         </div>
                         <div>
-                            <IconDots size={25} color="white"/>
+                            <IconDots size={20} color="white"/>
                         </div>
                     </div>
                 </div>
