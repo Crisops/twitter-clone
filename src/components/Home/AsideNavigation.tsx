25fd9e3d7f17aac87680e0ff9148a3bcef9504b1
Crisops <crialeperez1835@gmail.com>
2024-12-08 14:04:51 -0500

    Syle gap

--- src/components/Home/AsideNavigation.tsx
+++ src/components/Home/AsideNavigation.tsx
@@ -5,7 +5,7 @@ export default function AsideNavigation() {
   return (
     <>
         <nav className="flex">
-            <ul className="flex flex-col justify-start w-full gap-5">
+            <ul className="flex flex-col justify-start w-full gap-1">
                 <LinksSideNav/>
             </ul>
         </nav>
