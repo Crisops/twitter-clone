e8825793d52806f70ee737470e398c376214f725
Crisops <crialeperez1835@gmail.com>
2024-12-08 17:26:13 -0500

    Create Component ChooseTwitts and import custom hook scroll

---
+++ src/components/Home/ChooseViewTwitts.tsx
@@ -0,0 +1,20 @@
+'use client'
+
+import { useScroll } from "@/hooks/useScroll"
+
+export default function ChooseViewTwitts() {
+
+    const { scroll } = useScroll()
+
+
+  return (
+    <div className={`flex justify-between items-center h-full [&>div]:flex [&>div]:justify-center [&>div]:items-center transition-all duration-200 ${scroll > 0 ? 'backdrop-blur-md bg-black/60' : 'backdrop-blur-none'}`}>
+        <div className="flex-grow h-full">
+            <button className={`w-full h-full text-white text-base font-bold transition-colors duration-200 hover:bg-white/10 `}>Para ti</button>
+        </div>
+        <div className="flex-grow h-full">
+            <button className="w-full h-full text-zinc-500 text-base font-medium transition-colors duration-200 hover:bg-white/10">Siguiendo</button>
+        </div>
+    </div>
+  )
+}
