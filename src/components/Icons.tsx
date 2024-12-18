96db4a8c864f7794eec40b6f813144ec0da3b792
Crisops <crialeperez1835@gmail.com>
2024-12-18 18:18:23 -0500

    style(icons): Change size icon google

--- src/components/Icons.tsx
+++ src/components/Icons.tsx
@@ -11,7 +11,7 @@ export const IconTwitter = ({size}: {size: string}) => {
 
 export const IconGoogle = () => {
     return (
-        <svg className="size-6" version="1.1" viewBox="0 0 48 48">
+        <svg className="size-5" version="1.1" viewBox="0 0 48 48">
             <g>
                 <path fill="#EA4335" d="M24 9.5c3.54 0 6.71 1.22 9.21 3.6l6.85-6.85C35.9 2.38 30.47 0 24 0 14.62 0 6.51 5.38 2.56 13.22l7.98 6.19C12.43 13.72 17.74 9.5 24 9.5z">
                 </path>
