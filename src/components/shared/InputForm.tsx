c1b1a538f27c7260bc611ecfeef46b1d5a378b05
Crisops <crialeperez1835@gmail.com>
2024-12-18 18:53:39 -0500

    style(input-form): change font size and height

--- src/components/shared/InputForm.tsx
+++ src/components/shared/InputForm.tsx
@@ -16,12 +16,12 @@ export function InputForm ({id, label, errorMessage, valueInput, handleInputRegi
     const value = valueInput ?? ''
 
     const HandlErrorAndCaracteresSpan = !errorMessage && value?.length > 0
-     ? 'text-sky-500 top-0 left-2 text-base' 
+     ? 'text-sky-500 top-0 left-2 text-sm' 
      : (errorMessage && value?.length > 0) 
-     ? 'text-red-500 top-0 left-2 text-base group-focus-within:text-base' 
+     ? 'text-red-500 top-0 left-2 text-sm group-focus-within:text-sm' 
      : (errorMessage && value?.length === 0) 
-     ? 'top-1/2 left-2 text-red-500 group-focus-within:top-0 group-focus-within:left-2 group-focus-within:text-base text-xl' 
-     : 'text-zinc-500 top-1/2 left-2 text-xl group-focus-within:text-sky-500 group-focus-within:top-0 group-focus-within:left-2 group-focus-within:text-base' 
+     ? 'top-1/2 left-2 text-red-500 group-focus-within:top-0 group-focus-within:left-2 group-focus-within:text-sm text-base' 
+     : 'text-zinc-500 top-1/2 left-2 text-base group-focus-within:text-sky-500 group-focus-within:top-0 group-focus-within:left-2 group-focus-within:text-sm' 
 
     const HandlErrorAndCaracteresInput = errorMessage && value?.length > 0 
     ? 'ring-red-500 focus:ring-red-500' 
@@ -36,11 +36,11 @@ export function InputForm ({id, label, errorMessage, valueInput, handleInputRegi
 
     return (
         <div className="relative first-of-type:mb-9 mb-9">
-            <div className="relative w-full h-16 group">
+            <div className="relative w-full h-14 group">
                 <label htmlFor={id} className="absolute w-full h-full left-0">
                     <input 
                     id={id}
-                    className={`absolute ${HandlErrorAndCaracteresInput} top-0  left-0 w-full h-full text-white text-xl bg-transparent ring-1 outline-none rounded-sm focus:border-none ${disabledActiveInput} ${IconSvg ? 'pl-3 pr-14' : 'px-3'}`}
+                    className={`absolute ${HandlErrorAndCaracteresInput} top-0  left-0 w-full h-full text-white text-base bg-transparent ring-1 outline-none rounded-sm focus:border-none ${disabledActiveInput} ${IconSvg ? 'pl-3 pr-14' : 'px-3'}`}
                     {...handleInputRegister}
                     {...rest}
                     />
