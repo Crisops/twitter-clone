f4e3998fa8a99d7d98fec3505ff806c7884c8632
Crisops <crialeperez1835@gmail.com>
2024-12-18 18:40:26 -0500

    style(media-query): change width and height

--- src/components/Login/SectionForm.tsx
+++ src/components/Login/SectionForm.tsx
@@ -2,7 +2,7 @@ import { Form } from "./Form"
 
 export const SectionForm = () => {
     return (
-        <section className="w-screen h-screen md:w-[750px] md:h-[800px] bg-black md:rounded-3xl">
+        <section className="w-screen h-screen md:max-w-[600px] md:h-[660px] bg-black md:rounded-3xl">
             <Form/>
         </section>
     )
--- src/components/SignUp/SectionForm.tsx
+++ src/components/SignUp/SectionForm.tsx
@@ -2,7 +2,7 @@ import FormSignUp from "./FormSignUp"
 
 export const SectionForm = () => {
     return (
-        <section className="w-screen h-screen md:w-[750px] md:h-[800px] bg-black md:rounded-3xl">
+        <section className="w-screen h-screen md:max-w-[600px] md:h-[660px] bg-black md:rounded-3xl">
             <FormSignUp/>
         </section>
     )
