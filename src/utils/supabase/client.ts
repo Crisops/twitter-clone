98b110ad5bb563d04a6cf7158adafc2ebf43ebb7
Crisops <crialeperez1835@gmail.com>
2024-12-18 18:12:53 -0500

    fix(client): remove config enviroment variables
    Access the enviroment variables with the object of process.env

--- src/utils/supabase/client.ts
+++ src/utils/supabase/client.ts
@@ -1,10 +1,8 @@
 import { createBrowserClient } from '@supabase/ssr'
-import { EnvConfig } from '@/config/env.config'
-
 
 export function createClient() {
   return createBrowserClient(
-    EnvConfig().SUPABASE_URL!,
-    EnvConfig().SUPABASE_ANON_KEY!
+    process.env.NEXT_PUBLIC_SUPABASE_URL!,
+    process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY!
   )
 }
