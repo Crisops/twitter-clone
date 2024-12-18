633e61470cf3033ee4c918d2c40fb1169904ad83
Crisops <crialeperez1835@gmail.com>
2024-12-18 18:47:33 -0500

    style(home-layout): resize icon svg

--- src/components/shared/HomeLayout.tsx
+++ src/components/shared/HomeLayout.tsx
@@ -4,7 +4,7 @@ import SectionForm from '@/components/ChooseAccount/SectionForm'
 export const HomeLayout = ({children}: {children?: React.ReactNode}) => {
     return (
         <section className="h-full grid place-items-center grid-cols-2 pl-20">
-            <IconTwitter size="size-[30rem]"/>
+            <IconTwitter size="size-[23rem]"/>
             <SectionForm/>
             {children}
         </section>
