d44ef0b9bf03c86d2644ab47018237f8b2cfbd43
Crisops <crialeperez1835@gmail.com>
2024-12-18 18:22:07 -0500

    style(ChooseAccount/Form): Change form width and font size in tag h1

--- src/components/ChooseAccount/Form.tsx
+++ src/components/ChooseAccount/Form.tsx
@@ -3,8 +3,8 @@ import { ButtonGoogle } from "../shared/ButtonGoogle"
 const Form = () => {
 
   return (
-    <form className="max-w-[370px] flex flex-col gap-10">
-        <h1 className="text-slate-100 font-extrabold text-4xl">Únete Hoy</h1>
+    <form className="max-w-72 flex flex-col gap-10">
+        <h1 className="text-slate-100 font-extrabold text-3xl">Únete Hoy</h1>
         <div className="relative w-full">
             <div className="flex flex-col gap-4">
               <ButtonGoogle textContent="Regístrarse con Google"/>
