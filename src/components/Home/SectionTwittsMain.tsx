f72ec33c24ddc1ab2eaba1a6d47c4b1d8ed84e86
Crisops <crialeperez1835@gmail.com>
2024-12-23 18:18:59 -0500

    style: remove position in main component

--- src/components/Home/SectionTwittsMain.tsx
+++ src/components/Home/SectionTwittsMain.tsx
@@ -5,9 +5,7 @@ import WriteTweet from "./WriteTweet";
 export default function SectionTwittsMain() {
   return (
     <section className="h-[200vh]">
-        <div className="sticky top-0 w-full h-14 border-b border-zinc-900 z-10">
-            <ChooseViewTwitts/>
-        </div>
+        <ChooseViewTwitts/>
         <WriteTweet/>
         <section>
           <Tweet/>
