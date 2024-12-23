b6498099d8c90290f94bb26bb28543f1cb7bc621
Crisops <crialeperez1835@gmail.com>
2024-12-23 18:20:26 -0500

    feat(supabase): add functions for retrieving user information

---
+++ src/utils/supabase/getUser.ts
@@ -0,0 +1,50 @@
+import { Tables } from "@/types/database.types"
+import { createClient } from "./server"
+import { User } from "@supabase/supabase-js"
+
+
+type GetUserProps = {
+    id: string
+}
+
+export const getSessionAuth = async (): Promise<User> => {
+
+    try {
+        const supabase = await createClient()
+        const {data: { user }, error} = await supabase.auth.getUser()
+
+        if(!user || error) throw new Error("Error. Fallo el proceso de obtener la sesión del usuario") 
+            
+        return user
+
+    } catch (error) {
+        console.log(error);
+        throw error
+    }
+    
+    
+}
+
+
+
+export const getUserProfile = async ({ id }: GetUserProps): Promise<Tables<'users'>> => {
+    try {
+        const supabase = await createClient()
+
+        const usersQuery = supabase
+        .from('users')
+        .select('*')
+        .eq('id', id)
+        .single()
+
+        const {data, error} = await usersQuery
+
+        if(error) throw new Error('Error. El usuario no fue encontrado')
+    
+        return data
+
+    } catch (error) {
+        console.log(error);
+        throw error
+    }
+}
