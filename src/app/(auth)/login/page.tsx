678e02ccfc2bd43966bc05ffa6d1681f8f74febf
Crisops <crialeperez1835@gmail.com>
2024-12-24 18:19:16 -0500

    style(responsive): change position element aslo width and height

--- src/app/(auth)/login/page.tsx
+++ src/app/(auth)/login/page.tsx
@@ -8,7 +8,7 @@ export const metadata: Metadata = {
 
 export default function LoginPage (){
     return (
-        <div className="fixed inset-0 flex justify-center items-center bg-sky-800/10 z-50 before:absolute before:inset-0 before:bg-white/10 before:-z-[1]">
+        <div className="absolute top-0 left-0 w-screen h-screen flex justify-center items-center bg-sky-800/10 z-50 before:absolute before:inset-0 before:bg-white/10 before:-z-[1]">
             <SectionForm/>
         </div>
     )
