786bab9f4ada5921f692cb95f80f003a739c4ea1
Crisops <crialeperez1835@gmail.com>
2024-12-24 13:03:34 -0500

    refactor: add props that have information from the created tweet
    Also added the function to format the time.

--- src/components/Home/TweetHeaderContent.tsx
+++ src/components/Home/TweetHeaderContent.tsx
@@ -1,21 +1,29 @@
 import { IconDots } from "@tabler/icons-react";
+import { formatTimeTweet } from "@/utils/formatTime";
 
-export default function TweetHeaderContent() {
+interface TweetHeaderContentProps {
+    name: string
+    username: string
+    created_at: string
+}
+
+export default function TweetHeaderContent({name, username, created_at}: TweetHeaderContentProps) {
+
 
   return (
     <header className='flex items-center justify-between'>
         <div className='flex gap-x-1 items-center [&>div>span]:text-base'>
             <div>
-                <span className="text-white font-medium">Alejandro Pérez</span>
+                <span className="text-white font-medium">{name}</span>
             </div>
             <div>
-                <span className="text-zinc-500 font-normal">@crisopsdev</span>
+                <span className="text-zinc-500 font-normal">@{username}</span>
             </div>
             <div>
                 <span className="text-zinc-500 font-normal">·</span>
             </div>
             <div>
-                <span className="text-zinc-500 font-normal">24h</span>
+                <span className="text-zinc-500 font-normal">{formatTimeTweet(created_at)}</span>
             </div>
         </div>
         <div >
