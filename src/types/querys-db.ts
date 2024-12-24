306ab1e0e260583cff44868a32017be52560cb3f
Crisops <crialeperez1835@gmail.com>
2024-12-24 18:12:28 -0500

    feat(types): add types for querys db

---
+++ src/types/querys-db.ts
@@ -0,0 +1,16 @@
+import { Tables } from "./database.types"
+
+export type TweetInfo = {
+    id: Tables<"tweets">["id"]
+    content: Tables<"tweets">["content"]
+    image_url: Tables<"tweets">["image_url"]
+    likes: Tables<"tweets">["likes"]
+    retuits: Tables<"tweets">["retuits"]
+    comments: Tables<"tweets">["comments"]
+    created_at: Tables<"tweets">["created_at"]
+    creator: {
+        name: Tables<'users'>['name']; 
+        username: Tables<'users'>['username']; 
+        avatar_url: Tables<'users'>['avatar_url'];
+    } | null
+}
