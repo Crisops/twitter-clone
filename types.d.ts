bb2c51ddca43e83379285e391ba211c507fd6ad4
Crisops <crialeperez1835@gmail.com>
2024-12-24 12:44:02 -0500

    feat: add type to get information tweets

--- types.d.ts
+++ types.d.ts
@@ -1,3 +1,5 @@
+import { Tables } from "@/types/database.types"
+
 type FormLogin = {
     email: string
     password: string
@@ -22,6 +24,23 @@ type LinksAsideNavHome = {
     icon: JSX.Element
     text: string
 }
+
+type TweetInfo = {
+    id: Tables<"tweets">["id"]
+    content: Tables<"tweets">["content"]
+    image_url: Tables<"tweets">["image_url"]
+    likes: Tables<"tweets">["likes"]
+    retuits: Tables<"tweets">["retuits"]
+    comments: Tables<"tweets">["comments"]
+    created_at: Tables<"tweets">["created_at"]
+    creator: {
+        name: Tables<'users'>['name']; 
+        username: Tables<'users'>['username']; 
+        avatar_url: Tables<'users'>['avatar_url'];
+    } | null
+}
+
+
 
 type Store = {
     initialForm: FormLogin
