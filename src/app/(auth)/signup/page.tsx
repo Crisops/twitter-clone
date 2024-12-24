803502fea279576b6987dc4a9ac2a36fcb51fda2
Crisops <crialeperez1835@gmail.com>
2024-12-24 18:20:17 -0500

    style(responsive): change position element also width and height

--- src/app/(auth)/signup/page.tsx
+++ src/app/(auth)/signup/page.tsx
@@ -2,7 +2,7 @@ import { SectionForm } from "@/components/SignUp/SectionForm";
 
 export default function SignUpPage() {
     return (
-        <div className="fixed inset-0 flex justify-center items-center bg-sky-800/10 z-50 before:absolute before:inset-0 before:bg-white/10 before:-z-[1]">
+        <div className="absolute top-0 left-0 w-screen h-screen flex justify-center items-center bg-sky-800/10 z-50 before:absolute before:inset-0 before:bg-white/10 before:-z-[1]">
             <SectionForm/>
         </div>
     )
