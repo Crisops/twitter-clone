e7f92bdffd1e5a01d70e703e8c2522520bcdf509
Crisops <crialeperez1835@gmail.com>
2024-12-24 18:21:39 -0500

    style(responsive): change font size with media querys

--- src/components/ChooseAccount/Form.tsx
+++ src/components/ChooseAccount/Form.tsx
@@ -4,7 +4,7 @@ const Form = () => {
 
   return (
     <form className="max-w-72 flex flex-col gap-10">
-        <h1 className="text-slate-100 font-extrabold text-3xl">Únete Hoy</h1>
+        <h1 className="text-slate-100 font-extrabold text-2xl md:text-3xl">Únete Hoy</h1>
         <div className="relative w-full">
             <div className="flex flex-col gap-4">
               <ButtonGoogle textContent="Regístrarse con Google"/>
