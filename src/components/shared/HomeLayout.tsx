deafb5a7308e43ce4800b6f2f5b83e27b6da8c17
Crisops <crialeperez1835@gmail.com>
2024-12-24 18:26:47 -0500

    style(responsive): change display container with media querys and paddings
    Also adjust icon size svg

--- src/components/shared/HomeLayout.tsx
+++ src/components/shared/HomeLayout.tsx
@@ -1,12 +1,19 @@
 import { IconTwitter } from '@/components/Icons'
 import SectionForm from '@/components/ChooseAccount/SectionForm'
+import Footer from '@/components/ChooseAccount/Footer'
+
 
 export const HomeLayout = ({children}: {children?: React.ReactNode}) => {
     return (
-        <section className="h-full grid place-items-center grid-cols-2 pl-20">
-            <IconTwitter size="size-[23rem]"/>
-            <SectionForm/>
+        <section className="h-full">
+            <div className='flex flex-col mx-16 py-5 sm:py-0 sm:mx-16 justify-center lg:grid lg:place-items-center lg:grid-cols-2 h-full xl:pl-20'>
+                <div className='lg:w-full lg:h-full lg:flex lg:justify-center lg:items-center'>
+                    <IconTwitter size="size-8 sm:size-12 lg:size-3/6"/>
+                </div>
+                <SectionForm/>
+            </div>
             {children}
+            <Footer/>
         </section>
     )
 }
