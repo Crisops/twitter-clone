35e0f0537c06b350669de7591b62caf165c1acf4
Crisops <crialeperez1835@gmail.com>
2024-12-24 12:55:17 -0500

    refactor(image): add functions that get all the information about the user
    Image component gets name, username and avatar by props

--- src/components/Home/TweetTextArea.tsx
+++ src/components/Home/TweetTextArea.tsx
@@ -2,12 +2,17 @@ import {Textarea} from "@nextui-org/react";
 import InputFileTweet from "./InputFileTweet";
 import TweetImageUser from "./TweetImageUser";
 import TweetImageLoad from "./TweetImageLoad";
+import { getSessionAuth, getUserProfile } from "@/utils/supabase/getUser";
 
-export default function TweetTextArea() {
+export default async function TweetTextArea() {
+
+  const {id} = await getSessionAuth()
+  const { name, username , avatar_url } = await getUserProfile({ id })
+
   return (
     <div className="grid grid-cols-[max-content_1fr] grid-rows-[max-content_max_content_60px] gap-x-2">
         <div>
-            <TweetImageUser/>
+            <TweetImageUser avatar_url={avatar_url ?? ''} name={name} username={username}/>
         </div>
         <div className="flex-grow">
             <Textarea disableAnimation radius="none" minRows={2} variant="underlined" placeholder="¡¿Qué está pasandó?!" classNames={{input: "text-white text-xl placeholder:text-zinc-500 placeholder:font-normal",  inputWrapper: "border-none after:bg-transpatent" }}/>
