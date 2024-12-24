3d321f8ce15b1bb66f3f1b5a631545d9185cee1c
Crisops <crialeperez1835@gmail.com>
2024-12-24 13:01:20 -0500

    feat(time): add a function that formats the time at which the user creates a tweet

---
+++ src/utils/formatTime.ts
@@ -0,0 +1,15 @@
+import { diffHours, diffMinutes, diffSeconds, format } from "@formkit/tempo"
+
+export const formatTimeTweet = (time: string): string => {
+
+    let currentTime: string = ''
+
+    const seconds = diffSeconds(new Date().toISOString(), time, "floor")
+    const minutes = diffMinutes(new Date().toISOString(), time, "floor")
+    const hours = diffHours(new Date().toISOString(), time, "floor")
+
+    currentTime = (seconds <= 60) ? `${seconds} seg` : (minutes <= 60) ? `${minutes}min` : (hours < 24) ? `${hours}h` : format(time, 'D MMM', 'en')
+
+
+    return currentTime
+}
