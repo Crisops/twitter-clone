b0c7dc9be9d7eb732e9ae022b96672f7afa3975d
Crisops <crialeperez1835@gmail.com>
2024-12-24 18:30:56 -0500

    refactor: enhance Footer component with dynamic positioning and improved layout
    Added 'use client' directive to enable client-side interactivity.
    - Integrated dynamic positioning based on the current route using usePathname:
      - Sets 'absolute' position for '/login' and '/signup' routes.
      - Uses 'relative sm:absolute' for other routes to ensure responsive behavior.
    - Simplified layout by consolidating links and copyright in the same container.
    - Adjusted spacing and alignment for better responsiveness across viewports.

--- src/components/ChooseAccount/Footer.tsx
+++ src/components/ChooseAccount/Footer.tsx
@@ -1,19 +1,24 @@
+'use client'
+
 import { linksFooter } from '@/lib/links'
 import Link from 'next/link'
+import {usePathname} from 'next/navigation'
 
 const Footer = () => {
 
+  const pathname = usePathname()
+
+  const position = pathname === '/login' || pathname === '/signup' ? 'absolute' : 'relative sm:absolute'
+
   return (
-    <footer className='relative w-full h-14 flex flex-col gap-1'>
-        <div className='mx-16'>
-          <div className="flex flex-wrap items-center justify-center 2xl:justify-between gap-2">
+    <footer className={`${position} left-0 bottom-0 w-full flex gap-1 py-1`}>
+        <div className='mx-4 lg:mx-16'>
+          <div className="flex flex-wrap items-start justify-start lg:justify-center gap-2">
             {
               linksFooter.map((link, index) => <Link className='text-zinc-500 hover:underline text-sm' key={index} href={link.href}>{link.title}</Link>)
             }
-          </div>
-        </div>
-        <div className='text-center'>
             <span className='text-zinc-500 text-sm'>© 2024 Crisops</span>
+          </div>
         </div>
     </footer>
   )
