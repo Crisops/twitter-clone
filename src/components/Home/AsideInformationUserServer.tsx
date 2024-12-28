188d9fab22484a9b70eaadb53563989f87703636
Crisops <crialeperez1835@gmail.com>
2024-12-28 13:34:05 -0500

    refactor(alias): rename variable avatar_url to avatar and created_at to date

--- src/components/Home/AsideInformationUserServer.tsx
+++ src/components/Home/AsideInformationUserServer.tsx
@@ -2,14 +2,13 @@ import AsideInformationUserClient from './AsideInformationUserClient'
 import { getSessionAuth, getUserProfile } from '@/utils/supabase/getUser'
 import TweetImageUser from './TweetImageUser'
 
-export default async function AsideInformationUserServer() {
-
-    const {id} = await getSessionAuth()
-    const { name, username , avatar_url } = await getUserProfile({ id })
+export default async function AsideInformationUserServer () {
+  const { id } = await getSessionAuth()
+  const { name, username, avatar_url: avatar } = await getUserProfile({ id })
 
   return (
     <AsideInformationUserClient name={name} username={username}>
-        <TweetImageUser avatar_url={avatar_url ?? ''} name={name} username={username}/>
+      <TweetImageUser avatar_url={avatar ?? ''} name={name} username={username} />
     </AsideInformationUserClient>
   )
 }
--- src/components/Home/Tweet.tsx
+++ src/components/Home/Tweet.tsx
@@ -8,29 +8,27 @@ interface TweetProps {
     tweet: TweetInfo
 }
 
-export default function Tweet({ tweet }: TweetProps) {
-    
-    const {content, image_url, likes, retuits, comments, created_at, creator} = tweet
+export default function Tweet ({ tweet }: TweetProps) {
+  const { content, image_url: avatar, likes, retuits, comments, created_at: date, creator } = tweet
 
-    if(!creator) return
-    
+  if (creator === null) return
+
   return (
     <article className='w-full h-full border-b border-zinc-900'>
-        <div className='py-2 px-4'>
-            <div className='flex gap-x-2'>
-                <TweetImageUser avatar_url={creator.avatar_url ?? ""} name={creator.name} username={creator.username}/>
-                <div className='w-full h-full'>
-                    <TweetHeaderContent name={creator.name} username={creator.username} created_at={created_at}/>
-                    <section className='w-full h-full'>
-                        <TweetContent content={content} image_url={image_url ?? ''}/>
-                        <div className='pt-2'>
-                            <TweetInteractions comments={comments} retuits={retuits} likes={likes}/>
-                        </div>
-                    </section>
-                </div>
-            </div>
+      <div className='py-2 px-4'>
+        <div className='flex gap-x-2'>
+          <TweetImageUser avatar_url={creator.avatar_url ?? ''} name={creator.name} username={creator.username} />
+          <div className='w-full h-full'>
+            <TweetHeaderContent name={creator.name} username={creator.username} created_at={date} />
+            <section className='w-full h-full'>
+              <TweetContent content={content} image_url={avatar ?? ''} />
+              <div className='pt-2'>
+                <TweetInteractions comments={comments} retuits={retuits} likes={likes} />
+              </div>
+            </section>
+          </div>
         </div>
+      </div>
     </article>
   )
 }
-
