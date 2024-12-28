f45ed7f1f758909f813fd7b802d773cc995c771f
Crisops <crialeperez1835@gmail.com>
2024-12-28 15:27:26 -0500

    feat: create function to handle image loading into database

---
+++ src/utils/supabase/storage/uploadImage.ts
@@ -0,0 +1,38 @@
+import imageCompression from 'browser-image-compression'
+import { v4 as uuidv4 } from 'uuid'
+import { createClient } from '../client'
+
+interface UploadImageProps {
+    bucket: 'post-tweets' | 'avatars'
+    file: File
+    folder: string
+}
+
+const getStorage = () => {
+  const supabase = createClient()
+  return supabase
+}
+
+export const uploadImage = async ({ bucket, file, folder }: UploadImageProps) => {
+  const fileName = file.name
+  const fileExt = fileName.split('.').pop()?.toLocaleLowerCase()
+  const filePath = `${folder}/${uuidv4()}.${fileExt}`
+
+  try {
+    file = await imageCompression(file, {
+      maxSizeMB: 1
+    })
+  } catch (error) {
+    console.error(error)
+    return { imageUrl: '', error: 'Image compression failed' }
+  }
+
+  const { storage } = getStorage()
+  const { error } = await storage.from(bucket).upload(filePath, file)
+
+  if (error) return { imageUrl: '', error: 'Image upload failed' }
+
+  const { data: { publicUrl } } = storage.from(bucket).getPublicUrl(filePath)
+
+  return { imageUrl: publicUrl, error: '' }
+}
