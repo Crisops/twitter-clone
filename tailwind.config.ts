20486de4a7ab5f7d558cd487032a74981d6464b4
Crisops <crialeperez1835@gmail.com>
2024-12-28 13:08:25 -0500

    chore(format): convert double quotes to single quotes

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,26 +1,20 @@
 import type { Config } from 'tailwindcss'
-import {nextui} from "@nextui-org/react";
+import { nextui } from '@nextui-org/react'
 
 export default {
   content: [
     './src/pages/**/*.{js,ts,jsx,tsx,mdx}',
     './src/components/**/*.{js,ts,jsx,tsx,mdx}',
     './src/app/**/*.{js,ts,jsx,tsx,mdx}',
-    "./node_modules/@nextui-org/theme/dist/**/*.{js,ts,jsx,tsx}"
+    './node_modules/@nextui-org/theme/dist/**/*.{js,ts,jsx,tsx}'
   ],
   theme: {
     extend: {
-      dropShadow : {
-        "3xl": "0px -3px 2px rgba(255, 255, 255, 0.25)",
+      dropShadow: {
+        '3xl': '0px -3px 2px rgba(255, 255, 255, 0.25)'
       }
     }
-    // extend: {
-    //   colors: {
-    //     background: 'var(--background)',
-    //     foreground: 'var(--foreground)'
-    //   }
-    // }
   },
-  darkMode: "class",
+  darkMode: 'class',
   plugins: [nextui()]
 } satisfies Config
