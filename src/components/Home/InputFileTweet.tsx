6bb88d6fdc1f993cdbf706aa06bfdbcaa05eb837
Crisops <crialeperez1835@gmail.com>
2024-12-28 14:56:14 -0500

    refactor: integrate react-hook-form for file input and form state management

--- src/components/Home/InputFileTweet.tsx
+++ src/components/Home/InputFileTweet.tsx
@@ -1,41 +1,43 @@
-'use client'
+import { ChangeEvent, useId } from 'react'
+import { IconPhoto } from '@tabler/icons-react'
+import { useCreateTweet } from '@/hooks/useStore'
+import { UseFormRegisterReturn } from 'react-hook-form'
 
-import { ChangeEvent, useId } from "react"
-import { IconPhoto } from "@tabler/icons-react"
-import { useCreateTweet } from "@/hooks/useStore"
+interface InputFileTweetProps {
+  registerField: UseFormRegisterReturn
+}
 
-export default function InputFileTweet() {
-
-  const {setImageTweet} = useCreateTweet(state => state) 
+export default function InputFileTweet ({ registerField }:InputFileTweetProps) {
+  const { initialForm, setFormCreateTweet } = useCreateTweet(state => state)
 
   const idFile = useId()
 
   const handleFileChange = (e: ChangeEvent<HTMLInputElement>) => {
+    const { files } = e.target
 
-    const {files} = e.target
-
-    if(files?.length){
-      const [ imageTweet ] = files
-      const urlImage = URL.createObjectURL(imageTweet)
-      setImageTweet(urlImage)    
-    }  
+    if (files?.length) {
+      const [file] = files
+      const imageUrlPreview = file ? URL.createObjectURL(file) : null
+      setFormCreateTweet({ ...initialForm, file, imageUrlPreview })
+    }
   }
 
   return (
     <>
-        <label
+      <label
         htmlFor={idFile}
-        title="Multimedia"
-        className="cursor-pointer"
-        >
-          <IconPhoto size={18} color="#1d9bf0" />
-        </label>
-        <input
+        title='Multimedia'
+        className='cursor-pointer'
+      >
+        <IconPhoto size={18} color='#1d9bf0' />
+      </label>
+      <input
         id={idFile}
-        type="file"
-        className="hidden"
+        {...registerField}
+        type='file'
+        className='hidden'
         onChange={handleFileChange}
-        />
+      />
     </>
 
   )
