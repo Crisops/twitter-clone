efb75c9d5d0392b1186b8743fdd4fd6eebd0b828
Crisops <crialeperez1835@gmail.com>
2024-12-28 13:28:25 -0500

    chore(format): convert double quotes to single quotes and adjust spaces

--- src/utils/supabase/middleware.ts
+++ src/utils/supabase/middleware.ts
@@ -1,10 +1,10 @@
 import { createServerClient } from '@supabase/ssr'
 import { NextResponse, type NextRequest } from 'next/server'
-import  { EnvConfig } from '@/config/env.config'
+import { EnvConfig } from '@/config/env.config'
 
-export async function updateSession(request: NextRequest) {
+export async function updateSession (request: NextRequest) {
   let supabaseResponse = NextResponse.next({
-    request,
+    request
   })
 
   const supabase = createServerClient(
@@ -12,19 +12,19 @@ export async function updateSession(request: NextRequest) {
     EnvConfig().SUPABASE_ANON_KEY!,
     {
       cookies: {
-        getAll() {
+        getAll () {
           return request.cookies.getAll()
         },
-        setAll(cookiesToSet) {
+        setAll (cookiesToSet) {
           cookiesToSet.forEach(({ name, value }) => request.cookies.set(name, value))
           supabaseResponse = NextResponse.next({
-            request,
+            request
           })
           cookiesToSet.forEach(({ name, value, options }) =>
             supabaseResponse.cookies.set(name, value, options)
           )
-        },
-      },
+        }
+      }
     }
   )
 
@@ -36,18 +36,17 @@ export async function updateSession(request: NextRequest) {
     !request.nextUrl.pathname.startsWith('/signup') &&
     !request.nextUrl.pathname.startsWith('/auth') &&
     request.nextUrl.pathname !== '/'
-  ) {    
+  ) {
     const url = request.nextUrl.clone()
     url.pathname = '/'
     return NextResponse.redirect(url)
   }
 
-  if(user && request.nextUrl.pathname === "/") {
+  if (user && request.nextUrl.pathname === '/') {
     const url = request.nextUrl.clone()
     url.pathname = '/home'
     return NextResponse.redirect(url)
   }
-
 
   return supabaseResponse
 }
--- src/utils/supabase/server.ts
+++ src/utils/supabase/server.ts
@@ -2,8 +2,8 @@ import { Database } from '@/types/database.types'
 import { createServerClient } from '@supabase/ssr'
 import { cookies } from 'next/headers'
 import { EnvConfig } from '@/config/env.config'
- 
-export async function createClient() {
+
+export async function createClient () {
   const cookieStore = await cookies()
 
   return createServerClient<Database>(
@@ -11,10 +11,10 @@ export async function createClient() {
     EnvConfig().SUPABASE_ANON_KEY!,
     {
       cookies: {
-        getAll() {
+        getAll () {
           return cookieStore.getAll()
         },
-        setAll(cookiesToSet) {
+        setAll (cookiesToSet) {
           try {
             cookiesToSet.forEach(({ name, value, options }) =>
               cookieStore.set(name, value, options)
@@ -24,8 +24,8 @@ export async function createClient() {
             // This can be ignored if you have middleware refreshing
             // user sessions.
           }
-        },
-      },
+        }
+      }
     }
   )
 }
