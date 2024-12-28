0264a56ccb5c8e41fbc2a17d6abb873663a7ac00
Crisops <crialeperez1835@gmail.com>
2024-12-28 15:23:00 -0500

    refactor: update the store functions and add the initial object of the form when the modal is closed

--- src/components/shared/HeaderFormAuth.tsx
+++ src/components/shared/HeaderFormAuth.tsx
@@ -3,34 +3,30 @@
 import { IconX } from '@tabler/icons-react'
 import { IconTwitter } from '../Icons'
 import { useRouter } from 'next/navigation'
-import { useCreateNewAccount, useLogin } from '@/hooks/useStore'
-
+import { useSignUp, useLogin } from '@/hooks/useStore'
+import { initialLoginForm, initialSignUpForm } from '@/config/fields-form'
 
 export const HeaderFormAuth = () => {
+  const { setFormLogin } = useLogin(state => state)
+  const { setFormSignUp } = useSignUp(state => state)
 
-    const {setInitialForm} = useLogin(state => state)
-    const {setInitialForm: setNewAccountForm} = useCreateNewAccount(state => state)
+  const router = useRouter()
+  const handleNavigationHome = () => {
+    setFormLogin(initialLoginForm)
+    setFormSignUp(initialSignUpForm)
+    router.push('/')
+  }
 
-    const router = useRouter()
-    const handleNavigationHome = () => {
-        
-        const initialFormChooseAccount = { email: "", password: ""}
-        const initialFormNewAccount = {fullName: "", username:"", email: "", password:"", birthday: { day: "", month: "", year: ""} }
-        setInitialForm(initialFormChooseAccount)
-        setNewAccountForm(initialFormNewAccount)
-        router.push('/')
-    }
-
-    return (
-        <div className="relative w-full p-2 h-14 flex justify-center items-center">
-            <div className='flex-grow basis-0'>
-                <button onClick={handleNavigationHome}  className='hover:bg-white/10 transition-colors ease-in duration-150 rounded-full p-1'>
-                    <IconX size={20} color='#fff'/>
-                </button>
-            </div>
-            <div className='flex-grow'>
-                <IconTwitter size="size-8"/>
-            </div>
-        </div>
-    )
+  return (
+    <div className='relative w-full p-2 h-14 flex justify-center items-center'>
+      <div className='flex-grow basis-0'>
+        <button onClick={handleNavigationHome} className='hover:bg-white/10 transition-colors ease-in duration-150 rounded-full p-1'>
+          <IconX size={20} color='#fff' />
+        </button>
+      </div>
+      <div className='flex-grow'>
+        <IconTwitter size='size-8' />
+      </div>
+    </div>
+  )
 }
