b2a05e7111b3ef3b9fd1b2dee0734bcbd0e3565e
Crisops <crialeperez1835@gmail.com>
2024-12-28 15:00:20 -0500

    refactor: separate tweet display into a separate component and add Suspense to wait for the reply

--- src/components/Home/SectionTwittsMain.tsx
+++ src/components/Home/SectionTwittsMain.tsx
@@ -1,21 +1,16 @@
-import { getTweets } from "@/utils/supabase/getTweets";
-import ChooseViewTwitts from "./ChooseViewTwitts";
-import Tweet from "./Tweet";
-import WriteTweet from "./WriteTweet";
+import ChooseViewTwitts from './ChooseViewTwitts'
+import WriteTweet from './WriteTweet'
+import { Suspense } from 'react'
+import ViewAllTweets from './ViewAllTweets'
 
-export default async function SectionTwittsMain() {
-
-  const tweets = await getTweets()
-
+export default async function SectionTwittsMain () {
   return (
-    <section className="h-full">
-        <ChooseViewTwitts/>
-        <WriteTweet/>
-        <section>
-          {
-            tweets.map((tweet) => <Tweet key={tweet.id} tweet={{...tweet}}/>)
-          }
-        </section>
+    <section className='h-full'>
+      <ChooseViewTwitts />
+      <WriteTweet />
+      <Suspense fallback='cargando...'>
+        <ViewAllTweets />
+      </Suspense>
     </section>
   )
 }
