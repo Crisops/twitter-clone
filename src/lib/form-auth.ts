42c0d3a7fe57812c93c85587fcaf8f8a58bd632d
Crisops <crialeperez1835@gmail.com>
2024-12-28 14:49:51 -0500

    refactoring: add new types and changing double quotes to single quotes

--- src/lib/form-auth.ts
+++ src/lib/form-auth.ts
@@ -1,83 +1,83 @@
-import { RegisterOptions } from "react-hook-form"
-
+import { type FormLogin, type FormSignUp } from '@/types/store'
+import { RegisterOptions } from 'react-hook-form'
 
 export const validationRulesLogin: Record<keyof FormLogin, RegisterOptions<FormLogin>> = {
-    email: {
-        required: "El correo es requerido",
-        pattern: {
-            value: /^[a-z0-9!#$%&'*+/=?^_`{|}~-]+(?:\.[a-z0-9!#$%&'*+/=?^_`{|}~-]+)*@(?:[a-z0-9](?:[a-z0-9-]*[a-z0-9])?\.)+[a-z0-9](?:[a-z0-9-]*[a-z0-9])?$/,
-            message: "Ingresa un email valido"
-        },
-    },
-    password: {
-        required: "La contraseña debe tener entre 4 y 60 caracteres.",
-        minLength: {
-            value: 4,
-            message: 'La contraseña debe tener entre 4 y 60 caracteres.',
-        },
-        maxLength: {
-            value: 60,
-            message: 'La contraseña debe tener entre 4 y 60 caracteres.',
-        },
+  email: {
+    required: 'El correo es requerido',
+    pattern: {
+      value: /^[a-z0-9!#$%&'*+/=?^_`{|}~-]+(?:\.[a-z0-9!#$%&'*+/=?^_`{|}~-]+)*@(?:[a-z0-9](?:[a-z0-9-]*[a-z0-9])?\.)+[a-z0-9](?:[a-z0-9-]*[a-z0-9])?$/,
+      message: 'Ingresa un email valido'
     }
-} 
+  },
+  password: {
+    required: 'La contraseña debe tener entre 4 y 60 caracteres.',
+    minLength: {
+      value: 4,
+      message: 'La contraseña debe tener entre 4 y 60 caracteres.'
+    },
+    maxLength: {
+      value: 60,
+      message: 'La contraseña debe tener entre 4 y 60 caracteres.'
+    }
+  }
+}
 
-export const validationRulesSignUp: Record<keyof NewAccountForm, RegisterOptions<NewAccountForm>> = {
-    fullName: {
-        required: "¿Cómo te llamas?",
-        minLength: {
-            value: 4,
-            message: 'El nombre debe tener entre 4 y 255 caracteres.',
-        },
-        maxLength: {
-            value: 255,
-            message: 'El nombre debe tener entre 4 y 255 caracteres.',
-        },
-        pattern: {
-            value: /^[a-zA-ZáéíóúÁÉÍÓÚñÑüÜ\s]{1,255}$/,
-            message: "El nombre debe tener solo letras y espacios"
-        }
+export const validationRulesSignUp: Record<keyof FormSignUp, RegisterOptions<FormSignUp>> = {
+  fullName: {
+    required: '¿Cómo te llamas?',
+    minLength: {
+      value: 4,
+      message: 'El nombre debe tener entre 4 y 255 caracteres.'
     },
-    username:{
-        required: "El nombre de usuario es requerido",
-        pattern: {
-            value: /^[a-zA-Z0-9_]{4,60}$/,
-            message: "El nombre de usuario debe tener solo letras y numeros"
-        },
-        minLength: {
-            value: 4,
-            message: 'El nombre de usuario debe tener entre 4 y 15 caracteres.',
-        },
-        maxLength: {
-            value: 15,
-            message: "El nombre de usuario debe tener entre 4 y 15 caracteres."
-        }
+    maxLength: {
+      value: 255,
+      message: 'El nombre debe tener entre 4 y 255 caracteres.'
     },
-    email: {
-        required: "El correo es requerido",
-        pattern: {
-            value: /^[a-z0-9!#$%&'*+/=?^_`{|}~-]+(?:\.[a-z0-9!#$%&'*+/=?^_`{|}~-]+)*@(?:[a-z0-9](?:[a-z0-9-]*[a-z0-9])?\.)+[a-z0-9](?:[a-z0-9-]*[a-z0-9])?$/,
-            message: "Ingresa un email valido"
-        },
-        minLength: {
-            value: 4,
-            message: "El correo electrónico debe de tener entre 10 y 320 caracteres."
-        },
-        maxLength: {
-            value: 320,
-            message: "El correo electrónico debe de tener entre 10 y 320 caracteres."
-        }
+    pattern: {
+      value: /^[a-zA-ZáéíóúÁÉÍÓÚñÑüÜ\s]{1,255}$/,
+      message: 'El nombre debe tener solo letras y espacios'
+    }
+  },
+  username: {
+    required: 'El nombre de usuario es requerido',
+    pattern: {
+      value: /^[a-zA-Z0-9_]{4,60}$/,
+      message: 'El nombre de usuario debe tener solo letras y numeros'
     },
-    password: {
-        required: "La contraseña debe tener entre 4 y 60 caracteres.",
-        minLength: {
-            value: 4,
-            message: 'La contraseña debe tener entre 4 y 60 caracteres.',
-        },
-        maxLength: {
-            value: 60,
-            message: 'La contraseña debe tener entre 4 y 60 caracteres.',
-        },
+    minLength: {
+      value: 4,
+      message: 'El nombre de usuario debe tener entre 4 y 15 caracteres.'
     },
-    birthday: {}
+    maxLength: {
+      value: 15,
+      message: 'El nombre de usuario debe tener entre 4 y 15 caracteres.'
+    }
+  },
+  email: {
+    required: 'El correo es requerido',
+    pattern: {
+      value: /^[a-z0-9!#$%&'*+/=?^_`{|}~-]+(?:\.[a-z0-9!#$%&'*+/=?^_`{|}~-]+)*@(?:[a-z0-9](?:[a-z0-9-]*[a-z0-9])?\.)+[a-z0-9](?:[a-z0-9-]*[a-z0-9])?$/,
+      message: 'Ingresa un email valido'
+    },
+    minLength: {
+      value: 4,
+      message: 'El correo electrónico debe de tener entre 10 y 320 caracteres.'
+    },
+    maxLength: {
+      value: 320,
+      message: 'El correo electrónico debe de tener entre 10 y 320 caracteres.'
+    }
+  },
+  password: {
+    required: 'La contraseña debe tener entre 4 y 60 caracteres.',
+    minLength: {
+      value: 4,
+      message: 'La contraseña debe tener entre 4 y 60 caracteres.'
+    },
+    maxLength: {
+      value: 60,
+      message: 'La contraseña debe tener entre 4 y 60 caracteres.'
+    }
+  },
+  birthday: {}
 }
