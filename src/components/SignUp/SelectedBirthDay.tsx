8ab3db91e5132794327d4a5d5659a827ff567f0a
Crisops <crialeperez1835@gmail.com>
2024-12-28 15:25:03 -0500

    refactor: update the store functions and adjust spaces

--- src/components/SignUp/SelectedBirthDay.tsx
+++ src/components/SignUp/SelectedBirthDay.tsx
@@ -1,51 +1,47 @@
-
-import {Select, SelectItem} from "@nextui-org/react";
-import {  months, days, years } from "@/lib/data-date";
-import { ChangeEvent } from "react";
-import { useCreateNewAccount } from "@/hooks/useStore";
+import { Select, SelectItem } from '@nextui-org/react'
+import { months, days, years } from '@/lib/data-date'
+import { ChangeEvent } from 'react'
+import { useSignUp } from '@/hooks/useStore'
 
 export const SelectedBirthday = () => {
+  const { initialForm, setFormSignUp } = useSignUp(state => state)
 
-    const {initialForm, setInitialForm} = useCreateNewAccount(state => state)
+  const handleOnSelectedChange = (e: ChangeEvent<HTMLSelectElement>) => {
+    const { value, name } = e.target
+    const birthday = { ...initialForm.birthday, [name]: value }
 
-    const handleOnSelectedChange = (e: ChangeEvent<HTMLSelectElement>) => {
-        
-        const {value, name} = e.target
-        const birthday = {...initialForm.birthday, [name]: value}
+    setFormSignUp({ ...initialForm, birthday })
+  }
 
-        setInitialForm({ ...initialForm, birthday })
-    }
-
-    
   return (
-    <div className="flex gap-4 mt-5">
-        <Select label="Mes" name="month" radius="sm" variant="bordered" size="md" onChange={handleOnSelectedChange} selectedKeys={[initialForm.birthday.month]}>
-            {
+    <div className='flex gap-4 mt-5'>
+      <Select label='Mes' name='month' radius='sm' variant='bordered' size='md' onChange={handleOnSelectedChange} selectedKeys={[initialForm.birthday.month]}>
+        {
                 months.map((month) => (
-                    <SelectItem key={month.key} >
-                        {month.label}
-                    </SelectItem>
+                  <SelectItem key={month.key}>
+                    {month.label}
+                  </SelectItem>
                 ))
             }
-        </Select>
-        <Select label="Día" name="day" radius="sm" variant="bordered" size="md" onChange={handleOnSelectedChange} selectedKeys={[initialForm.birthday.day]}>
-            {
+      </Select>
+      <Select label='Día' name='day' radius='sm' variant='bordered' size='md' onChange={handleOnSelectedChange} selectedKeys={[initialForm.birthday.day]}>
+        {
                 days.map((day) => (
-                    <SelectItem key={day.key}>
-                        {day.label}
-                    </SelectItem>
+                  <SelectItem key={day.key}>
+                    {day.label}
+                  </SelectItem>
                 ))
             }
-        </Select>
-        <Select label="Año" name="year" radius="sm" variant="bordered" size="md" onChange={handleOnSelectedChange} selectedKeys={[initialForm.birthday.year]}>
-            {
+      </Select>
+      <Select label='Año' name='year' radius='sm' variant='bordered' size='md' onChange={handleOnSelectedChange} selectedKeys={[initialForm.birthday.year]}>
+        {
                 years.map((year) => (
-                    <SelectItem key={year.key}>
-                        {year.label}
-                    </SelectItem>
+                  <SelectItem key={year.key}>
+                    {year.label}
+                  </SelectItem>
                 ))
             }
-        </Select>
+      </Select>
     </div>
   )
 }
