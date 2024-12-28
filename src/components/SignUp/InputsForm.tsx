e6bcb25cd2f280b6495316362986f0e52f04ddad
Crisops <crialeperez1835@gmail.com>
2024-12-28 15:24:43 -0500

    refactor: update the store functions and adjust spaces

--- src/components/SignUp/InputsForm.tsx
+++ src/components/SignUp/InputsForm.tsx
@@ -1,47 +1,42 @@
-import {  useEffect, useId } from "react"
-import { InputForm } from "@/components/shared/InputForm"
-import { useCreateNewAccount } from "@/hooks/useStore"
-import { useFormAuthSignUp } from "@/hooks/useFormAuthSignUp"
-
+import { useEffect, useId } from 'react'
+import { InputForm } from '@/components/shared/InputForm'
+import { useSignUp } from '@/hooks/useStore'
+import { useFormAuthSignUp } from '@/hooks/useFormAuthSignUp'
 
 export const InputsForm = () => {
+  const { registerField, watch, errors } = useFormAuthSignUp('onChange')
 
+  const idNameUser = useId()
+  const idEmailUser = useId()
 
-  const {registerField, watch, errors} = useFormAuthSignUp("onChange")
-  
-    const idNameUser = useId()
-    const idEmailUser = useId()
+  const { initialForm, setFormSignUp } = useSignUp(state => state)
 
-    const {initialForm, setInitialForm} = useCreateNewAccount(state => state)
+  useEffect(() => {
+    const { unsubscribe } = watch(({ fullName, email }) => {
+      if (fullName && email) setFormSignUp({ ...initialForm, fullName, email })
+    })
 
-    useEffect(() => {
-
-      const {unsubscribe} = watch(({fullName, email}) => {
-        if(fullName && email ) setInitialForm({...initialForm, fullName, email})
-      })
-
-      return () => unsubscribe()
-
-    }, [watch, initialForm, setInitialForm])
+    return () => unsubscribe()
+  }, [watch, initialForm, setFormSignUp])
 
   return (
     <>
-        <InputForm
+      <InputForm
         id={idNameUser}
-        label="Nombre"
-        type="text"
-        handleInputRegister={{...registerField("fullName")}}
+        label='Nombre'
+        type='text'
+        handleInputRegister={{ ...registerField('fullName') }}
         errorMessage={errors.fullName?.message}
-        valueInput={watch("fullName")}
-        />
-        <InputForm
+        valueInput={watch('fullName')}
+      />
+      <InputForm
         id={idEmailUser}
-        label="Correo electrónico"
-        type="email"
-        handleInputRegister={{...registerField("email")}}
+        label='Correo electrónico'
+        type='email'
+        handleInputRegister={{ ...registerField('email') }}
         errorMessage={errors.email?.message}
-        valueInput={watch("email")}
-        />
+        valueInput={watch('email')}
+      />
     </>
   )
 }
