c619be7e0ce37fac1cbe997d8bfe6a3a98846495
Crisops <crialeperez1835@gmail.com>
2024-12-28 13:34:24 -0500

    refactor(alias): rename variable created_at to date

--- src/components/Home/TweetHeaderContent.tsx
+++ src/components/Home/TweetHeaderContent.tsx
@@ -1,5 +1,5 @@
-import { IconDots } from "@tabler/icons-react";
-import { formatTimeTweet } from "@/utils/formatTime";
+import { IconDots } from '@tabler/icons-react'
+import { formatTimeTweet } from '@/utils/formatTime'
 
 interface TweetHeaderContentProps {
     name: string
@@ -7,28 +7,26 @@ interface TweetHeaderContentProps {
     created_at: string
 }
 
-export default function TweetHeaderContent({name, username, created_at}: TweetHeaderContentProps) {
-
-
+export default function TweetHeaderContent ({ name, username, created_at: date }: TweetHeaderContentProps) {
   return (
     <header className='flex items-center justify-between'>
-        <div className='flex gap-x-1 items-center [&>div>span]:text-base'>
-            <div>
-                <span className="text-white font-medium">{name}</span>
-            </div>
-            <div>
-                <span className="text-zinc-500 font-normal">@{username}</span>
-            </div>
-            <div>
-                <span className="text-zinc-500 font-normal">·</span>
-            </div>
-            <div>
-                <span className="text-zinc-500 font-normal">{formatTimeTweet(created_at)}</span>
-            </div>
+      <div className='flex gap-x-1 items-center [&>div>span]:text-base'>
+        <div>
+          <span className='text-white font-medium'>{name}</span>
         </div>
-        <div >
-            <IconDots size={20} color="#eee"/>
+        <div>
+          <span className='text-zinc-500 font-normal'>@{username}</span>
         </div>
+        <div>
+          <span className='text-zinc-500 font-normal'>·</span>
+        </div>
+        <div>
+          <span className='text-zinc-500 font-normal'>{formatTimeTweet(date)}</span>
+        </div>
+      </div>
+      <div>
+        <IconDots size={20} color='#eee' />
+      </div>
     </header>
   )
 }
