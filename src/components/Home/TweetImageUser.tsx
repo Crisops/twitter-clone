9bd4517357931915f256a94de8aa8b738bbfe58f
Crisops <crialeperez1835@gmail.com>
2024-12-28 20:26:32 -0500

    refactoring: add props to manage the width and height of images

--- src/components/Home/TweetImageUser.tsx
+++ src/components/Home/TweetImageUser.tsx
@@ -4,12 +4,14 @@ interface TweetImageUserProps {
   avatar_url: string
   name: string
   username: string
+  width: number
+  height: number
 }
 
-export default async function TweetImageUser ({ avatar_url: avatar, name, username }: TweetImageUserProps) {
+export default async function TweetImageUser ({ avatar_url: avatar, name, username, width = 40, height = 40 }: TweetImageUserProps) {
   return (
     <div>
-      <Image className='rounded-full' src={avatar ?? ''} width={40} height={40} alt={`${name} - @${username}`} />
+      <Image className='rounded-full' src={avatar ?? ''} width={width} height={height} alt={`${name} - @${username}`} />
     </div>
   )
 }
