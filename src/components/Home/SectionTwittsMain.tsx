2ac03979ec8cc68cc7ea4e391503f32ca61feb0c
Crisops <crialeperez1835@gmail.com>
2024-12-28 20:32:08 -0500

    feat: add ChooseViewTweetsMoviel component when the user navigates on a smarthphone

--- src/components/Home/SectionTwittsMain.tsx
+++ src/components/Home/SectionTwittsMain.tsx
@@ -2,11 +2,13 @@ import ChooseViewTwitts from './ChooseViewTwitts'
 import WriteTweet from './WriteTweet'
 import { Suspense } from 'react'
 import ViewAllTweets from './ViewAllTweets'
+import ChooseViewTweetsMoviel from '@/components/Movile/Home/ChooseViewTweetsMoviel'
 
 export default async function SectionTwittsMain () {
   return (
     <section className='h-full'>
       <ChooseViewTwitts />
+      <ChooseViewTweetsMoviel />
       <WriteTweet />
       <Suspense fallback='cargando...'>
         <ViewAllTweets />
