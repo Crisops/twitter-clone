0ccba478cdd0b01df574578f58b42eb19ef57f34
Crisops <crialeperez1835@gmail.com>
2024-12-29 16:09:36 -0500

    refactor: add support for dynamic link rendering in AsideNavigation
    This update enables dynamic link rendering for desktop and mobile views, enhancing flexibility and reducing duplication.

--- src/components/Home/AsideNavigation.tsx
+++ src/components/Home/AsideNavigation.tsx
@@ -1,11 +1,18 @@
 import LinksSideNav from './LinksSideNav'
+import { linksDesktop, linksMovileDrawer } from '@/lib/links-navbar'
 
-export default function AsideNavigation () {
+interface AsideNavigationProps {
+  viewMovil?: boolean
+}
+
+export default function AsideNavigation ({ viewMovil }: AsideNavigationProps) {
+  const preLoadLinks = viewMovil ? linksMovileDrawer : linksDesktop
+
   return (
     <>
       <nav className='flex'>
         <ul className='flex flex-col justify-start w-full gap-1'>
-          <LinksSideNav />
+          <LinksSideNav links={preLoadLinks} viewMovil={viewMovil} />
         </ul>
       </nav>
     </>
