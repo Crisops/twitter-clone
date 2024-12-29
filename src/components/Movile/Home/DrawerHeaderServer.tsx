4a37a1776fbb055726011bd88dfda73ba1844d6b
Crisops <crialeperez1835@gmail.com>
2024-12-29 16:01:00 -0500

    feat: add DrawerHeaderContentClient prop to DrawerHeaderClient and refactor DrawerHeaderServer

--- src/components/Movile/Home/DrawerHeaderServer.tsx
+++ src/components/Movile/Home/DrawerHeaderServer.tsx
@@ -1,13 +1,22 @@
 import TweetImageUser from '@/components/Home/TweetImageUser'
 import DrawerHeaderClient from './DrawerHeaderClient'
 import { getSessionAuth, getUserProfile } from '@/utils/supabase/getUser'
+import DrawerHeaderContentClient from './DrawerHeaderContentClient'
 
 async function DrawerHeaderServer () {
   const { id } = await getSessionAuth()
   const { name, username, avatar_url: avatar } = await getUserProfile({ id })
 
   return (
-    <DrawerHeaderClient>
+    <DrawerHeaderClient
+      DrawerHeaderContentClient={
+        <DrawerHeaderContentClient
+          avatar={<TweetImageUser avatar_url={avatar ?? ''} name={name} username={username} width={40} height={40} />}
+          name={name}
+          username={username}
+        />
+      }
+    >
       <TweetImageUser avatar_url={avatar ?? ''} name={name} username={username} width={30} height={30} />
     </DrawerHeaderClient>
   )
