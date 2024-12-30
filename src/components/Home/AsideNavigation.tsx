1bc3e287a09be983c1183cbe4afd0ac2ceb58dc4
Crisops <crialeperez1835@gmail.com>
2024-12-29 20:17:14 -0500

    refactor: add prop LinkProfile that will correctly render the correct link to visit the page

--- src/components/Home/AsideNavigation.tsx
+++ src/components/Home/AsideNavigation.tsx
@@ -1,18 +1,20 @@
+import { ReactNode } from 'react'
 import LinksSideNav from './LinksSideNav'
 import { linksDesktop, linksMovileDrawer } from '@/lib/links-navbar'
 
 interface AsideNavigationProps {
   viewMovil?: boolean
+  LinkProfile?: ReactNode
 }
 
-export default function AsideNavigation ({ viewMovil }: AsideNavigationProps) {
+export default function AsideNavigation ({ viewMovil, LinkProfile }: AsideNavigationProps) {
   const preLoadLinks = viewMovil ? linksMovileDrawer : linksDesktop
 
   return (
     <>
       <nav className='flex'>
         <ul className='flex flex-col justify-start w-full gap-1'>
-          <LinksSideNav links={preLoadLinks} viewMovil={viewMovil} />
+          <LinksSideNav links={preLoadLinks} viewMovil={viewMovil} LinkProfile={LinkProfile} />
         </ul>
       </nav>
     </>
