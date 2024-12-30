4eb58dd72f19c3306d32b0e8cddf6b9c0bdb7c42
Crisops <crialeperez1835@gmail.com>
2024-12-29 20:15:33 -0500

    feat: add AsideNavigationDesktop to show correct navigation in desktop layout

---
+++ src/components/Home/AsideNavigationDesktop.tsx
@@ -0,0 +1,8 @@
+import AsideNavigation from './AsideNavigation'
+import LinkProfile from './LinkProfile'
+
+export default function AsideNavigationDesktop () {
+  return (
+    <AsideNavigation LinkProfile={<LinkProfile />} />
+  )
+}
