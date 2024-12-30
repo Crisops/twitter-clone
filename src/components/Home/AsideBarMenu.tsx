a60b23b9b1d3badc5e64324f36dade3bd2549e55
Crisops <crialeperez1835@gmail.com>
2024-12-29 20:19:11 -0500

    refactor: remove style border in aside and add AsideNavigationDesktop component

--- src/components/Home/AsideBarMenu.tsx
+++ src/components/Home/AsideBarMenu.tsx
@@ -1,10 +1,11 @@
 import { IconTwitter } from '../Icons'
 import AsideInformationUserServer from './AsideInformationUserServer'
 import AsideNavigation from './AsideNavigation'
+import AsideNavigationDesktop from './AsideNavigationDesktop'
 
 export default function AsideBarMenu () {
   return (
-    <aside className='hidden min-[500px]:block sticky top-0 h-screen border-r-1 border-zinc-900'>
+    <aside className='hidden min-[500px]:block sticky top-0 h-screen'>
       <div className='h-full px-1'>
         <div className='flex flex-col justify-between h-full'>
           <div className='flex flex-col w-full h-full'>
@@ -12,8 +13,11 @@ export default function AsideBarMenu () {
               <IconTwitter size='size-[2rem]' />
             </header>
             <div className='h-full'>
-              <div className='pl-1 h-full'>
+              <div className='min-[500px]:hidden pl-1 h-full'>
                 <AsideNavigation />
+              </div>
+              <div className='hidden min-[500px]:block pl-1 h-full'>
+                <AsideNavigationDesktop />
               </div>
             </div>
           </div>
