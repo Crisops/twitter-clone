2d288ab929776b09a7b8c2403b736d6f3eaf18b7
Crisops <crialeperez1835@gmail.com>
2024-12-31 18:27:34 -0500

    refactoring: adding components that display user information

--- src/app/(home)/[username]/page.tsx
+++ src/app/(home)/[username]/page.tsx
@@ -1,11 +1,22 @@
+import HeaderBack from '@/components/Profile/HeaderBack'
+import InformationProfile from '@/components/Profile/InformationProfile'
+import ViewInteractivity from '@/components/Profile/ViewInteractivity'
+import { getUserProfileByUsername } from '@/utils/supabase/getUser'
+
 interface ProfilePageParams {
     params: {
         username: string
     }
 }
 
-export default function ProfilePage ({ params: { username } }:ProfilePageParams) {
+export default async function ProfilePage ({ params: { username } }:ProfilePageParams) {
+  const { id, avatar_url: avatarUrl, banner_url: bannerUrl, name, created_at: joined } = await getUserProfileByUsername({ username })
+
   return (
-    <div>Hola : {username.replace('%40', '')}</div>
+    <section className='h-[200vh] border-r border-zinc-700'>
+      <HeaderBack name={name} countPost={0} />
+      <InformationProfile idUserVisited={id} avatarUrl={avatarUrl} bannerUrl={bannerUrl} createdAt={joined} />
+      <ViewInteractivity />
+    </section>
   )
 }
