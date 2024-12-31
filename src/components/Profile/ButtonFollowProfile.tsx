3c2b3b06ab7a4239132c6af2b72d02b6049b5056
Crisops <crialeperez1835@gmail.com>
2024-12-31 18:23:45 -0500

    feat: add component that handles follow user visited

---
+++ src/components/Profile/ButtonFollowProfile.tsx
@@ -0,0 +1,9 @@
+function ButtonFollowProfile () {
+  return (
+    <button className='bg-slate-100 text-black font-semibold border border-black rounded-full py-1.5 px-4 transition-colors duration-200 ease-in-out hover:bg-slate-200'>
+      Seguir
+    </button>
+  )
+}
+
+export default ButtonFollowProfile
