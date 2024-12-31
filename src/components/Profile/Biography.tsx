67dfc5284967933557b83d5dd94f386a5143a873
Crisops <crialeperez1835@gmail.com>
2024-12-31 18:25:14 -0500

    feat: add component to display user profile biography

---
+++ src/components/Profile/Biography.tsx
@@ -0,0 +1,9 @@
+function Biography () {
+  return (
+    <div className='my-2'>
+      <p className='text-white'>Canal de programación y desarrollo</p>
+    </div>
+  )
+}
+
+export default Biography
