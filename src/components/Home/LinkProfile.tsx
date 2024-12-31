91250cae7bf7c51b876d63f7bcf8356572e897f8
Crisops <crialeperez1835@gmail.com>
2024-12-31 18:26:46 -0500

    refactor: delete "@" in URL

--- src/components/Home/LinkProfile.tsx
+++ src/components/Home/LinkProfile.tsx
@@ -11,7 +11,7 @@ export default async function LinkProfile ({ viewMovil }: LinkProfileProps) {
   const { username } = await getUserProfile({ id })
   return (
     <li className='group'>
-      <Link href={`/@${username}`} className={`w-full flex  ${viewMovil ? 'justify-start' : 'justify-end xl:justify-start'}`}>
+      <Link href={`/${username}`} className={`w-full flex  ${viewMovil ? 'justify-start' : 'justify-end xl:justify-start'}`}>
         <div className={` ${viewMovil ? 'w-full group-hover:bg-[#5b70831A]' : 'w-max rounded-full group-hover:bg-white/10'} py-3 pr-4 transition-colors duration-150`}>
           <div className='flex items-center justify-start gap-5 pl-3 pr-2'>
             <div>
