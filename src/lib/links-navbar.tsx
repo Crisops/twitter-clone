20762555d185e5fe721a6e81812edc41cff3f12c
Crisops <crialeperez1835@gmail.com>
2025-01-05 18:03:38 -0500

    refactor: add props for color stroke icons and sizes

--- src/lib/links-navbar.tsx
+++ src/lib/links-navbar.tsx
@@ -1,19 +1,18 @@
+import { IconHome } from '@/components/Icons'
 import { LinksAsideNavHome } from '@/types/generics'
-import { IconBell, IconBookmark, IconDoorExit, IconMail, IconSearch, IconWashDryOff } from '@tabler/icons-react'
-import { IconBolt, IconHome, IconUsers } from '@/components/Icons'
+import { IconBell, IconBookmark, IconDoorExit, IconMail, IconSearch, IconUsers, IconWashDryOff } from '@tabler/icons-react'
 
 export const linksDesktop: LinksAsideNavHome[] = [
   { href: '/home', icon: <IconHome />, text: 'Inicio' },
-  { href: '/explore', icon: <IconSearch size='1.75rem' />, text: 'Explorar' },
-  { href: '/notifications', icon: <IconBell size='1.75rem' />, text: 'Notificaciones' },
-  { href: '/messages', icon: <IconMail size='1.75rem' />, text: 'Mensajes' },
-  { href: '/grok', icon: <IconWashDryOff size='1.75rem' />, text: 'Grok' },
-  { href: '/bookmarks', icon: <IconBookmark size='1.75rem' />, text: 'Guardados' },
-  { href: '/communities', icon: <IconUsers />, text: 'Comunidades' },
-  { href: '/verified-orgs-signup', icon: <IconBolt />, text: 'Organizaciones ver' }
+  { href: '/explore', icon: <IconSearch size='1.75rem' color='white' />, text: 'Explorar' },
+  { href: '/notifications', icon: <IconBell size='1.75rem' color='white' />, text: 'Notificaciones' },
+  { href: '/messages', icon: <IconMail size='1.75rem' color='white' />, text: 'Mensajes' },
+  { href: '/grok', icon: <IconWashDryOff size='1.75rem' color='white' />, text: 'Grok' },
+  { href: '/bookmarks', icon: <IconBookmark size='1.75rem' color='white' fill='currentColor' />, text: 'Guardados' },
+  { href: '/communities', icon: <IconUsers size='1.75rem' color='white' />, text: 'Comunidades' }
 ]
 
 export const linksMovileDrawer: LinksAsideNavHome[] = [
-  { href: '/bookmarks', icon: <IconBookmark size='1.75rem' />, text: 'Guardados' },
-  { href: '/logout', icon: <IconDoorExit />, text: 'Cerrar sesión' }
+  { href: '/bookmarks', icon: <IconBookmark size='1.75rem' color='white' />, text: 'Guardados' },
+  { href: '/logout', icon: <IconDoorExit size='1.75rem' color='white' />, text: 'Cerrar sesión' }
 ]
