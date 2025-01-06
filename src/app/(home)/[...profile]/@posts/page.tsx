7e1a14d886b541c2ab9d54eb945b0f6bad50ac48
Crisops <crialeperez1835@gmail.com>
2025-01-06 15:17:02 -0500

    refactor: render all tweets made by the user

--- src/app/(home)/[...profile]/@posts/page.tsx
+++ src/app/(home)/[...profile]/@posts/page.tsx
@@ -1,5 +1,21 @@
-export default function PostPage () {
+import Tweet from '@/components/Home/Tweet'
+import { getTweetsById } from '@/utils/supabase/getTweets'
+import { getUserProfileByUsername } from '@/utils/supabase/getUser'
+
+interface PostsPageProps {
+  params: {
+    profile: string[]
+  }
+}
+
+export default async function PostsPage ({ params: { profile } }:PostsPageProps) {
+  const [username] = profile
+  const { id } = await getUserProfileByUsername({ username })
+  const tweets = await getTweetsById({ id })
+
   return (
-    <div className='text-white'>ruta parallet posts</div>
+    <section>
+      {tweets.map((tweet) => <Tweet key={tweet.id} tweet={{ ...tweet }} />)}
+    </section>
   )
 }
