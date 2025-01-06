60e80f3286bd3a495676ef4de498dbd50b61c9c5
Crisops <crialeperez1835@gmail.com>
2025-01-06 14:45:51 -0500

    feat: add data in page [...profile]

---
+++ src/app/(home)/[...profile]/page.tsx
@@ -0,0 +1,33 @@
+import HeaderBack from '@/components/Profile/HeaderBack'
+import InformationProfile from '@/components/Profile/InformationProfile'
+// import ViewInteractivity from '@/components/Profile/ViewInteractivity'
+import { getUserProfileByUsername } from '@/utils/supabase/getUser'
+
+interface ProfilePageParams {
+    params: {
+        profile: string
+    }
+}
+
+export default async function ProfilePage ({ params: { profile: [username] } }:ProfilePageParams) {
+  const { id, username: usernameUser, biography, followers, following, location, web_site: webSite, avatar_url: avatarUrl, banner_url: bannerUrl, name, created_at: joined } = await getUserProfileByUsername({ username })
+
+  return (
+    <>
+      <HeaderBack name={name} countPost={0} />
+      <InformationProfile
+        idUserVisited={id}
+        name={name}
+        username={usernameUser}
+        biography={biography}
+        followers={followers}
+        following={following}
+        location={location}
+        webSite={webSite}
+        avatarUrl={avatarUrl}
+        bannerUrl={bannerUrl}
+        createdAt={joined}
+      />
+    </>
+  )
+}
