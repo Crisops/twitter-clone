722080caebe01658ef5936a4598c2bd31d79dca1
Crisops <crialeperez1835@gmail.com>
2025-01-06 14:42:40 -0500

    refactor: add contidional

--- src/components/Home/LinkProfileClient.tsx
+++ src/components/Home/LinkProfileClient.tsx
@@ -17,11 +17,11 @@ export default function LinkProfileClient ({ viewMovil, username }:LinkProfileCl
       <Link href={`/${username}`} className={`w-full flex  ${viewMovil ? 'justify-start' : 'justify-end xl:justify-start'}`}>
         <div className={` ${viewMovil ? 'w-full group-hover:bg-[#5b70831A]' : 'w-max rounded-full group-hover:bg-white/10'} py-3 pr-4 transition-colors duration-150`}>
           <div className='flex items-center justify-start gap-5 pl-3 pr-2'>
-            <div className={`${pathname === `/${username}` ? 'text-white ' : 'text-transparent'}`}>
+            <div className={`${pathname === `/${username}` || pathname === `/${username}/media` ? 'text-white ' : 'text-transparent'}`}>
               <IconUser size='1.75rem' color='white' fill='currentColor' />
             </div>
             <div className={`${viewMovil ? 'block' : 'hidden xl:block'}`}>
-              <span className={`${pathname === `/${username}` ? 'font-semibold text-white ' : 'font-normal text-gray-200'}  text-xl`}>Perfil</span>
+              <span className={`${pathname === `/${username}` || pathname === `/${username}/media` ? 'font-semibold text-white ' : 'font-normal text-gray-200'}  text-xl`}>Perfil</span>
             </div>
           </div>
         </div>
