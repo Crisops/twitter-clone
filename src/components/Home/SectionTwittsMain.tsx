9bf9f7bdab1a75efde41887b01ca87dd2442ea75
Crisops <crialeperez1835@gmail.com>
2025-01-06 17:32:52 -0500

    style: add media query to border color and width

--- src/components/Home/SectionTwittsMain.tsx
+++ src/components/Home/SectionTwittsMain.tsx
@@ -7,7 +7,7 @@ import LoadingSpinner from '../shared/LoadingSpinner'
 
 export default async function SectionTwittsMain () {
   return (
-    <section className='h-full border-r border-zinc-700'>
+    <section className='h-full min-[500px]:border-r min-[500px]:border-zinc-700'>
       <ChooseViewTwitts />
       <ChooseViewTweetsMoviel />
       <WriteTweet />
