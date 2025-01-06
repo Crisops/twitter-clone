0e22708a107461ca053478c55a192c116cda3332
Crisops <crialeperez1835@gmail.com>
2025-01-06 16:24:10 -0500

    refactor: remove prop countPost and add function get the number of posts made by user

--- src/components/Profile/HeaderBack.tsx
+++ src/components/Profile/HeaderBack.tsx
@@ -1,11 +1,14 @@
+import { getTweetsCountUser } from '@/utils/supabase/getTweets'
 import { IconArrowNarrowLeft } from '@tabler/icons-react'
 
 interface HeaderBackProps {
-    name: string
-    countPost: number
+  id: string
+  name: string
 }
 
-function HeaderBack ({ name, countPost = 0 }: HeaderBackProps) {
+async function HeaderBack ({ id, name }: HeaderBackProps) {
+  const { count } = await getTweetsCountUser({ id })
+
   return (
     <div className='hidden min-[500px]:block sticky top-0 w-full h-14 px-2 border-b border-zinc-900 z-10 backdrop-blur-md bg-black/60'>
       <div className='flex items-center'>
@@ -14,10 +17,10 @@ function HeaderBack ({ name, countPost = 0 }: HeaderBackProps) {
         </div>
         <div className='flex-grow flex flex-col justify-center'>
           <div>
-            <span className='text-xl font-bold text-white'>{name}</span>
+            <span className='text-xl font-bold text-gray-100'>{name}</span>
           </div>
           <div>
-            <span className='text-sm font-normal text-zinc-500'>{countPost} posts</span>
+            <span className='text-sm font-normal text-zinc-500'>{count} posts</span>
           </div>
         </div>
       </div>
