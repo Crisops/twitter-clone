0dc13ec0d0904d8b4fe99e9510279e23ffd80d4b
Crisops <crialeperez1835@gmail.com>
2025-01-09 11:48:22 -0500

    refactor: remove custom hook files and renaming to useFormAuth

---
+++ src/hooks/useFormAuth.ts
@@ -0,0 +1,10 @@
+import { FormAuth, validationRules } from '@/lib/form-auth'
+import { type Mode, useForm } from 'react-hook-form'
+
+export const useFormAuth = (mode?: Mode) => {
+  const { register, handleSubmit, control, trigger, formState: { errors } } = useForm<FormAuth>({ mode })
+
+  const registerField = (name: keyof FormAuth) => register(name, validationRules[name])
+
+  return { registerField, handleSubmit, trigger, control, errors }
+}
