8fd6a300b149fa040d63b5c2f8c3a8756f216eaa
Crisops <crialeperez1835@gmail.com>
2025-01-09 12:02:05 -0500

    refactor: remove component InputsForm and add the component InputForm only

--- src/components/SignUp/DataCredentialsForm.tsx
+++ src/components/SignUp/DataCredentialsForm.tsx
@@ -1,22 +1,15 @@
-'use client'
-
-import { useFormAuthSignUp } from '@/hooks/useFormAuthSignUp'
-import { InputsForm } from './InputsForm'
 import { SelectedBirthday } from './SelectedBirthDay'
-import { useSignUp } from '@/hooks/useStore'
 import { months, days, years } from '@/lib/data-date'
+import { InputForm } from '@/components/shared/InputForm'
+import { useId } from 'react'
 
 interface DataCredentialsFormProps {
     handleNextData: () => void
 }
 
 export const DataCredentialsForm = ({ handleNextData }: DataCredentialsFormProps) => {
-  const { initialForm } = useSignUp(state => state)
-  const { errors } = useFormAuthSignUp()
-
-  const { birthday } = initialForm
-
-  const handleErrorInputs = errors.fullName?.message || errors.email?.message || (birthday.day === '' || birthday.month === '' || birthday.year === '')
+  const idNameUser = useId()
+  const idEmailUser = useId()
 
   return (
     <div className='flex items-center flex-col w-full h-[calc(100%-3.5rem)]'>
@@ -25,7 +18,18 @@ export const DataCredentialsForm = ({ handleNextData }: DataCredentialsFormProps
           <h1 className='text-3xl text-white font-semibold'>Crea tu cuenta</h1>
         </div>
         <section className='flex flex-col w-full h-full'>
-          <InputsForm />
+          <InputForm
+            id={idNameUser}
+            label='Nombre'
+            type='text'
+            registerName='fullName'
+          />
+          <InputForm
+            id={idEmailUser}
+            registerName='email'
+            label='Correo electrónico'
+            type='email'
+          />
           <div>
             <h3 className='text-white text-base font-medium'>Fecha de nacimiento</h3>
             <p className='text-zinc-500 text-sm'>Esta información no será pública. Confirma tu propia edad, incluso si esta cuenta es para una empresa, una mascota u otra cosa.</p>
@@ -38,7 +42,7 @@ export const DataCredentialsForm = ({ handleNextData }: DataCredentialsFormProps
         </section>
         <div className='mb-10'>
           <div>
-            <button onClick={handleNextData} type='button' className={`${handleErrorInputs ? 'bg-white/40 pointer-events-none' : 'bg-white hover:bg-white/90'} text-black text-lg w-full rounded-full font-medium p-3`}>Siguiente</button>
+            <button onClick={handleNextData} type='button' className='text-black bg-white text-lg w-full rounded-full font-medium p-3'>Siguiente</button>
           </div>
         </div>
       </div>
