6413857c7aa46097be07199e870a1da5ed351ebe
Crisops <crialeperez1835@gmail.com>
2025-01-09 11:46:29 -0500

    refactor: unify registration and login forms in a single function

--- src/hooks/useStore.ts
+++ src/hooks/useStore.ts
@@ -1,15 +1,10 @@
-import { initialCreateTweetForm, initialLoginForm, initialSignUpForm } from '@/config/fields-form'
-import type { StoreSignUp, StoreCreateTweet, StoreLogin } from '@/types/store'
+import { initialCreateTweetForm, initialFormAuth } from '@/config/fields-form'
+import type { StoreCreateTweet, StoreAuth } from '@/types/store'
 import { create } from 'zustand'
 
-export const useLogin = create<StoreLogin>()((set) => ({
-  initialForm: initialLoginForm,
+export const useAuth = create<StoreAuth>()((set) => ({
+  initialForm: initialFormAuth,
   setFormLogin: (initialForm) => set(() => ({ initialForm }))
-}))
-
-export const useSignUp = create<StoreSignUp>((set) => ({
-  initialForm: initialSignUpForm,
-  setFormSignUp: (initialForm) => set(() => ({ initialForm }))
 }))
 
 export const useCreateTweet = create<StoreCreateTweet>((set) => ({
