7138429876544cd64bba56d7077f4ca394ef47d3
Crisops <crialeperez1835@gmail.com>
2025-01-09 11:57:21 -0500

    refactor: add component HeaderFormAuth

--- src/components/SignUp/SectionForm.tsx
+++ src/components/SignUp/SectionForm.tsx
@@ -1,9 +1,13 @@
 import FormSignUp from './FormSignUp'
+import { HeaderFormAuth } from '@/components/shared/HeaderFormAuth'
 
 export const SectionForm = () => {
   return (
     <section className='w-screen h-screen md:max-w-[600px] md:h-[660px] bg-black md:rounded-3xl'>
-      <FormSignUp />
+      <div className='relative w-full h-full'>
+        <HeaderFormAuth />
+        <FormSignUp />
+      </div>
     </section>
   )
 }
