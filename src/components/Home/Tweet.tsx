19a50b0f9175e8758420a3ba25e9d48eb3d8a580
Crisops <crialeperez1835@gmail.com>
2025-01-10 16:02:28 -0500

    refactor: add props interface and add component that show if the user retuiteo a post

--- src/components/Home/Tweet.tsx
+++ src/components/Home/Tweet.tsx
@@ -3,19 +3,23 @@ import TweetHeaderContent from './TweetHeaderContent'
 import TweetContent from './TweetContent'
 import TweetInteractions from './TweetInteractions'
 import { type TweetInfo } from '@/types/querys-db'
+import TweetPostType from '../Profile/TweetPostType'
 
 interface TweetProps {
-    tweet: TweetInfo
+  idUser?: string
+  nameUserVisited?: string
+  tweet: TweetInfo
 }
 
-export default function Tweet ({ tweet }: TweetProps) {
-  const { content, image_url: avatar, likes, retuits, comments, created_at: date, creator } = tweet
+export default function Tweet ({ tweet, idUser, nameUserVisited }: TweetProps) {
+  const { content, image_url: avatar, likes, retuits, comments, created_at: date, creator, post_type: postType } = tweet
 
   if (creator === null) return
 
   return (
     <article className='w-full h-full border-b border-zinc-700'>
       <div className='py-2 px-4'>
+        {postType === 'retweet' && <TweetPostType idUserVisited={idUser ?? ''} nameUserVisited={nameUserVisited ?? ''} />}
         <div className='flex gap-x-2'>
           <TweetImageUser avatar_url={creator.avatar_url ?? ''} name={creator.name} username={creator.username} />
           <div className='w-full h-full'>
