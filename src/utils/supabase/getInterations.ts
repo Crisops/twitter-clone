b10682c58c5e04ffd243f183702224aced20ed8a
Crisops <crialeperez1835@gmail.com>
2025-01-11 17:33:19 -0500

    feat: add functions to obtain interaction data on a tweet

---
+++ src/utils/supabase/getInterations.ts
@@ -0,0 +1,64 @@
+import { Tables } from '@/types/database.types'
+import { createClient } from './server'
+
+interface GetInteractionsProps {
+    userId: Tables<'users'>['id']
+    tweetId: Tables<'tweets'>['id']
+}
+
+export const getComments = async ({ userId, tweetId }: GetInteractionsProps): Promise<Tables<'comments'>[]> => {
+  const supabase = await createClient()
+
+  try {
+    const userQuery = supabase
+      .from('comments')
+      .select()
+      .match({ user_id: userId, tweet_id: tweetId })
+    const { data, error } = await userQuery
+
+    if (error) throw new Error(error.message)
+
+    return data
+  } catch (error) {
+    console.error(error)
+    throw error
+  }
+}
+
+export const getRetweets = async ({ userId, tweetId }: GetInteractionsProps): Promise<Tables<'retuits'>[]> => {
+  const supabase = await createClient()
+
+  try {
+    const userQuery = supabase
+      .from('retuits')
+      .select()
+      .match({ user_id: userId, tweet_id: tweetId })
+    const { data, error } = await userQuery
+
+    if (error) throw new Error(error.message)
+
+    return data
+  } catch (error) {
+    console.error(error)
+    throw error
+  }
+}
+
+export const getLikes = async ({ userId, tweetId }: GetInteractionsProps): Promise<Tables<'likes'>[]> => {
+  const supabase = await createClient()
+
+  try {
+    const userQuery = supabase
+      .from('likes')
+      .select()
+      .match({ user_id: userId, tweet_id: tweetId })
+    const { data, error } = await userQuery
+
+    if (error) throw new Error(error.message)
+
+    return data
+  } catch (error) {
+    console.error(error)
+    throw error
+  }
+}
