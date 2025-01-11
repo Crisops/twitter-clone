98bdb7ce110578d59c85b348e25e9befa5a3c85b
Crisops <crialeperez1835@gmail.com>
2025-01-11 17:36:56 -0500

    feat: integrate server-side interaction logic into TweetInteractions
    - Added server-side logic to fetch user session using `getSessionAuth`.
    - Fetched likes and retweets data dynamically from Supabase using `getLikes` and `getRetweets`.
    - Passed user and tweet-specific data (`idUser`, `idTweet`, `data`) to `InteractionTweet` for handling client-side interactions.
    - Introduced actions for retweets and likes (`insertRetweet`, `deleteRetweet`, `insertLikes`, `deleteLikes`) to enable real-time interaction updates.
    - Updated `InteractionTweet` props to include `data`, `insertData`, and `deleteData` for enhanced functionality.
    - Enhanced interaction styles for hover states and responsiveness.

--- src/components/Home/TweetInteractions.tsx
+++ src/components/Home/TweetInteractions.tsx
@@ -1,40 +1,68 @@
+import { getSessionAuth } from '@/utils/supabase/getUser'
 import InteractionTweet from './InteractionTweet'
-import { IconBookmark, IconHeart, IconMessageCircle, IconRepeat } from '@tabler/icons-react'
+import { IconBookmark, IconMessageCircle, IconRepeat } from '@tabler/icons-react'
+import { getLikes, getRetweets } from '@/utils/supabase/getInterations'
+import { insertRetweet, deleteRetweet, insertLikes, deleteLikes } from '@/actions/actions'
+import { IconHeart } from '../Icons'
 
 interface TweetInteractionsProps {
-    likes: number
-    retuits: number
-    comments: number
+  idTweet: string
+  likes: number
+  retuits: number
+  comments: number
 }
 
-export default function TweetInteractions ({ comments, retuits, likes }:TweetInteractionsProps) {
+export default async function TweetInteractions ({ idTweet, comments, retuits, likes }:TweetInteractionsProps) {
+  const { id } = await getSessionAuth()
+
+  const dataRetweets = await getRetweets({ userId: id, tweetId: idTweet })
+  const dataLikes = await getLikes({ userId: id, tweetId: idTweet })
+
   return (
     <div className='flex justify-between'>
       <div className='flex flex-grow justify-between'>
         <InteractionTweet
-          icon={<IconMessageCircle size={20} className='stroke-zinc-600 group-hover:stroke-sky-500 transition-colors duration-300 ease-in-out' />}
+          icon={<IconMessageCircle size={20} className='group-hover:stroke-sky-500 transition-colors duration-300 ease-in-out' />}
           bgColor='group-hover:before:bg-sky-500/10'
-          textColor='group-hover:text-sky-500'
+          textColor='text-sky-500'
+          textColorHover='group-hover:text-sky-500'
           quantity={comments}
+          idUser={id}
+          idTweet={idTweet}
         />
         <InteractionTweet
-          icon={<IconRepeat size={20} className='stroke-zinc-600 group-hover:stroke-green-500 transition-colors duration-300 ease-in-out' />}
+          icon={<IconRepeat size={20} color='currentColor' className='group-hover:stroke-green-500 transition-colors duration-300 ease-in-out' />}
           bgColor='group-hover:before:bg-green-500/10'
-          textColor='group-hover:text-green-500'
+          textColor='text-green-500'
+          textColorHover='group-hover:text-green-500'
           quantity={retuits}
+          idUser={id}
+          idTweet={idTweet}
+          data={dataRetweets}
+          insertData={insertRetweet}
+          deleteData={deleteRetweet}
         />
         <InteractionTweet
-          icon={<IconHeart size={20} className='stroke-zinc-600 group-hover:stroke-pink-600 transition-colors duration-300 ease-in-out' />}
+          icon={<IconHeart className='group-hover:stroke-pink-600 size-5 stroke-2 transition-colors duration-300 ease-in-out' />}
           bgColor='group-hover:before:bg-pink-500/10'
-          textColor='group-hover:text-red-500'
+          textColor='text-pink-600'
+          textColorHover='group-hover:text-red-500'
           quantity={likes}
+          idUser={id}
+          idTweet={idTweet}
+          data={dataLikes}
+          insertData={insertLikes}
+          deleteData={deleteLikes}
         />
       </div>
       <div className='flex w-1/4 justify-end'>
         <InteractionTweet
-          icon={<IconBookmark size={20} className='stroke-zinc-600 group-hover:stroke-sky-500 transition-colors duration-300 ease-in-out' />}
+          icon={<IconBookmark size={20} className='group-hover:stroke-sky-500 transition-colors duration-300 ease-in-out' />}
           bgColor='group-hover:before:bg-sky-500/10'
-          textColor='group-hover:text-sky-500'
+          textColor='text-sky-500'
+          textColorHover='group-hover:text-sky-500'
+          idUser={id}
+          idTweet={idTweet}
         />
       </div>
     </div>
