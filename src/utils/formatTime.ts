8c84d70f5ec9105829384e0b09c833b150ad47ba
Crisops <crialeperez1835@gmail.com>
2025-01-11 17:33:51 -0500

    refactor: change locale in format time of tweet

--- src/utils/formatTime.ts
+++ src/utils/formatTime.ts
@@ -9,7 +9,7 @@ export const formatTimeTweet = (time: string): string => {
   const minutes = Math.abs(diffMinutes(now, time, 'floor'))
   const hours = Math.abs(diffHours(now, time, 'floor'))
 
-  currentTime = (seconds <= 60) ? `${seconds} seg` : (minutes <= 60) ? `${minutes}min` : (hours < 24) ? `${hours}h` : format(time, 'D MMM', 'en')
+  currentTime = (seconds <= 60) ? `${seconds} seg` : (minutes <= 60) ? `${minutes}min` : (hours < 24) ? `${hours}h` : format(time, 'D MMM', 'es')
 
   return currentTime
 }
