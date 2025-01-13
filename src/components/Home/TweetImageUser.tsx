e1a2dd1f9a5ef574a1a66262ccff9e0d698962f6
Crisops <crialeperez1835@gmail.com>
2025-01-12 20:44:31 -0500

    refactor: change of type data in the interface and addition of some styles

--- src/components/Home/TweetImageUser.tsx
+++ src/components/Home/TweetImageUser.tsx
@@ -1,9 +1,10 @@
+import { Tables } from '@/types/database.types'
 import Image from 'next/image'
 
 interface TweetImageUserProps {
-  avatar_url: string
-  name: string
-  username: string
+  avatar_url: Tables<'users'>['avatar_url']
+  name: Tables<'users'>['name']
+  username: Tables<'users'>['username']
   width?: number
   height?: number
 }
@@ -11,7 +12,7 @@ interface TweetImageUserProps {
 export default async function TweetImageUser ({ avatar_url: avatar, name, username, width = 40, height = 40 }: TweetImageUserProps) {
   return (
     <div>
-      <Image className='rounded-full' src={avatar ?? ''} width={width} height={height} alt={`${name} - @${username}`} />
+      <Image className='rounded-full transition-all duration-150 ease-out cursor-pointer hover:brightness-90' src={avatar ?? ''} width={width} height={height} alt={`${name} - @${username}`} />
     </div>
   )
 }
