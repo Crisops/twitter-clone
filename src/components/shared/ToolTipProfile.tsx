7977e980ddc3e8a8cff9b6b646b017199220bef6
Crisops <crialeperez1835@gmail.com>
2025-01-14 12:20:04 -0500

    refactor: add props in ToolTipProfile with id of the logged in user and id of the user creating the tweet

--- src/components/shared/ToolTipProfile.tsx
+++ src/components/shared/ToolTipProfile.tsx
@@ -2,9 +2,11 @@ import { Tables } from '@/types/database.types'
 import { Tooltip } from '@nextui-org/react'
 import { ReactNode } from 'react'
 import CardProfile from './CardProfile'
+import { getSessionAuth } from '@/utils/supabase/getUser'
 
 interface ToolTipProfileProps {
-    name: Tables<'users'>['id']
+    id: Tables<'users'>['id']
+    name: Tables<'users'>['name']
     username: Tables<'users'>['username']
     src: Tables<'users'>['avatar_url']
     biography: Tables<'users'>['biography']
@@ -13,14 +15,16 @@ interface ToolTipProfileProps {
     children: ReactNode
 }
 
-export default function ToolTipProfile ({ name, username, src, biography, following, followers, children }:ToolTipProfileProps) {
+export default async function ToolTipProfile ({ id: idUserCreatorTweet, name, username, src, biography, following, followers, children }:ToolTipProfileProps) {
+  const { id: idSession } = await getSessionAuth()
+
   return (
     <Tooltip
       delay={800}
       closeDelay={500}
       placement='bottom'
       classNames={{ content: 'bg-black p-1 shadow-[0px_0px_10px_#676767]' }}
-      content={<CardProfile name={name} username={username} src={src} biography={biography} following={following} followers={followers} />}
+      content={<CardProfile ids={[idSession, idUserCreatorTweet]} name={name} username={username} src={src} biography={biography} following={following} followers={followers} />}
     >
       <div className='h-max'>
         {children}
