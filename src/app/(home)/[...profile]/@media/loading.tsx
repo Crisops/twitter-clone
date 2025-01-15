9cc7c0fa74850ca702f4897da1f2729f6bdf2024
Crisops <crialeperez1835@gmail.com>
2025-01-14 19:30:23 -0500

    refactor: move [[...profile]] layout and pages into [username] folder

--- src/app/(home)/[...profile]/@media/loading.tsx
+++
@@ -1,7 +0,0 @@
-import LoadingSpinner from '@/components/shared/LoadingSpinner'
-
-export default function Loading () {
-  return (
-    <LoadingSpinner />
-  )
-}
--- src/app/(home)/[...profile]/@media/page.tsx
+++
@@ -1,5 +0,0 @@
-export default function MediaPage () {
-  return (
-    <div className='text-white'>ruta parallet media</div>
-  )
-}
