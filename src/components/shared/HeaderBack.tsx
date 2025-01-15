a074a87bac0b17aae99ea03ca1ad1c57b7dfb0ed
Crisops <crialeperez1835@gmail.com>
2025-01-14 19:02:17 -0500

    feat: add HeaderBack component in shared components
    - HeaderBack is a component used to return between visited pages.

---
+++ src/components/shared/HeaderBack.tsx
@@ -0,0 +1,41 @@
+import { getUserTweetCount } from '@/utils/supabase/getTweets'
+import ButtonNavigationBack from '@/components/shared/ButtonNavigationBack'
+import { Tables } from '@/types/database.types'
+import { ReactNode } from 'react'
+
+interface HeaderBackProps {
+  id?: Tables<'users'>['id']
+  name: Tables<'users'>['name']
+  username?: Tables<'users'>['username']
+  children?: ReactNode
+}
+
+async function HeaderBack ({ id, name, username, children }: HeaderBackProps) {
+  let quantity = 0
+
+  if (id) {
+    const { count } = await getUserTweetCount({ id })
+    quantity = count
+  }
+
+  return (
+    <div className={`hidden min-[500px]:block sticky top-0 w-full h-max px-2 ${children ? '' : ''} border-b border-zinc-900 z-10 backdrop-blur-md bg-black/60`}>
+      <div className='flex items-center'>
+        <div className='flex items-center w-16 h-full'>
+          <ButtonNavigationBack />
+        </div>
+        <div className='flex-grow flex flex-col justify-center'>
+          <div>
+            <span className='text-xl font-bold text-gray-100'>{name}</span>
+          </div>
+          <div>
+            <span className='text-sm font-normal text-zinc-500'>{id ? `${quantity} posts` : `@${username}`}</span>
+          </div>
+        </div>
+      </div>
+      {children}
+    </div>
+  )
+}
+
+export default HeaderBack
