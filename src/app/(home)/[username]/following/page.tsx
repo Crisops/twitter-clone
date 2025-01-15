481b3db6a61813bd9dfb6eb62043bcbf46442b5a
Crisops <crialeperez1835@gmail.com>
2025-01-14 19:22:21 -0500

    feat: add followers page and add HeaderBackProfile component.
    HeaderBackProfile provides the user's name and username and the interactivity to navigate to followers or people you are following.

---
+++ src/app/(home)/[username]/following/page.tsx
@@ -0,0 +1,16 @@
+import HeaderBackProfile from '@/components/Followers/HeaderBackProfile'
+import { Tables } from '@/types/database.types'
+
+interface FollowersPageProps {
+  params: {
+    username: Tables<'users'>['username']
+  }
+}
+
+export default function FollowingPage ({ params: { username } }:FollowersPageProps) {
+  return (
+    <div className='w-full h-full'>
+      <HeaderBackProfile username={username} />
+    </div>
+  )
+}
