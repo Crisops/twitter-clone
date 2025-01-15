5447b2a1c82461acf5d26927cc78557bdd27ddb3
Crisops <crialeperez1835@gmail.com>
2025-01-14 19:24:13 -0500

    feat: add HeaderBackProfile component for user profile header

---
+++ src/components/Followers/HeaderBackProfile.tsx
@@ -0,0 +1,20 @@
+import { getUserProfileByUsername } from '@/utils/supabase/getUser'
+import HeaderBack from '@/components/shared/HeaderBack'
+import { Tables } from '@/types/database.types'
+import ButtonInteractivity from '@/components/shared/ButtonInteractivity'
+
+interface HeaderBackProfileProps{
+  username: Tables<'users'>['username']
+}
+
+export default async function HeaderBackProfile ({ username }:HeaderBackProfileProps) {
+  const { name } = await getUserProfileByUsername({ username })
+  return (
+    <HeaderBack name={name} username={username}>
+      <div className='flex [&>div]:flex-grow [&>div]:flex [&>div]:justify-center [&>div]:items-center [&>div>button]:w-full [&>div>button]:h-full'>
+        <ButtonInteractivity href={`/${username}/followers`} text='Seguidores' />
+        <ButtonInteractivity href={`/${username}/following`} text='Siguiendo' />
+      </div>
+    </HeaderBack>
+  )
+}
