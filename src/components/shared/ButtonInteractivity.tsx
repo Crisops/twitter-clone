1f2c13b06b977c0d0ecfeb3c67209af480f87c87
Crisops <crialeperez1835@gmail.com>
2025-01-14 19:03:25 -0500

    feat: add ButtonInteractivity component in shared components.

---
+++ src/components/shared/ButtonInteractivity.tsx
@@ -0,0 +1,24 @@
+'use client'
+
+import Link from 'next/link'
+import { usePathname } from 'next/navigation'
+
+interface ButtonInteractivityProps {
+    text: string
+    href: string
+}
+
+export default function ButtonInteractivity ({ href, text }:ButtonInteractivityProps) {
+  const pathname = usePathname()
+
+  return (
+    <div className='relative'>
+      <button className='transition-colors duration-200 ease-in-out hover:bg-white/10'>
+        <Link className='block w-full h-full py-3' href={href}>
+          <span className={` font-medium ${pathname === href ? 'text-white' : 'text-zinc-500'}`}>{text}</span>
+        </Link>
+      </button>
+      <div className={`absolute w-3/4 h-1 left-1/2 bottom-0 -translate-x-1/2 rounded-full ${pathname === href ? 'bg-sky-500' : ''}`} />
+    </div>
+  )
+}
