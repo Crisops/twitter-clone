cf4fb13a9b3384bf601e792a365c07748d200a9a
Crisops <crialeperez1835@gmail.com>
2025-01-16 13:33:53 -0500

    feat: create ViewAllFollowers component for displaying user followers
    - Added `ViewAllFollowers` component to fetch and display followers of a user.
    - Utilizes `getUserProfileByUsername` to resolve user ID from username.
    - Fetches follower data with `getFollowersUserData` using dynamic filtering and relationships.
    - Renders a list of `CardFollower` components with follower details (name, username, avatar, and biography).
    - Accepts `username`, `filterColumn`, and `columnRelation` as props for flexible filtering and relationship mapping.
    - Includes responsive container styling for dynamic layout.

---
+++ src/components/Followers/ViewAllFollowers.tsx
@@ -0,0 +1,36 @@
+import { Tables } from '@/types/database.types'
+import { getFollowersUserData } from '@/utils/supabase/getFollowers'
+import { getUserProfileByUsername } from '@/utils/supabase/getUser'
+import CardFollower from './CardFollower'
+import { TableRelationships } from '@/types/generics'
+
+interface ViewAllFollowersProps {
+    username: Tables<'users'>['username']
+    filterColumn: keyof Tables<'followers'>
+    columnRelation: TableRelationships<'followers'>
+}
+
+export default async function ViewAllFollowers ({ username, filterColumn, columnRelation }: ViewAllFollowersProps) {
+  const { id: idUser } = await getUserProfileByUsername({ username })
+  const followers = await getFollowersUserData({ idUser, filterColumn, columnRelation })
+
+  return (
+    <div className='w-full h-full'>
+      {followers.map(({ user, idFollower, idFollowing }) => (
+        <CardFollower
+          key={user.id}
+          idFollower={idFollower}
+          idFollowing={idFollowing}
+          idUser={user.id}
+          name={user.name}
+          username={user.username}
+          src={user.src}
+          biography={user.biography}
+          followers={user.followers}
+          following={user.following}
+        />
+      )
+      )}
+    </div>
+  )
+}
