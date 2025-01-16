64dff245fcd21e8a9e4669c8663c80704d038b87
Crisops <crialeperez1835@gmail.com>
2025-01-16 13:40:12 -0500

    feat: add CardFollower component for displaying user profile previews
    - Created `CardFollower` component to display a user's profile information, including name, username, avatar, biography, followers, and following count.
    - Integrated `ToolTipProfile` to provide additional user details on hover for name, username, and avatar elements.
    - Added a `ButtonFollow` component for follow/unfollow functionality, with conditional rendering for hidden state based on user relationship.
    - Utilized `@nextui-org/react` components for styling, including `Card`, `CardHeader`, and `CardBody`.
    - Included responsive and interactive features such as hover effects and dynamic tooltip content.
    
    This component enhances user interaction and is reusable in followers and following views.

---
+++ src/components/Followers/CardFollower.tsx
@@ -0,0 +1,56 @@
+import { Avatar, Card, CardBody, CardHeader } from '@nextui-org/react'
+import ButtonFollow from '@/components/shared/ButtonFollow'
+import { Tables } from '@/types/database.types'
+import Link from 'next/link'
+import ToolTipProfile from '../shared/ToolTipProfile'
+
+interface CardFollowerProps {
+  idFollower: Tables<'users'>['id']
+  idUser: Tables<'users'>['id']
+  idFollowing: Tables<'followers'>['user_id_following']
+  name: Tables<'users'>['name']
+  username: Tables<'users'>['username']
+  src: Tables<'users'>['avatar_url']
+  biography: Tables<'users'>['biography']
+  followers: Tables<'users'>['followers'];
+  following: Tables<'users'>['following'];
+}
+
+export default async function CardFollower ({ idFollower, idUser, idFollowing, name, username, src, biography, followers, following }:CardFollowerProps) {
+  const hiddenButtonFollow = idFollower === idUser
+
+  const isFollowed = idUser === idFollowing
+
+  return (
+    <Card className='w-full border-none bg-transparent' shadow='none'>
+      <CardHeader className='justify-between'>
+        <div className='flex gap-2'>
+          <ToolTipProfile id={idUser} name={name} username={username} biography={biography} followers={followers} following={following} src={src}>
+            <Avatar
+              className='hover:cursor-pointer hover:brightness-90'
+              radius='full'
+              size='md'
+              src={src ?? ''}
+            />
+          </ToolTipProfile>
+          <div className='flex flex-col items-start justify-center'>
+            <Link href={`/${username}`}>
+              <ToolTipProfile id={idUser} name={name} username={username} biography={biography} followers={followers} following={following} src={src}>
+                <h4 className='text-medium font-semibold leading-none text-gray-100 hover:underline'>{name}</h4>
+              </ToolTipProfile>
+              <ToolTipProfile id={idUser} name={name} username={username} biography={biography} followers={followers} following={following} src={src}>
+                <h5 className='text-small tracking-tight text-zinc-500'>@{username}</h5>
+              </ToolTipProfile>
+            </Link>
+          </div>
+        </div>
+        <ButtonFollow idUserSession={idFollower} idUserProfile={idUser} hiddenButtonFollow={hiddenButtonFollow} validateStateInitialFollow={isFollowed} />
+      </CardHeader>
+      <CardBody className='px-3 py-0'>
+        <p className='text-small pl-px text-default-500'>
+          {biography}
+        </p>
+      </CardBody>
+    </Card>
+  )
+}
