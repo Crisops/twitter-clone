a4698c9bb843c943a686a174d321b29c99eabd0d
Crisops <crialeperez1835@gmail.com>
2025-01-16 13:07:35 -0500

    feat: add interfaces and types to handle return data in followers

--- src/types/querys-db.ts
+++ src/types/querys-db.ts
@@ -1,4 +1,5 @@
 import { type Tables } from './database.types'
+import { TableRelationships } from './generics'
 
 export type TweetInfo = {
     id: Tables<'tweets'>['id']
@@ -33,4 +34,31 @@ export type TweetPostAndRetweet = {
     likes: Tables<'tweets'>['likes'],
     created_at: Tables<'tweets'>['created_at'],
     post_type: string
+}
+
+export interface UserFollower {
+    id: Tables<'users'>['id'];
+    src: Tables<'users'>['avatar_url'];
+    name: Tables<'users'>['name'];
+    username: Tables<'users'>['username'];
+    biography: Tables<'users'>['biography'];
+    followers: Tables<'users'>['followers'];
+    following: Tables<'users'>['following'];
+}
+
+export interface UsersFollowers {
+    idFollower: keyof Tables<'followers'>
+    idFollowing: keyof Tables<'followers'>
+    user: UserFollower;
+}
+
+export type GetFollowersPage = {
+  idUser: Tables<'users'>['id']
+  filterColumn: keyof Tables<'followers'>
+  columnRelation: TableRelationships<'followers'>
+}
+
+export type GetAllFollowersProps = {
+    follower: string[]
+    following: string[]
 }
