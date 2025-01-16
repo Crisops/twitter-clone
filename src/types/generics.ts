00dec4966805eac94b90092b1ab8a5cf678ce7cd
Crisops <crialeperez1835@gmail.com>
2025-01-16 13:18:29 -0500

    feat: add types that handle the type of action in the hook useReducer and add types that display the names of the columns that are foreign keys in my database tables

--- src/types/generics.ts
+++ src/types/generics.ts
@@ -1,7 +1,12 @@
 import { ReactNode } from 'react'
+import { Database } from './database.types'
 
 export type LinksAsideNavHome = {
     href: string,
     icon: ReactNode
     text: string
 }
+
+export type ReducerAction = { type: 'follow' | 'unfollow' };
+
+export type TableRelationships<T extends keyof Database['public']['Tables']> = Database['public']['Tables'][T]['Relationships'][number]['foreignKeyName'];
