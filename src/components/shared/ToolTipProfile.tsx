545a8c39bec4154c4c288d98691385139c3cd895
Crisops <crialeperez1835@gmail.com>
2025-01-16 13:22:33 -0500

    refactor: : remove the function that gets the user session information and remove the id alias in props and remove the array in props component CardProfile

--- src/components/shared/ToolTipProfile.tsx
+++ src/components/shared/ToolTipProfile.tsx
@@ -2,7 +2,6 @@ import { Tables } from '@/types/database.types'
 import { Tooltip } from '@nextui-org/react'
 import { ReactNode } from 'react'
 import CardProfile from './CardProfile'
-import { getSessionAuth } from '@/utils/supabase/getUser'
 
 interface ToolTipProfileProps {
     id: Tables<'users'>['id']
@@ -15,16 +14,14 @@ interface ToolTipProfileProps {
     children: ReactNode
 }
 
-export default async function ToolTipProfile ({ id: idUserCreatorTweet, name, username, src, biography, following, followers, children }:ToolTipProfileProps) {
-  const { id: idSession } = await getSessionAuth()
-
+export default async function ToolTipProfile ({ id, name, username, src, biography, following, followers, children }:ToolTipProfileProps) {
   return (
     <Tooltip
       delay={800}
       closeDelay={500}
       placement='bottom'
       classNames={{ content: 'bg-black p-1 shadow-[0px_0px_10px_#676767]' }}
-      content={<CardProfile ids={[idSession, idUserCreatorTweet]} name={name} username={username} src={src} biography={biography} following={following} followers={followers} />}
+      content={<CardProfile id={id} name={name} username={username} src={src} biography={biography} following={following} followers={followers} />}
     >
       <div className='h-max'>
         {children}
