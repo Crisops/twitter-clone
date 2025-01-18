3b4b57a31b6ecca4315fdeb2f5b1784712115c64
Crisops <crialeperez1835@gmail.com>
2025-01-18 16:42:21 -0500

    refactor: add div with padding

--- src/components/Home/TweetInteractions.tsx
+++ src/components/Home/TweetInteractions.tsx
@@ -20,52 +20,55 @@ export default async function TweetInteractions ({ idTweet, comments, retuits, l
   const dataLikes = await getLikes({ userId: id, tweetId: idTweet })
 
   return (
-    <div className='flex justify-between'>
-      <div className='flex flex-grow justify-between'>
-        <InteractionTweet
-          icon={<IconMessageCircle size={20} className='group-hover:stroke-sky-500 transition-colors duration-300 ease-in-out' />}
-          bgColor='group-hover:before:bg-sky-500/10'
-          textColor='text-sky-500'
-          textColorHover='group-hover:text-sky-500'
-          quantity={comments}
-          idUser={id}
-          idTweet={idTweet}
-        />
-        <InteractionTweet
-          icon={<IconRepeat size={20} color='currentColor' className='group-hover:stroke-green-500 transition-colors duration-300 ease-in-out' />}
-          bgColor='group-hover:before:bg-green-500/10'
-          textColor='text-green-500'
-          textColorHover='group-hover:text-green-500'
-          quantity={retuits}
-          idUser={id}
-          idTweet={idTweet}
-          data={dataRetweets}
-          insertData={insertRetweet}
-          deleteData={deleteRetweet}
-        />
-        <InteractionTweet
-          icon={<IconHeart className='group-hover:stroke-pink-600 size-5 stroke-2 transition-colors duration-300 ease-in-out' />}
-          bgColor='group-hover:before:bg-pink-500/10'
-          textColor='text-pink-600'
-          textColorHover='group-hover:text-red-500'
-          quantity={likes}
-          idUser={id}
-          idTweet={idTweet}
-          data={dataLikes}
-          insertData={insertLikes}
-          deleteData={deleteLikes}
-        />
-      </div>
-      <div className='flex w-1/4 justify-end'>
-        <InteractionTweet
-          icon={<IconBookmark size={20} className='group-hover:stroke-sky-500 transition-colors duration-300 ease-in-out' />}
-          bgColor='group-hover:before:bg-sky-500/10'
-          textColor='text-sky-500'
-          textColorHover='group-hover:text-sky-500'
-          idUser={id}
-          idTweet={idTweet}
-        />
+    <div className='pt-2'>
+      <div className='flex justify-between'>
+        <div className='flex flex-grow justify-between'>
+          <InteractionTweet
+            icon={<IconMessageCircle size={20} className='group-hover:stroke-sky-500 transition-colors duration-300 ease-in-out' />}
+            bgColor='group-hover:before:bg-sky-500/10'
+            textColor='text-sky-500'
+            textColorHover='group-hover:text-sky-500'
+            quantity={comments}
+            idUser={id}
+            idTweet={idTweet}
+          />
+          <InteractionTweet
+            icon={<IconRepeat size={20} color='currentColor' className='group-hover:stroke-green-500 transition-colors duration-300 ease-in-out' />}
+            bgColor='group-hover:before:bg-green-500/10'
+            textColor='text-green-500'
+            textColorHover='group-hover:text-green-500'
+            quantity={retuits}
+            idUser={id}
+            idTweet={idTweet}
+            data={dataRetweets}
+            insertData={insertRetweet}
+            deleteData={deleteRetweet}
+          />
+          <InteractionTweet
+            icon={<IconHeart className='group-hover:stroke-pink-600 size-5 stroke-2 transition-colors duration-300 ease-in-out' />}
+            bgColor='group-hover:before:bg-pink-500/10'
+            textColor='text-pink-600'
+            textColorHover='group-hover:text-red-500'
+            quantity={likes}
+            idUser={id}
+            idTweet={idTweet}
+            data={dataLikes}
+            insertData={insertLikes}
+            deleteData={deleteLikes}
+          />
+        </div>
+        <div className='flex w-1/4 justify-end'>
+          <InteractionTweet
+            icon={<IconBookmark size={20} className='group-hover:stroke-sky-500 transition-colors duration-300 ease-in-out' />}
+            bgColor='group-hover:before:bg-sky-500/10'
+            textColor='text-sky-500'
+            textColorHover='group-hover:text-sky-500'
+            idUser={id}
+            idTweet={idTweet}
+          />
+        </div>
       </div>
     </div>
+
   )
 }
