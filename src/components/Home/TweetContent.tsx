264a29066436ebc95c2946c7d9006ce69c197058
Crisops <crialeperez1835@gmail.com>
2025-01-19 13:49:25 -0500

    feat: add className prop to TweetContent for customizable content styles
    - Added `className` prop to allow dynamic styling of the tweet content text.
    - Updated the structure to wrap the content and image sections in a parent `div` for better layout control.
    - Ensures `TweetContent` is adaptable for viewing a single tweet or multiple tweets in different contexts.
    - Retained functionality for lazy loading and base64 image blur placeholders.

--- src/components/Home/TweetContent.tsx
+++ src/components/Home/TweetContent.tsx
@@ -5,9 +5,10 @@ import Image from 'next/image'
 interface TweetContentProps {
   content: Tables<'tweets'>['content'],
   image_url: Tables<'tweets'>['image_url']
+  className?: string
 }
 
-export default async function TweetContent ({ content, image_url: url }: TweetContentProps) {
+export default async function TweetContent ({ content, image_url: url, className }: TweetContentProps) {
   let dataUrl: string = ''
   let w: number = 0
   let h: number = 0
@@ -20,9 +21,9 @@ export default async function TweetContent ({ content, image_url: url }: TweetCo
   }
 
   return (
-    <>
+    <div className='mb-2'>
       <div className='mb-2'>
-        <p>{content}</p>
+        <p className={className}>{content}</p>
       </div>
       <div className='w-full h-full'>
         <div className='flex'>
@@ -38,6 +39,6 @@ export default async function TweetContent ({ content, image_url: url }: TweetCo
             />}
         </div>
       </div>
-    </>
+    </div>
   )
 }
