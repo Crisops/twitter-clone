1e4c573df5846f2f2a031912588ff31c51cf0e91
Crisops <crialeperez1835@gmail.com>
2025-01-20 12:31:58 -0500

    refactor: add prop idPost in interface and add prop in component  FormPostClient

--- src/components/Posts/FormPostServer.tsx
+++ src/components/Posts/FormPostServer.tsx
@@ -1,12 +1,17 @@
 import { getSessionAuth, getUserProfile } from '@/utils/supabase/getUser'
 import FormPostClient from './FormPostClient'
 import TweetImageUser from '../Home/TweetImageUser'
+import { Tables } from '@/types/database.types'
 
-export default async function FormPostServer () {
+interface FormPostServerProps {
+  idPost: Tables<'tweets'>['id']
+}
+
+export default async function FormPostServer ({ idPost }: FormPostServerProps) {
   const { id } = await getSessionAuth()
   const { name, username, avatar_url: avatar } = await getUserProfile({ id })
   return (
-    <FormPostClient idSession={id}>
+    <FormPostClient idSession={id} idTweet={idPost}>
       <TweetImageUser avatar_url={avatar} name={name} username={username} />
     </FormPostClient>
   )
