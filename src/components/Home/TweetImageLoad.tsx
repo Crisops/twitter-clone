39cd41044d062595a8912fe021bed385f3548e11
Crisops <crialeperez1835@gmail.com>
2025-01-20 12:38:48 -0500

    refactor: add validation to remove value in input file

--- src/components/Home/TweetImageLoad.tsx
+++ src/components/Home/TweetImageLoad.tsx
@@ -1,11 +1,20 @@
 import Image from 'next/image'
 import { useCreateTweet } from '@/hooks/useStore'
 import { IconX } from '@tabler/icons-react'
+import { RefObject } from 'react'
 
-export default function TweetImageLoad () {
+interface TweetImageLoadProps {
+  refFile: RefObject<HTMLInputElement>
+}
+
+export default function TweetImageLoad ({ refFile }:TweetImageLoadProps) {
   const { initialForm, setFormCreateTweet } = useCreateTweet(state => state)
 
   const handleOnClick = () => {
+    if (refFile.current) {
+      refFile.current.value = ''
+    }
+
     setFormCreateTweet({ ...initialForm, imageUrlPreview: null, file: null })
   }
 
@@ -15,7 +24,7 @@ export default function TweetImageLoad () {
         <div className='relative w-full max-h-[720px] overflow-hidden rounded-3xl'>
           <Image className='w-full h-auto object-cover object-center' src={initialForm.imageUrlPreview} alt='Image load tweet' width={550} height={450} />
           <div className='absolute flex justify-center items-center top-2 right-2'>
-            <button onClick={handleOnClick} className='p-2 rounded-full bg-slate-900/60 backdrop-blur-sm transition-colors ease-in-out hover:bg-slate-800/70'>
+            <button type='button' onClick={handleOnClick} className='p-2 rounded-full bg-slate-900/60 backdrop-blur-sm transition-colors ease-in-out hover:bg-slate-800/70'>
               <IconX color='#fff' size={17} />
             </button>
           </div>
