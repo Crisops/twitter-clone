5a549ee9402d8edfbb05e2640b8d4d12d2c837d1
Crisops <crialeperez1835@gmail.com>
2025-01-20 12:47:04 -0500

    refactor: remove watch function

--- src/hooks/useFormTweet.ts
+++ src/hooks/useFormTweet.ts
@@ -3,14 +3,13 @@ import { type FormCreateTweet } from '@/types/store'
 import { useForm } from 'react-hook-form'
 
 export const useFormTweet = () => {
-  const { register, handleSubmit, watch, formState: { errors, isSubmitting }, trigger } = useForm<FormCreateTweet>()
+  const { register, handleSubmit, formState: { errors, isSubmitting }, trigger } = useForm<FormCreateTweet>()
 
   const registerField = (name: keyof FormCreateTweet) => register(name, validationRulesTweet[name])
 
   return {
     registerField,
     handleSubmit,
-    watch,
     trigger,
     errors,
     isSubmitting
