9a8e96dcdd4bb8f62768f3d925dc93218f40b7bb
Crisops <crialeperez1835@gmail.com>
2025-01-20 12:28:29 -0500

    refactor: remove prop mode and add trigger function of React Hook Form

--- src/hooks/useFormTweet.ts
+++ src/hooks/useFormTweet.ts
@@ -1,9 +1,9 @@
 import { validationRulesTweet } from '@/lib/form-tweet'
 import { type FormCreateTweet } from '@/types/store'
-import { type Mode, useForm } from 'react-hook-form'
+import { useForm } from 'react-hook-form'
 
-export const useFormTweet = (mode?: Mode) => {
-  const { register, handleSubmit, watch, formState: { errors, isSubmitting } } = useForm<FormCreateTweet>({ mode })
+export const useFormTweet = () => {
+  const { register, handleSubmit, watch, formState: { errors, isSubmitting }, trigger } = useForm<FormCreateTweet>()
 
   const registerField = (name: keyof FormCreateTweet) => register(name, validationRulesTweet[name])
 
@@ -11,6 +11,7 @@ export const useFormTweet = (mode?: Mode) => {
     registerField,
     handleSubmit,
     watch,
+    trigger,
     errors,
     isSubmitting
   }
