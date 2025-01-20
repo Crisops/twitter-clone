c4d670a64970de87fa6d2a9dd60e0e6dbcd36650
Crisops <crialeperez1835@gmail.com>
2025-01-20 12:27:36 -0500

    refactor: add prop id in table comments

--- src/types/database.types.ts
+++ src/types/database.types.ts
@@ -13,6 +13,7 @@ export type Database = {
         Row: {
           content: string
           created_at: string
+          id: string
           image_url: string | null
           tweet_id: string
           user_id: string
@@ -20,6 +21,7 @@ export type Database = {
         Insert: {
           content: string
           created_at?: string
+          id?: string
           image_url?: string | null
           tweet_id: string
           user_id: string
@@ -27,6 +29,7 @@ export type Database = {
         Update: {
           content?: string
           created_at?: string
+          id?: string
           image_url?: string | null
           tweet_id?: string
           user_id?: string
