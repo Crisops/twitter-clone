eb984f9e3d7c215c91e5f5e9c10c2a2ba76cac60
Crisops <crialeperez1835@gmail.com>
2025-01-20 12:44:29 -0500

    feat: add trigger function to handle errors when changing the value of the text area and add useRef Hook to reference the input file.
    -Also remove the onChange function and rest operator and pass it directly in the TextAreaForm component.
    -Delete the variablevalueContent and change it to initialForm.content.

--- src/components/Home/FormTweet.Client.tsx
+++ src/components/Home/FormTweet.Client.tsx
@@ -1,6 +1,5 @@
 'use client'
 
-import { Textarea } from '@nextui-org/react'
 import InputFileTweet from './InputFileTweet'
 import TweetImageLoad from './TweetImageLoad'
 import { useFormTweet } from '@/hooks/useFormTweet'
@@ -8,8 +7,9 @@ import { useCreateTweet } from '@/hooks/useStore'
 import { uploadImage } from '@/utils/supabase/storage/uploadImage'
 import { createTweet } from '@/actions/actions'
 import { initialCreateTweetForm } from '@/config/fields-form'
-import { ChangeEvent, ReactNode } from 'react'
+import { ReactNode, useRef } from 'react'
 import { Tables } from '@/types/database.types'
+import TextAreaForm from '../shared/TextAreaForm'
 
 interface FormTweetClientProps {
   idSession: Tables<'users'>['id']
@@ -19,11 +19,8 @@ interface FormTweetClientProps {
 export default function FormTweetClient ({ idSession, children: avatarImage }: FormTweetClientProps) {
   const { initialForm, setFormCreateTweet } = useCreateTweet(state => state)
 
-  const { registerField, handleSubmit, errors, watch, isSubmitting } = useFormTweet()
-
-  const valueContent = watch('content')
-
-  const { onChange, ...rest } = registerField('content')
+  const { registerField, handleSubmit, errors, trigger, isSubmitting } = useFormTweet()
+  const inputFileRef = useRef<HTMLInputElement>(null)
 
   const handleOnSubmit = handleSubmit(async (data) => {
     const { content } = data
@@ -45,37 +42,19 @@ export default function FormTweetClient ({ idSession, children: avatarImage }: F
     setFormCreateTweet(initialCreateTweetForm)
   })
 
-  const handleOnChange = (e: ChangeEvent<HTMLInputElement>) => {
-    const { name, value } = e.target
-    setFormCreateTweet({ ...initialForm, [name]: value })
-    onChange(e)
-  }
-
   return (
     <form onSubmit={handleOnSubmit} encType='multipart/form-data' className='grid grid-cols-[max-content_1fr] grid-rows-[max-content_max_content_60px] gap-x-2'>
       <div>
         {avatarImage}
       </div>
       <div className='flex-grow'>
-        <Textarea
-          onChange={handleOnChange}
-          isInvalid={!!errors.content}
-          errorMessage={errors.content?.message}
-          {...rest}
-          value={initialForm.content}
-          disableAnimation
-          radius='none'
-          minRows={2}
-          variant='underlined'
-          placeholder='¡¿Qué está pasandó?!'
-          classNames={{ input: 'text-white text-xl placeholder:text-zinc-500 placeholder:font-normal', inputWrapper: 'border-none after:bg-transpatent' }}
-        />
+        <TextAreaForm registerField={registerField('content')} trigger={trigger} errors={errors} placeholder='¡¿Qué está pasando?!' />
       </div>
-      <TweetImageLoad />
+      <TweetImageLoad refFile={inputFileRef} />
       <div className='row-start-3 col-start-2 flex items-center justify-between px-2 py-2 border-t border-zinc-800'>
-        <InputFileTweet registerField={{ ...registerField('file') }} />
+        <InputFileTweet refFile={inputFileRef} />
         <div>
-          <button className={`text-black font-semibold py-2 px-4 rounded-full ${isSubmitting || errors.content || !valueContent?.length ? 'bg-white/40 pointer-events-none' : 'bg-slate-100'}`}>{isSubmitting ? 'Publicando...' : 'Publicar'}</button>
+          <button className={`text-black font-semibold py-2 px-4 rounded-full ${isSubmitting || errors.content || !initialForm?.content.length ? 'bg-white/40 pointer-events-none' : 'bg-slate-100'}`}>{isSubmitting ? 'Publicando...' : 'Publicar'}</button>
         </div>
       </div>
     </form>
