eec39caec6320e662f7f781357afa52cea2effad
Crisops <crialeperez1835@gmail.com>
2025-01-21 11:52:08 -0500

    fix: prevent redirection on elements with "data-no-redirect" attribute
    - Updated the redirection logic to check for the "data-no-redirect" attribute using `closest`.
    - Prevented unintended redirection when clicking on specific child elements.
    - Removed redundant parameter passing in `handleRedirect`.

--- src/components/shared/RedirectWrapperClient.tsx
+++ src/components/shared/RedirectWrapperClient.tsx
@@ -1,6 +1,6 @@
 'use client'
 
-import { ReactNode } from 'react'
+import { MouseEvent, ReactNode } from 'react'
 import { useRouter } from 'next/navigation'
 
 interface RedirectWrapperClientProps {
@@ -11,12 +11,15 @@ interface RedirectWrapperClientProps {
 export default function RedirectWrapperClient ({ slug, children }:RedirectWrapperClientProps) {
   const router = useRouter()
 
-  const handleRedirect = ({ slug }: {slug: string}) => {
+  const handleRedirect = (e: MouseEvent<HTMLDivElement, globalThis.MouseEvent>) => {
+    if ((e.target as HTMLElement).closest('[data-no-redirect]')) {
+      return
+    }
     router.push(slug)
   }
 
   return (
-    <div role='button' onClick={() => handleRedirect({ slug })} className='transition-colors duration-200 ease-in-out hover:bg-zinc-800/20'>
+    <div role='button' onClick={handleRedirect} className='transition-colors duration-200 ease-in-out hover:bg-zinc-800/20'>
       {children}
     </div>
   )
