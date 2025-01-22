f0ca008c22ec2ed02a72fa5a48edeb3a2c6d3f19
Crisops <crialeperez1835@gmail.com>
2025-01-22 18:15:52 -0500

    refactor: remove fragment and add data attribute to stop propagation events

--- src/components/Home/TweetInteractions.tsx
+++ src/components/Home/TweetInteractions.tsx
@@ -22,54 +22,52 @@ export default async function TweetInteractions ({ idTweet, comments, retuits, l
   const [IconMessageCircle, IconRepeat, IconHeart, IconBookmark] = Children.toArray(children)
 
   return (
-    <>
-      <div className='flex justify-between'>
-        <div className='flex flex-grow justify-between'>
-          <InteractionTweet
-            icon={IconMessageCircle}
-            bgColor='group-hover:before:bg-sky-500/10'
-            textColor='text-sky-500'
-            textColorHover='group-hover:text-sky-500'
-            quantity={comments}
-            idUser={id}
-            idTweet={idTweet}
-          />
-          <InteractionTweet
-            icon={IconRepeat}
-            bgColor='group-hover:before:bg-green-500/10'
-            textColor='text-green-500'
-            textColorHover='group-hover:text-green-500'
-            quantity={retuits}
-            idUser={id}
-            idTweet={idTweet}
-            data={dataRetweets}
-            insertData={insertRetweet}
-            deleteData={deleteRetweet}
-          />
-          <InteractionTweet
-            icon={IconHeart}
-            bgColor='group-hover:before:bg-pink-500/10'
-            textColor='text-pink-600'
-            textColorHover='group-hover:text-red-500'
-            quantity={likes}
-            idUser={id}
-            idTweet={idTweet}
-            data={dataLikes}
-            insertData={insertLikes}
-            deleteData={deleteLikes}
-          />
-        </div>
-        <div className='flex w-1/4 justify-end'>
-          <InteractionTweet
-            icon={IconBookmark}
-            bgColor='group-hover:before:bg-sky-500/10'
-            textColor='text-sky-500'
-            textColorHover='group-hover:text-sky-500'
-            idUser={id}
-            idTweet={idTweet}
-          />
-        </div>
+    <div className='flex justify-between' data-no-redirect>
+      <div className='flex flex-grow justify-between'>
+        <InteractionTweet
+          icon={IconMessageCircle}
+          bgColor='group-hover:before:bg-sky-500/10'
+          textColor='text-sky-500'
+          textColorHover='group-hover:text-sky-500'
+          quantity={comments}
+          idUser={id}
+          idTweet={idTweet}
+        />
+        <InteractionTweet
+          icon={IconRepeat}
+          bgColor='group-hover:before:bg-green-500/10'
+          textColor='text-green-500'
+          textColorHover='group-hover:text-green-500'
+          quantity={retuits}
+          idUser={id}
+          idTweet={idTweet}
+          data={dataRetweets}
+          insertData={insertRetweet}
+          deleteData={deleteRetweet}
+        />
+        <InteractionTweet
+          icon={IconHeart}
+          bgColor='group-hover:before:bg-pink-500/10'
+          textColor='text-pink-600'
+          textColorHover='group-hover:text-red-500'
+          quantity={likes}
+          idUser={id}
+          idTweet={idTweet}
+          data={dataLikes}
+          insertData={insertLikes}
+          deleteData={deleteLikes}
+        />
       </div>
-    </>
+      <div className='flex w-1/4 justify-end'>
+        <InteractionTweet
+          icon={IconBookmark}
+          bgColor='group-hover:before:bg-sky-500/10'
+          textColor='text-sky-500'
+          textColorHover='group-hover:text-sky-500'
+          idUser={id}
+          idTweet={idTweet}
+        />
+      </div>
+    </div>
   )
 }
