41e9702cbd087f0ab99ffa823696c8340a828349
Crisops <crialeperez1835@gmail.com>
2025-01-23 17:55:22 -0500

    style: change paddings in elements

--- src/components/Home/LinkProfileClient.tsx
+++ src/components/Home/LinkProfileClient.tsx
@@ -15,8 +15,8 @@ export default function LinkProfileClient ({ viewMovil, username }:LinkProfileCl
   return (
     <li className='group'>
       <Link href={`/${username}`} className={`w-full flex  ${viewMovil ? 'justify-start' : 'justify-end xl:justify-start'}`}>
-        <div className={` ${viewMovil ? 'w-full group-hover:bg-[#5b70831A]' : 'w-max rounded-full group-hover:bg-white/10'} py-3 pr-4 transition-colors duration-150`}>
-          <div className='flex items-center justify-start gap-5 pl-3 pr-2'>
+        <div className={` ${viewMovil ? 'w-full group-hover:bg-[#5b70831A]' : 'w-max rounded-full group-hover:bg-white/10'} p-3 transition-colors duration-150`}>
+          <div className='flex items-center justify-start gap-5'>
             <div className={`${pathname === `/${username}` || pathname === `/${username}/media` ? 'text-white ' : 'text-transparent'}`}>
               <IconUser size='1.75rem' color='white' fill='currentColor' />
             </div>
