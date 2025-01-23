a42c64733f2d2d5d9a6aa42a4954d04062c2a92c
Crisops <crialeperez1835@gmail.com>
2025-01-23 16:03:18 -0500

    feat: add function generate metadata to improve SEO on user profile page

--- src/app/(home)/[username]/[[...profile]]/page.tsx
+++ src/app/(home)/[username]/[[...profile]]/page.tsx
@@ -2,14 +2,39 @@ import HeaderBack from '@/components/shared/HeaderBack'
 import InformationProfile from '@/components/Profile/InformationProfile'
 import { Tables } from '@/types/database.types'
 import { getUserProfileByUsername } from '@/utils/supabase/getUser'
+import { Metadata } from 'next'
 
-interface ProfileLayoutProps {
+interface ProfilePageProps {
   params: {
     username: Tables<'users'>['username']
   }
 }
 
-export default async function ProfilePage ({ params: { username } }:ProfileLayoutProps) {
+export async function generateMetadata ({ params: { username } }: ProfilePageProps): Promise<Metadata> {
+  const { name, username: alias, biography, avatar_url: avatar } = await getUserProfileByUsername({ username })
+
+  return {
+    title: `${name} (@${alias}) / Twitter Clone`,
+    description: `${biography} - Twitter clone elaborado con Next Js`,
+    openGraph: {
+      type: 'profile',
+      title: `${name} (@${alias}) / Twitter Clone`,
+      description: `http://localhost:3000/${alias}`,
+      url: `http://localhost:3000/${alias}`,
+      siteName: 'Twitter Clone',
+      images: [
+        {
+          url: `${avatar}`,
+          width: 800,
+          height: 800,
+          alt: `${name} - (@${username})`
+        }
+      ]
+    }
+  }
+}
+
+export default async function ProfilePage ({ params: { username } }:ProfilePageProps) {
   const { id, username: usernameUser, biography, followers, following, location, web_site: webSite, avatar_url: avatarUrl, banner_url: bannerUrl, name, created_at: joined } = await getUserProfileByUsername({ username })
 
   return (
