5f1f495e7f4580445ecbfbc6c84fd686edfbf328
Crisops <crialeperez1835@gmail.com>
2025-01-23 11:47:16 -0500

    refactor: remove reference to tweet id and replace with table id

--- src/utils/supabase/getComments.ts
+++ src/utils/supabase/getComments.ts
@@ -5,7 +5,7 @@ import { CommentInfo } from '@/types/querys-db'
 export const getCommentsTweetById = async ({ idTweet }: {idTweet: Tables<'tweets'>['id']}): Promise<CommentInfo[]> => {
   const supabase = await createClient()
   try {
-    const { data, error } = await supabase.from('comments').select('id:tweet_id, content, image_url, created_at, creator:users!comments_user_id_fkey (id, name,username, avatar_url, biography, followers, following)').eq('tweet_id', idTweet).returns<CommentInfo[]>().order('created_at', { ascending: false })
+    const { data, error } = await supabase.from('comments').select('id, content, image_url, created_at, creator:users!comments_user_id_fkey (id, name,username, avatar_url, biography, followers, following)').eq('tweet_id', idTweet).returns<CommentInfo[]>().order('created_at', { ascending: false })
 
     if (error) throw new Error('Error, Failure to get post comments ')
 
