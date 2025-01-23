0edadd975bdb082a603c4caaa606e742e6646615
Crisops <crialeperez1835@gmail.com>
2025-01-23 17:54:41 -0500

    refactor: remove some element tags and change function 'handleSignOut'

--- src/components/Home/SignOutUser.tsx
+++ src/components/Home/SignOutUser.tsx
@@ -11,14 +11,11 @@ export default function SignOutUser ({ username }: {username: string}) {
   }
 
   return (
-    <div className='absolute -top-1 left-0 -translate-y-full w-72 h-24 bg-black shadow-md shadow-white/20 rounded-3xl'>
-      <div className='absolute bottom-0 left-1/2 -translate-x-1/2 translate-y-full rotate-180 border-r-8 border-r-transparent border-t-8 border-t-transparent border-l-8 border-l-transparent border-b-8 border-black drop-shadow-3xl' />
-      <div onClick={handleSignOut} role='button' className='absolute top-0 left-0 w-full h-full flex items-end py-2'>
-        <div className='hover:bg-slate-600/10 cursor-pointer flex-grow rounded-br-xl rounded-bl-xl'>
-          <div className='py-3 px-4'>
-            <div className='max-w-36'>
-              <span className='text-white text-base font-medium'>Cerrar la sesión de @{username}</span>
-            </div>
+    <div className='relative top-0 left-0 w-72 h-24 flex items-end py-2'>
+      <div className='hover:bg-slate-600/10 cursor-pointer flex-grow'>
+        <div onClick={handleSignOut} role='button' className='py-3 px-4'>
+          <div className='max-w-36'>
+            <span className='text-white text-base font-medium'>Cerrar la sesión de @{username}</span>
           </div>
         </div>
       </div>
