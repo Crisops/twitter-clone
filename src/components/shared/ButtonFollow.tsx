17d53b15f0d2188d73c27f66da77ff7410e0b266
Crisops <crialeperez1835@gmail.com>
2025-01-23 15:28:05 -0500

    refactor: change `useReducer` to `useOptimistic` for optimistic status handling in ButtonFollow
    - Replaced `useReducer` with `useOptimistic` to handle optimistic status updates.
    - Used `startTransition` to mark optimistic updates as non-urgent, improving user experience.
    - Simplified state handling by removing reducer logic and delegating state inversion to `updateFollowedOptimistic` function.
    - Adjusted the behavior of the optimistic state to handle server errors and revert state in case of failure.
    - Maintained the previous component design and behavior, but improved performance and UI flow.

--- src/components/shared/ButtonFollow.tsx
+++ src/components/shared/ButtonFollow.tsx
@@ -1,10 +1,9 @@
 'use client'
 
 import { Button } from '@heroui/react'
-import { useReducer } from 'react'
+import { startTransition, useOptimistic } from 'react'
 import { deleteFollowUser, insertFollowUser } from '@/actions/actions'
 import { Tables, TablesInsert } from '@/types/database.types'
-import { ReducerAction } from '@/types/generics'
 
 interface ButtonFollowProps {
     idUserSession: Tables<'users'>['id']
@@ -13,48 +12,41 @@ interface ButtonFollowProps {
     validateStateInitialFollow: boolean
 }
 
-function reducer (state: boolean, action: ReducerAction) {
-  switch (action.type) {
-    case 'follow':
-      return true
-    case 'unfollow':
-      return false
-    default:
-      return state
-  }
+function updateFollowedOptimistic (newState: boolean) {
+  return !newState
 }
 
 export default function ButtonFollow ({ idUserSession, idUserProfile, hiddenButtonFollow, validateStateInitialFollow }: ButtonFollowProps) {
-  const [isFollowed, dispatch] = useReducer(reducer, validateStateInitialFollow)
+  const [optimisticFollowed, setOptimisticFollowed] = useOptimistic(validateStateInitialFollow, updateFollowedOptimistic)
 
   const handleFollowUser = async ({ user_id_follower: idUserFollower, user_id_following: idUserFollowing }: TablesInsert<'followers'>) => {
-    if (isFollowed) {
-      const { error } = await deleteFollowUser({ user_id_follower: idUserFollower, user_id_following: idUserFollowing })
-      if (error) {
-        dispatch({ type: 'follow' })
-        return
-      }
-      dispatch({ type: 'unfollow' })
-      return
+    startTransition(() => {
+      setOptimisticFollowed(optimisticFollowed)
+    })
+
+    let error
+    if (optimisticFollowed) {
+      const response = await deleteFollowUser({ user_id_follower: idUserFollower, user_id_following: idUserFollowing })
+      error = response.error
+    } else {
+      const response = await insertFollowUser({ user_id_follower: idUserFollower, user_id_following: idUserFollowing })
+      error = response.error
     }
 
-    const { error } = await insertFollowUser({ user_id_follower: idUserFollower, user_id_following: idUserFollowing })
     if (error) {
-      dispatch({ type: 'unfollow' })
-      return
+      setOptimisticFollowed(optimisticFollowed)
     }
-    dispatch({ type: 'follow' })
   }
 
   return (
     <Button
-      className={`${hiddenButtonFollow ? 'hidden' : ''} ${isFollowed ? 'bg-transparent text-foreground border-slate-500 before:content-["Siguiendo"] before:text-white before:opacity-100 hover:before:hidden hover:before:opacity-0 hover:after:content-["Dejar_de_seguir"] after:hidden hover:after:block after:opacity-0 hover:after:opacity-100 hover:after:text-red-600 after:font-semibold hover:border-red-600' : 'bg-white text-black'} border-1 font-semibold text-base py-2 px-3`}
+      className={`${hiddenButtonFollow ? 'hidden' : ''} ${optimisticFollowed ? 'bg-transparent text-foreground border-slate-500 before:content-["Siguiendo"] before:text-white before:opacity-100 hover:before:hidden hover:before:opacity-0 hover:after:content-["Dejar_de_seguir"] after:hidden hover:after:block after:opacity-0 hover:after:opacity-100 hover:after:text-red-600 after:font-semibold hover:border-red-600' : 'bg-white text-black'} border-1 font-semibold text-base py-2 px-3`}
       radius='full'
       size='sm'
-      variant={isFollowed ? 'bordered' : 'solid'}
+      variant={optimisticFollowed ? 'bordered' : 'solid'}
       onPress={() => handleFollowUser({ user_id_follower: idUserSession, user_id_following: idUserProfile })}
     >
-      {isFollowed ? '' : 'Seguir'}
+      {optimisticFollowed ? '' : 'Seguir'}
     </Button>
   )
 }
