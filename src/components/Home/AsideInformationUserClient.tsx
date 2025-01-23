45c6934be1a6e5118c25079aec24d7d1716ee880
Crisops <crialeperez1835@gmail.com>
2025-01-23 17:58:37 -0500

    refactor: replace `useState` by `Popover` for view handling in AsideInformationUserClient
    - Removed manual state handling (`useState`) for show/hide `SignOutUser` component.
    - Integrated `Popover` component of `@heroui/react` to improve user experience when displaying menu.
    - Applied custom styles to the popover (`classNames`) to maintain visual consistency of the layout.
    - Simplified the code by removing unnecessary logic for state and event handling.
    - Retained previous functionality of the component while improving accessibility and fluidity in the UI.

--- src/components/Home/AsideInformationUserClient.tsx
+++ src/components/Home/AsideInformationUserClient.tsx
@@ -1,9 +1,8 @@
-'use client'
-
-import { ReactNode, useState } from 'react'
+import { ReactNode } from 'react'
 import { IconDots } from '@tabler/icons-react'
 import SignOutUser from './SignOutUser'
 import { Tables } from '@/types/database.types'
+import { Popover, PopoverContent, PopoverTrigger } from '@heroui/react'
 
 type AsideInformationUserProps = {
   name: Tables<'users'>['id']
@@ -12,30 +11,33 @@ type AsideInformationUserProps = {
 }
 
 export default function AsideInformationUserClient ({ name, username, children: imageUser }: AsideInformationUserProps) {
-  const [view, setView] = useState<boolean>(false)
-  const handleViewSignOut = () => {
-    setView(!view)
-  }
-
   return (
-    <div onClick={handleViewSignOut} role='button' className='relative top-0 left-0 py-3 pl-1'>
-      <div className='flex items-center justify-end xl:justify-between pl-2 py-2 pr-3 rounded-full cursor-pointer transition-colors duration-150 hover:bg-white/10'>
-        <div className='flex items-center'>
-          <div className='shrink-0'>
-            {imageUser}
-          </div>
-          <div className='ml-3 hidden xl:block'>
-            <div className='flex flex-col justify-between'>
-              <h4 className='text-white font-medium text-base max-w-36 truncate'>{name}</h4>
-              <span className='text-zinc-500 text-base font-normal'>@{username}</span>
+    <Popover placement='bottom' showArrow classNames={{ base: 'before:bg-black shadow-lg shadow-white/20 rounded-xl', content: 'bg-black' }}>
+      <PopoverTrigger>
+        <div className='relative top-0 left-0 py-3 pl-1'>
+          <div className='flex w-full'>
+            <div className='flex w-full items-center justify-end xl:justify-between rounded-full xl:cursor-pointer xl:transition-colors xl:duration-150 xl:hover:bg-white/10'>
+              <div className='flex items-center pl-2 py-2 pr-2 transition-colors duration-150 hover:bg-white/10 xl:hover:bg-transparent rounded-full hover:cursor-pointer '>
+                <div className='shrink-0'>
+                  {imageUser}
+                </div>
+                <div className='ml-3 hidden xl:block'>
+                  <div className='flex flex-col justify-between'>
+                    <h4 className='text-white font-medium text-base max-w-36 truncate'>{name}</h4>
+                    <span className='text-zinc-500 text-base font-normal'>@{username}</span>
+                  </div>
+                </div>
+              </div>
+              <div className='hidden xl:block'>
+                <IconDots size={20} color='white' />
+              </div>
             </div>
           </div>
         </div>
-        <div className='hidden xl:block'>
-          <IconDots size={20} color='white' />
-        </div>
-      </div>
-      {view && <SignOutUser username={username} />}
-    </div>
+      </PopoverTrigger>
+      <PopoverContent className='px-0 pb-0 '>
+        <SignOutUser username={username} />
+      </PopoverContent>
+    </Popover>
   )
 }
