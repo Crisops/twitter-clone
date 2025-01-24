d0b2594ff64ea771fdbc53c5b973b37d6b126140
Crisops <crialeperez1835@gmail.com>
2025-01-24 17:51:02 -0500

    style: change min hegith to h-screen viewport and add NavbarLinks movile

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -2,6 +2,7 @@ import './globals.css'
 import type { Metadata } from 'next'
 import { Providers } from './providers'
 import { ReactNode } from 'react'
+import NavbarLinks from '@/components/Movile/Home/NavbarLinks'
 
 export const metadata: Metadata = {
   title: 'X. Twitter Clone / ByCrisops',
@@ -18,10 +19,11 @@ export default function RootLayout ({
       <head>
         <script src='https://unpkg.com/react-scan/dist/auto.global.js' async />
       </head>
-      <body className='min-h-screen'>
+      <body className='h-screen'>
         <Providers>
           {children}
         </Providers>
+        <NavbarLinks />
       </body>
     </html>
   )
