26e0efe4d30056e76af7b8f7a0f7ddad41e17c0b
Crisops <crialeperez1835@gmail.com>
2025-01-29 19:41:41 -0500

    feat: add SearchPeople component with InputSearch
    - Created `SearchPeople` component to encapsulate the search input for users.
    - Integrated `InputSearch` for a consistent search experience.
    - Added responsive styling with a max width of 370px and margin adjustments.
    - Prepared for future integration with `QueryUsers` (commented for now).

---
+++ src/components/Home/SearchPeople.tsx
@@ -0,0 +1,10 @@
+import InputSearch from '@/components/shared/InputSearch'
+// import QueryUsers from './QueryUsers'
+
+export default function SearchPeople () {
+  return (
+    <div className='relative max-w-[370px] w-full ml-8 mt-1'>
+      <InputSearch />
+    </div>
+  )
+}
