9107fe324a4852cb0d0897a1270c74a63f69d120
Crisops <crialeperez1835@gmail.com>
2025-01-29 19:43:31 -0500

    refactor: add SidebarInformation to display search functionality in layout

--- src/app/(home)/layout.tsx
+++ src/app/(home)/layout.tsx
@@ -1,4 +1,5 @@
 import AsideBarMenu from '@/components/Home/AsideBarMenu'
+import SidebarInformation from '@/components/Home/SidebarInformation'
 import { ReactNode } from 'react'
 
 export default function HomeLayout ({ children }: {children: ReactNode}) {
@@ -9,11 +10,7 @@ export default function HomeLayout ({ children }: {children: ReactNode}) {
           <section className='min-[500px]:grid min-[500px]:grid-cols-[max-content_1fr] sm:grid-cols-[100px_512px] md:grid-cols-[180px_512px] lg:grid-cols-[max-content_1fr_450px] min-[1200px]:grid-cols-[150px_1fr_450px] xl:grid-cols-[270px_1fr_400px]'>
             <AsideBarMenu />
             {children}
-            <aside className='hidden lg:block sticky top-0 h-screen'>
-              <div className='h-full'>
-                3
-              </div>
-            </aside>
+            <SidebarInformation />
           </section>
         </div>
       </section>
