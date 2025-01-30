952c714580d9f97956c5f3258ee92cdfe8b40a31
Crisops <crialeperez1835@gmail.com>
2025-01-29 19:40:46 -0500

    refactor: improve InputSearch layout and responsiveness
    - Wrapped Input in a div with relative positioning for better layout control.
    - Adjusted styles to enhance responsiveness and maintain design consistency.
    - Ensured placeholder and text colors provide better contrast in dark mode.
    - Kept clear button visible only when input is filled.

--- src/components/shared/InputSearch.tsx
+++ src/components/shared/InputSearch.tsx
@@ -3,41 +3,42 @@ import { IconSearch } from '@tabler/icons-react'
 
 function InputSearch () {
   return (
-    <Input
-      isClearable
-      variant='bordered'
-      size='md'
-      classNames={{
-        input: [
-          'bg-transparent',
-          'text-black/90 dark:text-white/90',
-          'text-medium',
-          'placeholder:text-zinc-500 placeholder:font-normal'
-        ],
-        innerWrapper: 'bg-transparent',
-        inputWrapper: [
-          'border-1 border-zinc-800',
-          'shadow-xl',
-          'data-[hover=true]:border-zinc-700',
-          'h-11',
-          'bg-black',
-          'backdrop-blur-xl',
-          'backdrop-saturate-200',
-          'group-data-[focus=true]:bg-black',
-          'group-data-[focus=true]:border-sky-500',
-          'group-data-[focus=true]:border-2',
-          '!cursor-text'
-        ],
-        clearButton: [
-          'peer-data-[filled=true]:opacity-100 text-white text-2xl'
-        ]
-      }}
-      placeholder='Buscar'
-      radius='full'
-      startContent={
-        <IconSearch size={18} className='text-zinc-500 pointer-events-none flex-shrink-0' />
+    <div className='relative w-full h-full max-h-28'>
+      <Input
+        isClearable
+        variant='bordered'
+        size='md'
+        classNames={{
+          input: [
+            'bg-transparent',
+            'text-white',
+            'text-medium',
+            'placeholder:text-zinc-500 placeholder:font-normal'
+          ],
+          innerWrapper: 'bg-transparent',
+          inputWrapper: [
+            'border-1 border-zinc-800',
+            'shadow-xl',
+            'data-[hover=true]:border-zinc-700',
+            'h-11',
+            'bg-black',
+            'group-data-[focus=true]:bg-black',
+            'group-data-[focus=true]:border-sky-500',
+            'group-data-[focus=true]:border-2',
+            '!cursor-text'
+          ],
+          clearButton: [
+            'peer-data-[filled=true]:opacity-100 text-white text-2xl'
+          ]
+        }}
+        placeholder='Buscar'
+        radius='full'
+        startContent={
+          <IconSearch size={18} className='text-zinc-500 pointer-events-none flex-shrink-0' />
         }
-    />
+      />
+    </div>
+
   )
 }
 
