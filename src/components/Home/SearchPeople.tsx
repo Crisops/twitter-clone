620ef894bd658688deadcc346952ea14071923d5
Crisops <crialeperez1835@gmail.com>
2025-02-02 20:01:29 -0500

    feat: add search state management and debounce in SearchPeople
    - Added `useState` to manage `valueInput` and `searchQuery`.
    - Implemented `useDebouncedCallback` to optimize search queries.
    - Integrated `useSearchContext` to control search modal state.
    - Added `useRef` to handle input reference.
    - Passed controlled props to `InputSearch` for better state management.
    - Added QueryUsers component when Popover is active.

--- src/components/Home/SearchPeople.tsx
+++ src/components/Home/SearchPeople.tsx
@@ -1,10 +1,46 @@
+'use client'
+
 import InputSearch from '@/components/shared/InputSearch'
-// import QueryUsers from './QueryUsers'
+import { useRef, useState } from 'react'
+import QueryUsers from './QueryUsers'
+import { useDebouncedCallback } from 'use-debounce'
+import { useSearchContext } from '@/hooks/useSearchContext'
 
 export default function SearchPeople () {
+  const [valueInput, setValueInput] = useState<string>('')
+  const [searchQuery, setSearchQuery] = useState<string>('')
+
+  const { open, setOpen } = useSearchContext()
+
+  const inputRef = useRef<HTMLInputElement>(null)
+
+  const debouncedSearch = useDebouncedCallback((letter: string) => {
+    setSearchQuery(letter)
+  }, 300)
+
+  const handleFocus = () => {
+    setOpen(true)
+  }
+
+  const handleChange = (letter: string) => {
+    setValueInput(letter)
+    debouncedSearch(letter)
+  }
+
+  const handleClearInput = () => {
+    setOpen(true)
+  }
+
   return (
-    <div className='relative max-w-[370px] w-full ml-8 mt-1'>
-      <InputSearch />
+    <div className='relative max-w-[370px] w-full ml-8 mt-1' data-search>
+      <InputSearch
+        ref={inputRef}
+        handleChange={handleChange}
+        handleFocus={handleFocus}
+        handleClearInput={handleClearInput}
+        value={valueInput}
+      />
+      {open && <QueryUsers valueInput={valueInput} searchQuery={searchQuery} />}
     </div>
   )
 }
