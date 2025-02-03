5998cd9fc95bc67ef44988535aab20f0af7aca1a
Crisops <crialeperez1835@gmail.com>
2025-02-03 16:13:27 -0500

    stylle: remove overflow-y-scroll

--- src/components/Home/QueryUsers.tsx
+++ src/components/Home/QueryUsers.tsx
@@ -7,7 +7,7 @@ interface QueryUsersProps {
 
 export default function QueryUsers ({ valueInput, searchQuery }: QueryUsersProps) {
   return (
-    <div className='w-full h-full max-h-[650px] bg-black shadow-lg shadow-white/20 rounded-lg overflow-y-scroll'>
+    <div className='w-full h-full bg-black shadow-lg shadow-white/20 rounded-lg'>
       <div className={`flex ${valueInput.length > 0 ? 'justify-start items-start pt-2' : 'items-center px-8 py-4'}`}>
         {valueInput.length === 0 && <p className='text-zinc-500 text-center'>Prueba buscar personas, listas o palabras clave</p>}
         {searchQuery.length > 0 && <MatchingUsers valueInput={valueInput} searchQuery={searchQuery} />}
