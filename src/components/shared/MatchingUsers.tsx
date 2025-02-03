3b1365dbf7e2e5513d8f5980ed9a810741fe122e
Crisops <crialeperez1835@gmail.com>
2025-02-03 14:08:36 -0500

    feat: integrate dynamic user search with Supabase in the MatchingUsers component.
    - Added state management using `useState` to store matched users.
    - Implementation of `useEffect` to get users dynamically based on `searchQuery`.
    - Integrated `getUserSearch` utility to get users from Supabase.
    - Replaced static `CardSearch` component with dynamic function to get users from API response.
    - Added error handling and logging for debugging purposes.

--- src/components/shared/MatchingUsers.tsx
+++ src/components/shared/MatchingUsers.tsx
@@ -1,4 +1,6 @@
-import React from 'react'
+import { useEffect, useState } from 'react'
+import { getUserSearch } from '@/utils/supabase/getUserSearch'
+import { UsersSearch } from '@/types/querys-db'
 import CardSearch from './CardSearch'
 
 interface MatchingUsersProps {
@@ -7,11 +9,32 @@ interface MatchingUsersProps {
 }
 
 export default function MatchingUsers ({ valueInput, searchQuery }: MatchingUsersProps) {
+  const [matchingUsers, setMachingUsers] = useState<UsersSearch[]>([])
+
+  useEffect(() => {
+    const getUsersSearch = async () => {
+      const result = await getUserSearch({ query: searchQuery })
+      setMachingUsers(result)
+      console.log(result)
+    }
+
+    getUsersSearch()
+  }, [searchQuery])
+
   return (
     <section className={`${valueInput.length === 0 ? 'hidden' : 'block w-full'}`}>
       <div className='w-full h-full py-0.5'>
         <div className='flex flex-col w-full'>
-          <CardSearch username='criale306' name='Alejandro Pérez' src='https://lh3.googleusercontent.com/a/ACg8ocIdoFpBGvCUcSGrTTWxiSweEzDkSESOmJMq_fmILY2khFmfNypo=s96-c' />
+          {
+            matchingUsers.map(({ id, name, username, src }) => (
+              <CardSearch
+                key={id}
+                name={name}
+                username={username}
+                src={src}
+              />
+            ))
+          }
         </div>
       </div>
     </section>
