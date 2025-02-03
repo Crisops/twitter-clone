3d41c24390789685362f7f27114cabb9b8913127
Crisops <crialeperez1835@gmail.com>
2025-02-03 16:15:09 -0500

    refactor: update SidebarInformation to include SearchPeople with conditional visibility
    - Wrapped `SearchPeople` in a `div` with specific styling and `data-search` attribute.
    - Added `shouldHidden` prop to `SearchPeople` to enable conditional visibility.
    - Improved structure and layout consistency.

--- src/components/Home/SidebarInformation.tsx
+++ src/components/Home/SidebarInformation.tsx
@@ -4,9 +4,11 @@ import SearchPeople from './SearchPeople'
 export default function SidebarInformation () {
   return (
     <aside className='hidden lg:block sticky top-0 h-screen'>
-      <SearchProvider>
-        <SearchPeople />
-      </SearchProvider>
+      <div className='relative max-w-[370px] w-full ml-8 mt-1' data-search>
+        <SearchProvider>
+          <SearchPeople shouldHidden />
+        </SearchProvider>
+      </div>
     </aside>
   )
 }
