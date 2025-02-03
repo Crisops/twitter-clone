3c64f159e4754f03c384cd515cf709471c97d89a
Crisops <crialeperez1835@gmail.com>
2025-02-02 20:03:24 -0500

    feat: add QueryUsers component and improve search handling
    - Created `QueryUsers` component to display search results.
    - Integrated `MatchingUsers` within `QueryUsers` for dynamic rendering.

---
+++ src/components/Home/QueryUsers.tsx
@@ -0,0 +1,17 @@
+import MatchingUsers from '@/components/shared/MatchingUsers'
+
+interface QueryUsersProps {
+  valueInput: string
+  searchQuery: string
+}
+
+export default function QueryUsers ({ valueInput, searchQuery }: QueryUsersProps) {
+  return (
+    <div className='w-full h-full min-h-24 max-h-52 bg-black shadow-lg shadow-white/20 rounded-md'>
+      <div className={`flex ${valueInput.length > 0 ? 'justify-start items-start pt-2' : 'items-center px-8 py-4'}`}>
+        {valueInput.length === 0 && <p className='text-zinc-500 text-center'>Prueba buscar personas, listas o palabras clave</p>}
+        {searchQuery.length > 0 && <MatchingUsers valueInput={valueInput} searchQuery={searchQuery} />}
+      </div>
+    </div>
+  )
+}
