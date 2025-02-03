fd2b7c49280f16c370a6125b755f4f60a7bb7a6f
Crisops <crialeperez1835@gmail.com>
2025-02-03 16:17:40 -0500

    feat: enhance ExplorePage with HeaderDesktop and SearchProvider
    - Added `HeaderDesktop` component to the `ExplorePage`.
    - Wrapped `HeaderDesktop` with `SearchProvider` to enable search functionality.

--- src/app/(home)/explore/page.tsx
+++ src/app/(home)/explore/page.tsx
@@ -1,6 +1,13 @@
+import HeaderDesktop from '@/components/Search/Desktop/HeaderDesktop'
+import { SearchProvider } from '@/context/SearchContext'
+
 function ExplorePage () {
   return (
-    <div>Explore</div>
+    <div className='w-full h-full border-r border-zinc-700'>
+      <SearchProvider>
+        <HeaderDesktop />
+      </SearchProvider>
+    </div>
   )
 }
 
