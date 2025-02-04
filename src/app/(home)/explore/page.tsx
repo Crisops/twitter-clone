e38b8dba59ee35ceb5c9f858b5b45e9c8b5d13df
Crisops <crialeperez1835@gmail.com>
2025-02-03 20:44:42 -0500

    feat: add HeaderMovile to ExplorePage to display the correct header when the user is on mobile or computer

--- src/app/(home)/explore/page.tsx
+++ src/app/(home)/explore/page.tsx
@@ -1,4 +1,5 @@
 import HeaderDesktop from '@/components/Search/Desktop/HeaderDesktop'
+import HeaderMovile from '@/components/Search/Movile/HeaderMovile'
 import { SearchProvider } from '@/context/SearchContext'
 
 function ExplorePage () {
@@ -6,6 +7,7 @@ function ExplorePage () {
     <div className='w-full h-full border-r border-zinc-700'>
       <SearchProvider>
         <HeaderDesktop />
+        <HeaderMovile />
       </SearchProvider>
     </div>
   )
