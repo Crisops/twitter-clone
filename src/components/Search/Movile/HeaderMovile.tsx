92df128ec364b1e104e828db1f48dc6bea68bc3d
Crisops <crialeperez1835@gmail.com>
2025-02-03 20:26:40 -0500

    feat: implement mobile header with DrawerServer and SearchPeople
    - Added `HeaderMovile` component for mobile view.
    - Integrated `DrawerServer` and `SearchPeople` components into the layout.
    - Styled the header with flexbox for proper alignment and spacing.
    - Ensured the header is hidden on larger screens (`min-[500px]:hidden`).

---
+++ src/components/Search/Movile/HeaderMovile.tsx
@@ -0,0 +1,15 @@
+import SearchPeople from '@/components/Home/SearchPeople'
+import DrawerServer from '@/components/Movile/Home/DrawerServer'
+
+export default function HeaderMovile () {
+  return (
+    <header className='min-[500px]:hidden relative w-full h-16'>
+      <div className='flex justify-evenly items-center pt-2 px-3 gap-3'>
+        <DrawerServer />
+        <div className='w-full'>
+          <SearchPeople />
+        </div>
+      </div>
+    </header>
+  )
+}
