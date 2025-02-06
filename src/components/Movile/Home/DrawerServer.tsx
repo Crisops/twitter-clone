004d05847362161e387efa08bcf765bb8de9868c
Crisops <crialeperez1835@gmail.com>
2025-02-06 15:57:44 -0500

    refactor(DrawerServer): simplify structure by directly passing components as children
    - Refactored `DrawerServer` to directly pass components like `DrawerHeader`, `AsideNavigation`, and `TweetImageUser` as children to `DrawerClient`.
    - This change eliminates the need for wrapping components within specific props, improving readability and flexibility.

--- src/components/Movile/Home/DrawerServer.tsx
+++ src/components/Movile/Home/DrawerServer.tsx
@@ -3,22 +3,16 @@ import DrawerClient from './DrawerClient'
 import { getSessionAuth, getUserProfile } from '@/utils/supabase/getUser'
 import DrawerHeader from './DrawerHeader'
 import LinkProfileServer from '@/components/Home/LinkProfileServer'
+import AsideNavigation from '@/components/Home/AsideNavigation'
 
 async function DrawerServer () {
   const { id } = await getSessionAuth()
   const { name, username, avatar_url: avatar } = await getUserProfile({ id })
 
   return (
-    <DrawerClient
-      DrawerHeader={
-        <DrawerHeader
-          avatar={<TweetImageUser avatar_url={avatar ?? ''} name={name} username={username} width={40} height={40} />}
-          name={name}
-          username={username}
-        />
-      }
-      LinkProfile={<LinkProfileServer viewMovil />}
-    >
+    <DrawerClient>
+      <DrawerHeader avatar={<TweetImageUser avatar_url={avatar ?? ''} name={name} username={username} width={40} height={40} />} name={name} username={username} />
+      <AsideNavigation viewMovil LinkProfile={<LinkProfileServer viewMovil />} />
       <TweetImageUser avatar_url={avatar ?? ''} name={name} username={username} width={30} height={30} />
     </DrawerClient>
   )
