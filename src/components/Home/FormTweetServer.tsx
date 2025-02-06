42c20a1a66dfc5b47e42eeae38c6ee17b1b23e47
Crisops <crialeperez1835@gmail.com>
2025-02-06 15:53:20 -0500

    feat: add conditional rendering for modal and home views
    - Introduced conditional rendering in `FormTweetServer` to differentiate between 'home' and 'modal' views.
    - Based on the `viewModal` prop, either `FormTweetClient` or `FormComposePost` is rendered, with the appropriate `TweetImageUser` displayed.

--- src/components/Home/FormTweetServer.tsx
+++ src/components/Home/FormTweetServer.tsx
@@ -1,14 +1,30 @@
 import { getSessionAuth, getUserProfile } from '@/utils/supabase/getUser'
 import FormTweetClient from './FormTweet.Client'
 import TweetImageUser from './TweetImageUser'
+import FormComposePost from './FormComposePost'
 
-export default async function FormTweetServer () {
+interface FormTweetServerProps {
+  viewModal: 'modal' | 'home'
+}
+
+export default async function FormTweetServer ({ viewModal }: FormTweetServerProps) {
   const { id } = await getSessionAuth()
   const { name, username, avatar_url: avatar } = await getUserProfile({ id })
 
   return (
-    <FormTweetClient idSession={id}>
-      <TweetImageUser avatar_url={avatar ?? ''} name={name} username={username} />
-    </FormTweetClient>
+    <>
+      {viewModal === 'home'
+        ? (
+          <FormTweetClient idSession={id}>
+            <TweetImageUser avatar_url={avatar ?? ''} name={name} username={username} />
+          </FormTweetClient>
+          )
+        : (
+          <FormComposePost idSession={id}>
+            <TweetImageUser avatar_url={avatar ?? ''} name={name} username={username} />
+          </FormComposePost>
+          )}
+    </>
+
   )
 }
