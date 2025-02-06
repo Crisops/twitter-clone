4eadb3b06bbb0d606608626cc928432fa877f816
Crisops <crialeperez1835@gmail.com>
2025-02-06 16:13:21 -0500

    feat(WriteTweet): pass viewModal prop to FormTweetServer
    - Added a `viewModal` prop with value `'home'` to the `FormTweetServer` component.
    - This allows customization or conditional rendering behavior inside `FormTweetServer` based on the passed `viewModal` prop.

--- src/components/Home/WriteTweet.tsx
+++ src/components/Home/WriteTweet.tsx
@@ -4,7 +4,7 @@ export default function WriteTweet () {
   return (
     <div className='hidden sm:block relative top-0 left-0 max-h-max border-b border-zinc-700'>
       <div className='px-4 pt-4 w-full'>
-        <FormTweetServer />
+        <FormTweetServer viewModal='home' />
       </div>
     </div>
   )
