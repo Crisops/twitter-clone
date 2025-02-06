ce7c1a70af8aa4396e45deefb1928751a6a6da6f
Crisops <crialeperez1835@gmail.com>
2025-02-06 16:52:03 -0500

    fix(HomeLayout): remove w-screen to prevent horizontal scroll
    - Removed the 'w-screen' class from the main section to prevent horizontal scrolling issues caused by the layout width.

--- src/app/(home)/layout.tsx
+++ src/app/(home)/layout.tsx
@@ -9,7 +9,7 @@ interface HomeLayoutProps {
 export default function HomeLayout ({ children }: HomeLayoutProps) {
   return (
     <main>
-      <section className='relative w-screen'>
+      <section className='relative'>
         <div className='mx-auto max-w-[80rem]'>
           <section className='min-[500px]:grid min-[500px]:grid-cols-[max-content_1fr] sm:grid-cols-[100px_512px] md:grid-cols-[180px_512px] lg:grid-cols-[max-content_1fr_450px] min-[1200px]:grid-cols-[150px_1fr_450px] xl:grid-cols-[270px_1fr_400px]'>
             <AsideBarMenu />
