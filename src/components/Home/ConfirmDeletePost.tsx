4ea1d34f8b3b914d87e4e7120c12f946760dcd06
Crisops <crialeperez1835@gmail.com>
2025-02-08 19:24:23 -0500

    feat: create ConfirmDeletePost component for post deletion confirmation
    - Added `ConfirmDeletePost` component with a confirmation message for post deletion.
    - Included two buttons: one for confirming the deletion (`Eliminar`) and another for canceling (`Cancelar`).
    - Styled buttons with `full` radius and different variants for better user interaction.
    - Provided detailed warning about the irreversibility of the deletion action.

---
+++ src/components/Home/ConfirmDeletePost.tsx
@@ -0,0 +1,22 @@
+import { Button } from '@heroui/react'
+
+export default function ConfirmDeletePost () {
+  return (
+    <div className='w-full h-full px-7 pb-7'>
+      <div className='flex flex-col gap-5'>
+        <div className='flex-1 gap-4'>
+          <div>
+            <h3 className='text-gray-50 font-semibold text-2xl'>¿Deseas eliminar post?</h3>
+          </div>
+          <div>
+            <p className='text-zinc-500'>Esta acción no se puede revertir, y se eliminará de tu perfil, de la cronología de las cuentas que te sigan y de los resultados de búsqueda. </p>
+          </div>
+        </div>
+        <div className='flex flex-col gap-2 w-full'>
+          <Button radius='full' variant='solid' className='w-full h-11 bg-red-600 text-white font-medium'>Eliminar</Button>
+          <Button radius='full' variant='bordered' className='w-full h-11 bg-transparent text-foreground border-1 border-slate-500 font-medium'>Cancelar</Button>
+        </div>
+      </div>
+    </div>
+  )
+}
