150f298e18c0b7f3692f5d0c8ba4b3a673c71a17
Crisops <crialeperez1835@gmail.com>
2025-02-12 18:54:03 -0500

    feat: add modal state management for tweet deletion
    - Implemented `ModalDeletePostContext` to manage modal state for deleting tweets.
    - Added `useReducer` to control modal visibility (`popover`, `modal`, or closed).
    - Integrated `handleDeletePost` with async logic using `useTransition` for smoother UI updates.
    - Improved error handling to provide clear user feedback.

---
+++ src/context/ModalDeletePostContext.tsx
@@ -0,0 +1,63 @@
+'use client'
+
+import React, { createContext, ReactNode, useReducer, useState, useTransition } from 'react'
+import { deleteTweet } from '@/actions/actions'
+import { Tables } from '@/types/database.types'
+
+type ReducerType = {
+    state: {
+      component: 'popover' | 'modal' | ''
+    }
+    action: {
+      type: 'OPEN_POPOVER' | 'OPEN_MODAL' | 'CLOSE'
+    }
+}
+
+type ModalContextType = {
+    message: string | null
+    view: ReducerType['state']
+    dispatch: React.Dispatch<ReducerType['action']>
+    handleDeletePost: ({ id, user_id }: {id: Tables<'tweets'>['id'], user_id: Tables<'users'>['id']}) => void
+    isPending: boolean
+}
+
+export const ModalDeletePostContext = createContext<ModalContextType | null>(null)
+
+function reducer (state:ReducerType['state'], action: ReducerType['action']): ReducerType['state'] {
+  switch (action.type) {
+    case 'OPEN_POPOVER':
+      return { component: 'popover' }
+    case 'OPEN_MODAL':
+      return { component: 'modal' }
+    case 'CLOSE':
+      return { component: '' }
+    default:
+      return state
+  }
+}
+
+export const ModalDeletePostProvider = ({ children }: {children: ReactNode | ReactNode[]}) => {
+  const [view, dispatch] = useReducer(reducer, { component: '' })
+  const [isPending, startTransition] = useTransition()
+  const [message, setMessageError] = useState<string | null>(null)
+
+  const handleDeletePost = ({ id: tweetId, user_id: userId }: {id: Tables<'tweets'>['id'], user_id: Tables<'users'>['id']}): void => {
+    dispatch({ type: 'CLOSE' })
+    startTransition(async () => {
+      try {
+        const { error } = await deleteTweet({ id: tweetId, user_id: userId })
+        if (error) throw new Error('Error. Algo falló al eliminar tu post')
+        setMessageError('Se eliminó tu post')
+      } catch (error: Error | unknown) {
+        const message = error instanceof Error ? error.message : 'Error desconocido'
+        setMessageError(message)
+      }
+    })
+  }
+
+  return (
+    <ModalDeletePostContext.Provider value={{ message, view, isPending, dispatch, handleDeletePost }}>
+      {children}
+    </ModalDeletePostContext.Provider>
+  )
+}
