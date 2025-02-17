fe56b6b6bc0535d5b64181b7f8c7aa15ce7ca6d6
Crisops <crialeperez1835@gmail.com>
2025-02-17 17:53:16 -0500

    feat: add InputEdit component for profile field management
    - Created reusable InputEdit component to handle dynamic form fields in the profile edit form.
    - Integrated form validation and error handling with the `useFormEditProfile` hook.
    - Connected input fields with the `useEditProfileContext` for dynamic data updates.

---
+++ src/components/Profile/InputEdit.tsx
@@ -0,0 +1,46 @@
+import { ChangeEvent } from 'react'
+import { EditableProfileFields, useFormEditProfile } from '@/hooks/useFormEditProfile'
+import { useEditProfileContext } from '@/hooks/useEditProfileContext'
+import { Input } from '@heroui/input'
+
+interface InputEditProfileProps {
+    label: string
+    defaultValue: string
+    registerName: keyof EditableProfileFields
+}
+
+export default function InputEdit ({ label, defaultValue, registerName }: InputEditProfileProps) {
+  const { initialForm, handleOnChangeData } = useEditProfileContext()
+  const { registerField, trigger, errors } = useFormEditProfile()
+  const { name, ref, onChange, ...rest } = registerField(registerName)
+
+  const valueInput = initialForm[registerName] === '' ? defaultValue : initialForm[registerName]
+
+  const handleOnChange = (e: ChangeEvent<HTMLInputElement>) => {
+    handleOnChangeData(e)
+    onChange(e)
+    trigger(name)
+  }
+  return (
+    <>
+      <Input
+        ref={ref}
+        name={name}
+        onChange={handleOnChange}
+        radius='sm'
+        variant='bordered'
+        label={label}
+        defaultValue={defaultValue}
+        value={valueInput ?? ''}
+        isInvalid={!!errors[registerName]}
+        errorMessage={errors[registerName]?.message}
+        classNames={{
+          inputWrapper: ['border-small border-zinc-700 group-data-[focus=true]:border-medium group-data-[focus=true]:border-sky-500'],
+          label: ['text-default-400 group-data-[focus-within=true]:text-sky-500']
+        }}
+        {...rest}
+      />
+    </>
+
+  )
+}
