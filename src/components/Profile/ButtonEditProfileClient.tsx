23047a39d66aecb31b54aa0a247cd74f1cb716ef
Crisops <crialeperez1835@gmail.com>
2025-02-17 17:49:20 -0500

    feat: create ButtonEditProfileClient with modal handling
    - Added `ButtonEditProfileClient` component for profile editing.
    - Integrated `useEditProfileContext` to manage modal state.
    - Implemented `ShowModal` to display edit profile form and validation modal.
    - Added `handleOpenModal`, `handleCloseModalEdit`, and `handleCloseModalValidateChange` for modal transitions.

---
+++ src/components/Profile/ButtonEditProfileClient.tsx
@@ -0,0 +1,58 @@
+'use client'
+
+import { ReactNode } from 'react'
+import { Button } from '@heroui/button'
+import { useEditProfileContext } from '@/hooks/useEditProfileContext'
+import ShowModal from '@/components/shared/ShowModal'
+import ValidateChangesEditProfile from '@/components/Profile/ValidateChangesEditProfile'
+
+interface ButtonEditProfileClientProps {
+  children: ReactNode
+}
+
+function ButtonEditProfileClient ({ children: formEditProfile }: ButtonEditProfileClientProps) {
+  const { isEqualData, openModal, dispatch } = useEditProfileContext()
+
+  const handleOpenModal = () => {
+    dispatch({ type: 'OPEN_MODAL_EDIT_PROFILE' })
+  }
+
+  const handleCloseModalEdit = () => {
+    if (isEqualData) {
+      dispatch({ type: 'CLOSE_MODAL' })
+    } else {
+      dispatch({ type: 'OPEN_MODAL_VALIDATE_CHANGES' })
+    }
+  }
+
+  const handleCloseModalValidateChange = () => {
+    dispatch({ type: 'OPEN_MODAL_EDIT_PROFILE' })
+  }
+
+  return (
+    <>
+      <Button onPress={handleOpenModal} variant='bordered' size='md' radius='full' className='bg-transparent  hover:bg-white/10 border-slate-500 text-medium'>
+        Editar Perfil
+      </Button>
+      <ShowModal
+        isOpen={(openModal.modal === 'edit_profile' && openModal.open) || openModal.modal === 'validate_changes'}
+        size='xl'
+        hideCloseButton={false}
+        placement='center'
+        handleClose={handleCloseModalEdit}
+        formTweet={formEditProfile}
+      />
+      <ShowModal
+        isOpen={openModal.modal === 'validate_changes' && openModal.open}
+        size='xs'
+        hideCloseButton={false}
+        placement='center'
+        handleClose={handleCloseModalValidateChange}
+        formTweet={<ValidateChangesEditProfile />}
+      />
+    </>
+
+  )
+}
+
+export default ButtonEditProfileClient
