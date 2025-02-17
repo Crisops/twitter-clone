9c0561a0dbe7f02401d081be4205a6c9f5819bf5
Crisops <crialeperez1835@gmail.com>
2025-02-17 17:58:12 -0500

    feat: add TextAreaBiography component for editing biography
    - Created reusable TextAreaBiography component for biography field in the profile edit form.
    - Integrated form validation and error handling using `useFormEditProfile` hook.
    - Connected textarea field with `useEditProfileContext` to manage dynamic data updates.

---
+++ src/components/Profile/TextAreaBiography.tsx
@@ -0,0 +1,38 @@
+import { ChangeEvent } from 'react'
+import { Tables } from '@/types/database.types'
+import { useFormEditProfile } from '@/hooks/useFormEditProfile'
+import { useEditProfileContext } from '@/hooks/useEditProfileContext'
+import { Textarea } from '@heroui/input'
+
+interface TextAreaBiographyProps {
+  biography: Tables<'users'>['biography']
+}
+
+export default function TextAreaBiography ({ biography }: TextAreaBiographyProps) {
+  const { registerField, trigger, errors } = useFormEditProfile()
+  const { initialForm, handleOnChangeData } = useEditProfileContext()
+  const { onChange, ...rest } = registerField('biography')
+
+  const valueInput = initialForm.biography === '' ? biography : initialForm.biography
+
+  const handleOnChange = (e: ChangeEvent<HTMLInputElement>) => {
+    onChange(e)
+    trigger('biography')
+    handleOnChangeData(e)
+  }
+
+  return (
+    <Textarea
+      onChange={handleOnChange}
+      isInvalid={!!errors.biography}
+      errorMessage={errors.biography?.message}
+      radius='sm' minRows={2} maxRows={2} variant='bordered' label='Biografía' defaultValue={biography ?? ''}
+      value={valueInput ?? ''}
+      classNames={{
+        inputWrapper: ['border-small border-zinc-700 group-data-[focus=true]:border-medium group-data-[focus=true]:border-sky-500 min-h-[82px]'],
+        label: ['text-default-400 group-data-[focus-within=true]:text-sky-500']
+      }}
+      {...rest}
+    />
+  )
+}
