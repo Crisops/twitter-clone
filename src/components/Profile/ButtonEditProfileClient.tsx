904a04c2ad0d12e6c24e83dd15d4ae7ee7647618
Crisops <crialeperez1835@gmail.com>
2025-02-18 18:06:00 -0500

    feat: add custom hook to detect the user's device and resize the modal depending on the device

--- src/components/Profile/ButtonEditProfileClient.tsx
+++ src/components/Profile/ButtonEditProfileClient.tsx
@@ -2,6 +2,7 @@
 
 import { ReactNode } from 'react'
 import { Button } from '@heroui/button'
+import { useDevice } from '@/hooks/useDevice'
 import { useEditProfileContext } from '@/hooks/useEditProfileContext'
 import ShowModal from '@/components/shared/ShowModal'
 import ValidateChangesEditProfile from '@/components/Profile/ValidateChangesEditProfile'
@@ -12,6 +13,7 @@ interface ButtonEditProfileClientProps {
 
 function ButtonEditProfileClient ({ children: formEditProfile }: ButtonEditProfileClientProps) {
   const { isEqualData, openModal, dispatch } = useEditProfileContext()
+  const { deviceType } = useDevice()
 
   const handleOpenModal = () => {
     dispatch({ type: 'OPEN_MODAL_EDIT_PROFILE' })
@@ -36,7 +38,7 @@ function ButtonEditProfileClient ({ children: formEditProfile }: ButtonEditProfi
       </Button>
       <ShowModal
         isOpen={(openModal.modal === 'edit_profile' && openModal.open) || openModal.modal === 'validate_changes'}
-        size='xl'
+        size={deviceType !== 'mobile' ? 'xl' : 'full'}
         hideCloseButton={false}
         placement='center'
         handleClose={handleCloseModalEdit}
@@ -44,7 +46,7 @@ function ButtonEditProfileClient ({ children: formEditProfile }: ButtonEditProfi
       />
       <ShowModal
         isOpen={openModal.modal === 'validate_changes' && openModal.open}
-        size='xs'
+        size={deviceType !== 'mobile' ? 'xs' : 'full'}
         hideCloseButton={false}
         placement='center'
         handleClose={handleCloseModalValidateChange}
