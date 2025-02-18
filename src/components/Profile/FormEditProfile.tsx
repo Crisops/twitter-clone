6d706b381a5ef0b00f0c6259daf05670ee71129e
Crisops <crialeperez1835@gmail.com>
2025-02-17 19:28:00 -0500

    feat: add profile update functionality in FormEditProfile
    - Integrated `updateProfile` action to handle profile updates.
    - Added `idUserSession` prop to track the user session.
    - Implemented form submission with error handling.
    - Introduced `messageError` state to display update failures.
    - Improved form control by checking if data has changed before submitting.
    - Updated `useEditProfileContext` to manage modal state on submission.

--- src/components/Profile/FormEditProfile.tsx
+++ src/components/Profile/FormEditProfile.tsx
@@ -1,41 +1,56 @@
 'use client'
 
-import { useEffect } from 'react'
+import { FormEvent, useEffect, useState } from 'react'
 import { Tables } from '@/types/database.types'
 import { Button } from '@heroui/react'
+import { updateProfile } from '@/actions/actions'
 import { useEditProfileContext } from '@/hooks/useEditProfileContext'
 import InputEdit from '@/components/Profile/InputEdit'
 import TextAreaBiography from '@/components/Profile/TextAreaBiography'
 import WrapperImagesEditProfile from '@/components/Profile/WrapperImagesEditProfile'
 
 interface FormEditProfileProps {
+  idUserSession: Tables<'users'>['id']
   name: Tables<'users'>['name']
-  avatar: Tables<'users'>['avatar_url']
-  bannerUrl: Tables<'users'>['banner_url']
+  avatar_url: Tables<'users'>['avatar_url']
+  banner_url: Tables<'users'>['banner_url']
   biography: Tables<'users'>['biography']
   location: Tables<'users'>['location']
-  webSite: Tables<'users'>['web_site']
+  web_site: Tables<'users'>['web_site']
 }
 
-export default function FormEditProfile ({ name, avatar, bannerUrl, biography, location, webSite }: FormEditProfileProps) {
-  const { updateOldData } = useEditProfileContext()
+export default function FormEditProfile ({ idUserSession, name, avatar_url: avatar, banner_url: bannerUrl, biography, location, web_site: webSite }: FormEditProfileProps) {
+  const { initialForm, isEqualData, dispatch, updateOldData } = useEditProfileContext()
+  const [messageError, setMessageError] = useState<string | null>(null)
 
   useEffect(() => {
-    updateOldData({ name, avatar, bannerUrl, biography, location, webSite })
+    updateOldData({ name, avatar_url: avatar, banner_url: bannerUrl, biography, location, web_site: webSite })
   }, [])
+
+  const onSubmit = async (e: FormEvent<HTMLFormElement>) => {
+    e.preventDefault()
+    if (isEqualData) {
+      dispatch({ type: 'CLOSE_MODAL' })
+    }
+
+    const { error } = await updateProfile(idUserSession, initialForm)
+    if (error) setMessageError('Algo fallo al momento de actualizar tu perfíl. Intentalo de nuevo')
+    dispatch({ type: 'CLOSE_MODAL' })
+  }
 
   return (
     <div className='w-full h-full'>
-      <form>
+      <form onSubmit={onSubmit}>
         <WrapperImagesEditProfile bannerUrl={bannerUrl} avatar={avatar} />
         <div className='flex flex-col gap-6 px-4 py-4'>
           <InputEdit registerName='name' label='Nombre' defaultValue={name} />
           <TextAreaBiography biography={biography} />
           <InputEdit registerName='location' label='Ubicación' defaultValue={location ?? ''} />
-          <InputEdit registerName='webSite' label='Sitio Web' defaultValue={webSite ?? ''} />
-          <Button variant='solid' radius='sm' size='lg' className='bg-white text-black font-semibold'>
+          <InputEdit registerName='web_site' label='Sitio Web' defaultValue={webSite ?? ''} />
+          <Button type='submit' variant='solid' radius='sm' size='lg' className='bg-white text-black font-semibold'>
             Guardar
           </Button>
+          {messageError && <p className='text-tiny font-bold text-red-600 text-center'>{messageError}</p>}
         </div>
       </form>
     </div>
