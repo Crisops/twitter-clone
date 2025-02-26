6629bfe3ebf3fabe7a26a14187c5dbc937b065d8
Crisops <crialeperez1835@gmail.com>
2025-02-26 13:52:03 -0500

    refactor: enhance useFormEditProfile with default values and state management
    - Added `defaultValues` from `initialFormEditProfile` to ensure form fields are pre-filled correctly.
    - Included `values` parameter to initialize the form dynamically.
    - Exposed `getValues` and `setValue` to allow better state control.
    - Improved `registerField` to handle validation rules more efficiently.

--- src/hooks/useFormEditProfile.ts
+++ src/hooks/useFormEditProfile.ts
@@ -1,13 +1,12 @@
 import { useForm } from 'react-hook-form'
-import { FormEditProfile } from '@/types/store'
+import { FormEditProfileUser } from '@/types/store'
 import { validationRulesEditProfile } from '@/lib/form-edit-profile'
+import { initialFormEditProfile } from '@/config/fields-form'
 
-export type EditableProfileFields = Omit<FormEditProfile, 'avatar' | 'bannerUrl'>;
+export const useFormEditProfile = (values?: FormEditProfileUser) => {
+  const { register, handleSubmit, trigger, formState: { errors }, getValues, setValue } = useForm<FormEditProfileUser>({ defaultValues: initialFormEditProfile, values })
 
-export const useFormEditProfile = () => {
-  const { register, handleSubmit, trigger, formState: { errors } } = useForm<EditableProfileFields>()
+  const registerField = (name: keyof FormEditProfileUser) => register(name, validationRulesEditProfile[name])
 
-  const registerField = (name: keyof EditableProfileFields) => register(name, validationRulesEditProfile[name])
-
-  return { registerField, handleSubmit, trigger, errors }
+  return { registerField, handleSubmit, trigger, getValues, setValue, errors }
 }
