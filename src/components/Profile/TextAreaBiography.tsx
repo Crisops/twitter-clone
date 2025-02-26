0fa9f235a874f0f2b9db59aa47f89181cae316fa
Crisops <crialeperez1835@gmail.com>
2025-02-26 14:01:02 -0500

    refactor: simplify TextAreaBiography component props and improve form handling
    - Removed `useEditProfileContext` and `useFormEditProfile` in favor of direct prop-based state management.
    - Now accepts `handleOnChange`, `registerField`, `trigger`, and `errors` as props for better reusability.

--- src/components/Profile/TextAreaBiography.tsx
+++ src/components/Profile/TextAreaBiography.tsx
@@ -1,31 +1,31 @@
 import { ChangeEvent } from 'react'
-import { Tables } from '@/types/database.types'
-import { useFormEditProfile } from '@/hooks/useFormEditProfile'
-import { useEditProfileContext } from '@/hooks/useEditProfileContext'
 import { Textarea } from '@heroui/input'
+import { FieldErrors, UseFormRegisterReturn, UseFormTrigger } from 'react-hook-form'
+import { FormEditProfileUser } from '@/types/store'
 
 interface TextAreaBiographyProps {
-  biography: Tables<'users'>['biography']
+    registerName: keyof FormEditProfileUser
+    errors: FieldErrors<FormEditProfileUser>
+    handleOnChange: (e: ChangeEvent<HTMLInputElement>) => void
+    registerField: (name: keyof FormEditProfileUser) => UseFormRegisterReturn<keyof FormEditProfileUser>
+    trigger: UseFormTrigger<FormEditProfileUser>
 }
 
-export default function TextAreaBiography ({ biography }: TextAreaBiographyProps) {
-  const { registerField, trigger, errors } = useFormEditProfile()
-  const { initialForm, handleOnChangeData } = useEditProfileContext()
-  const { onChange, ...rest } = registerField('biography')
+export default function TextAreaBiography ({ registerName, errors, registerField, trigger, handleOnChange }: TextAreaBiographyProps) {
+  const { onChange, ...rest } = registerField(registerName)
 
-  const handleOnChange = (e: ChangeEvent<HTMLInputElement>) => {
+  const handleOnChangeBiography = (e: ChangeEvent<HTMLInputElement>) => {
     onChange(e)
     trigger('biography')
-    handleOnChangeData(e)
+    handleOnChange(e)
   }
 
   return (
     <Textarea
-      onChange={handleOnChange}
+      onChange={handleOnChangeBiography}
       isInvalid={!!errors.biography}
       errorMessage={errors.biography?.message}
-      radius='sm' minRows={2} maxRows={2} variant='bordered' label='Biografía' defaultValue={biography ?? ''}
-      value={initialForm.biography ?? ''}
+      radius='sm' minRows={2} maxRows={2} variant='bordered' label='Biografía'
       classNames={{
         inputWrapper: ['border-small border-zinc-700 group-data-[focus=true]:border-medium group-data-[focus=true]:border-sky-500 min-h-[82px]'],
         label: ['text-default-400 group-data-[focus-within=true]:text-sky-500']
