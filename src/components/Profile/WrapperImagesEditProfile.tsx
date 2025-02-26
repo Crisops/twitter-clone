50f3c2d94bd038c83956d617b5679c4a4b6a6b19
Crisops <crialeperez1835@gmail.com>
2025-02-26 14:01:59 -0500

    refactor: integrate UpdateImageProfile component in WrapperImagesEditProfile
    - Replaced inline image update logic with `UpdateImageProfile` for better reusability.
    - Added `handleOnChange` prop to handle image updates dynamically.
    - Adjusted avatar and banner rendering to improve UI consistency.
    - Updated styles for better appearance and maintainability.

--- src/components/Profile/WrapperImagesEditProfile.tsx
+++ src/components/Profile/WrapperImagesEditProfile.tsx
@@ -1,31 +1,38 @@
+import { ChangeEvent } from 'react'
 import { Tables } from '@/types/database.types'
 import Image from 'next/image'
+import UpdateImageProfile from '@/components/Profile/UpdateImageProfile'
 
 interface WrapperImagesEditProfileProps {
-    bannerUrl: Tables<'users'>['banner_url']
-    avatar: Tables<'users'>['avatar_url'] | undefined
+  handleOnChange: (e: ChangeEvent<HTMLInputElement>) => void
+  avatar: Tables<'users'>['avatar_url']
+  banner: Tables<'users'>['banner_url']
 }
 
-export default function WrapperImagesEditProfile ({ bannerUrl, avatar }:WrapperImagesEditProfileProps) {
+export default function WrapperImagesEditProfile ({ handleOnChange, avatar, banner }:WrapperImagesEditProfileProps) {
   return (
     <div className='relative w-full h-64'>
       <div className='absolute top-0 left-0 w-full h-48'>
-        {bannerUrl ? <Image className='max-w-full h-full object-cover' src={bannerUrl} width={750} height={128} alt='Banner' /> : <div className='w-full h-48 bg-zinc-800' />}
+        <UpdateImageProfile handleOnChange={handleOnChange} registerName='banner_url' />
+        {banner
+          ? <Image className='max-w-full h-full object-cover' src={banner} width={750} height={128} alt='Banner' />
+          : <div className='w-full h-48 bg-black' />}
       </div>
-      <div className='absolute left-0 bottom-0 w-full h-32 z-[2]'>
-        <div className='ml-4 w-32 h-full rounded-full bg-zinc-950 outline -outline-offset-2 outline-4 outline-black hover:brightness-90 overflow-hidden'>
-          <div>
-            {
+      <div className='absolute left-0 bottom-0 w-full h-28 z-[2]'>
+        <div className='relative ml-4 w-28 h-full rounded-full bg-zinc-950 outline -outline-offset-2 outline-4 outline-black overflow-hidden'>
+          {
             avatar &&
-              <Image
-                className='object-cover'
-                src={avatar}
-                width={128}
-                height={128}
-                alt='Avatar User Profile'
-              />
+              <>
+                <Image
+                  className='object-cover brightness-50'
+                  src={avatar}
+                  width={112}
+                  height={112}
+                  alt='Avatar User Profile'
+                />
+                <UpdateImageProfile handleOnChange={handleOnChange} registerName='avatar_url' />
+              </>
             }
-          </div>
         </div>
       </div>
     </div>
