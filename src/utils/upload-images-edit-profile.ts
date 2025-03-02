1fd30dcece4a9e57e165175d03100cefebd14f51
Crisops <crialeperez1835@gmail.com>
2025-03-02 18:46:46 -0500

    refactor: simplify upload logic for profile images
    - Extracted `uploadImageEditProfile` to handle individual image uploads for avatars and banners.
    - Removed dependency on `FormEditProfileUser` type, making the function more reusable.
    - Standardized folder structure using dynamic `folder` parameter.
    - Maintained error handling consistency.

--- src/utils/upload-images-edit-profile.ts
+++ src/utils/upload-images-edit-profile.ts
@@ -1,29 +1,11 @@
-import { FormEditProfileUser } from '@/types/store'
-import { uploadImage } from '@/utils/supabase/storage/uploadImage'
 import { Tables } from '@/types/database.types'
+import { uploadImage } from '@/utils/supabase/storage/uploadImage'
 
-type UploadImagesEditProfileOutPut = Omit<FormEditProfileUser, 'name' | 'biography' | 'location' | 'web_site'>
-
-export async function uploadImagesEditProfile (images: (File | null)[], idUserSession: Tables<'users'>['id']): Promise<UploadImagesEditProfileOutPut> {
-  const [avatar, banner] = images
-  const imagesUrls: UploadImagesEditProfileOutPut = {
-    avatar_url: '',
-    banner_url: ''
-  }
-
+export async function uploadImageEditProfile (image: File, idUserSession: Tables<'users'>['id'], folder: 'avatars' | 'banners'): Promise<string> {
   try {
-    if (avatar) {
-      const { imageUrl, error } = await uploadImage({ bucket: 'avatars', file: avatar, folder: `${idUserSession}/avatars` })
-      if (error) throw new Error(error)
-      imagesUrls.avatar_url = imageUrl
-    }
-    if (banner) {
-      const { imageUrl, error } = await uploadImage({ bucket: 'avatars', file: banner, folder: `${idUserSession}/banners` })
-      if (error) throw new Error(error)
-      imagesUrls.banner_url = imageUrl
-    }
-
-    return imagesUrls
+    const { imageUrl, error } = await uploadImage({ bucket: 'avatars', file: image, folder: `${idUserSession}/${folder}` })
+    if (error) throw new Error(error)
+    return imageUrl
   } catch (error) {
     throw new Error(`${(error as Error).message}`)
   }
