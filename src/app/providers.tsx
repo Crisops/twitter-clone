5b1a2e5a60eb635aa029d8903c43bfef963239d7
Crisops <crialeperez1835@gmail.com>
2025-03-02 19:26:20 -0500

    chore: add context for managing toasts in application

--- src/app/providers.tsx
+++ src/app/providers.tsx
@@ -1,11 +1,13 @@
 'use client'
 
-import { HeroUIProvider } from '@heroui/react'
 import { ReactNode } from 'react'
+import { HeroUIProvider } from '@heroui/react'
+import { ToastProvider } from '@heroui/toast'
 
 export function Providers ({ children }: { children: ReactNode }) {
   return (
     <HeroUIProvider>
+      <ToastProvider />
       {children}
     </HeroUIProvider>
   )
