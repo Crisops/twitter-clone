0401ea363056857973c577dcd3430ec4dbd5457a
Crisops <crialeperez1835@gmail.com>
2025-03-08 15:07:42 -0500

    refactor: wrap FormTweetServer with ModalProvider in WriteTweet
    - Replaced `viewModal='home'` with `loadingForm='create-post'` for better clarity.
    - Wrapped `FormTweetServer` inside `ModalProvider` for improved modal state management.
    - Updated class structure for better layout and styling.

--- src/components/Home/WriteTweet.tsx
+++ src/components/Home/WriteTweet.tsx
@@ -1,10 +1,13 @@
 import FormTweetServer from '@/components/Home/FormTweetServer'
+import { ModalProvider } from '@/context/ModalComposeContext'
 
 export default function WriteTweet () {
   return (
     <div className='hidden sm:block relative top-0 left-0 max-h-max border-b border-zinc-700'>
-      <div className='px-4 pt-4 w-full'>
-        <FormTweetServer viewModal='home' />
+      <div className='w-full'>
+        <ModalProvider>
+          <FormTweetServer className='flex flex-col w-full h-full px-4 pt-4' loadingForm='create-post' />
+        </ModalProvider>
       </div>
     </div>
   )
