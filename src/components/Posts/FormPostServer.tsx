d20e9c1503c066e362f77a4c0b58d8aa7c486cc7
Crisops <crialeperez1835@gmail.com>
2025-03-08 17:41:21 -0500

    feat: add tweet details to FormPostServer
    - Integrated `getTweetById` to fetch tweet details (content, date, creator).
    - Passed `creatorTweet`, `contentTweet`, and `dateTweet` props to `FormPostClient`.
    - Enhanced `FormPostServer` to provide more context when replying to a tweet.

--- src/components/Posts/FormPostServer.tsx
+++ src/components/Posts/FormPostServer.tsx
@@ -3,6 +3,7 @@ import { Tables } from '@/types/database.types'
 import { getSessionAuth, getUserProfile } from '@/utils/supabase/getUser'
 import FormPostClient from '@/components/Posts/FormPostClient'
 import TweetImageUser from '@/components/Home/TweetImageUser'
+import { getTweetById } from '@/utils/supabase/getTweets'
 
 interface FormPostServerProps {
   className: HTMLProps<HTMLElement>['className']
@@ -12,8 +13,10 @@ interface FormPostServerProps {
 export default async function FormPostServer ({ idPost, className }: FormPostServerProps) {
   const { id } = await getSessionAuth()
   const { name, username, avatar_url: avatar } = await getUserProfile({ id })
+  const { content, created_at: date, creator } = await getTweetById({ idPost })
+
   return (
-    <FormPostClient idSession={id} idTweet={idPost} className={className} replyingUser={username}>
+    <FormPostClient idSession={id} idTweet={idPost} className={className} creatorTweet={creator} contentTweet={content} dateTweet={date}>
       <TweetImageUser avatar_url={avatar} name={name} username={username} />
     </FormPostClient>
   )
