e26e8faf4dc59bc0521a2d5551e19d4f80c3c5da
Crisops <crialeperez1835@gmail.com>
2025-03-08 15:13:47 -0500

    refactor: decouple TweetImageLoad from Zustand store
    - Removed Zustand dependency (`useStoreHook`) and replaced it with explicit props.
    - Introduced `handleRemoveImage` prop to manage image removal externally.
    - Updated button to use `@heroui/button` for improved styling and accessibility.
    - Kept `Image` rendering logic intact while simplifying component state management.

--- src/components/Home/TweetImageLoad.tsx
+++ src/components/Home/TweetImageLoad.tsx
@@ -1,33 +1,22 @@
-import { RefObject } from 'react'
-import { StoreApi, UseBoundStore } from 'zustand'
-import { StoreCreateTweet } from '@/types/store'
 import { IconX } from '@tabler/icons-react'
 import Image from 'next/image'
+import { FormCreateTweet } from '@/types/store'
+import { Button } from '@heroui/button'
 
 interface TweetImageLoadProps {
-  refFile: RefObject<HTMLInputElement>
-  useStoreHook: UseBoundStore<StoreApi<StoreCreateTweet>>
+  handleRemoveImage: () => void
+  imagePreview: FormCreateTweet['imageUrlPreview']
 }
 
-export default function TweetImageLoad ({ refFile, useStoreHook }:TweetImageLoadProps) {
-  const { initialForm, setFormCreateTweet } = useStoreHook(state => state)
-
-  const handleOnClick = () => {
-    if (refFile.current) {
-      refFile.current.value = ''
-    }
-
-    setFormCreateTweet({ ...initialForm, imageUrlPreview: null, file: null })
-  }
-
+export default function TweetImageLoad ({ imagePreview, handleRemoveImage }:TweetImageLoadProps) {
   return (
-    <div className={`row-start-2 col-start-2 px-2 ${initialForm.file ? 'my-2' : ''}`}>
+    <div className={`row-start-2 col-start-2 px-2 ${imagePreview ? 'my-2' : ''}`}>
       <div className='relative w-full max-h-[720px] overflow-hidden rounded-3xl'>
-        <Image className='w-full h-auto object-cover object-center' src={initialForm.imageUrlPreview ?? ''} alt='Image load tweet' width={550} height={450} />
+        <Image className='w-full h-auto object-cover object-center' src={imagePreview ?? ''} alt='Image load tweet' width={550} height={450} />
         <div className='absolute flex justify-center items-center top-2 right-2'>
-          <button type='button' onClick={handleOnClick} className='p-2 rounded-full bg-slate-900/60 backdrop-blur-sm transition-colors ease-in-out hover:bg-slate-800/70'>
+          <Button type='button' onPress={handleRemoveImage} isIconOnly variant='solid' radius='full' className='p-2 bg-slate-900/60 backdrop-blur-sm transition-colors ease-in-out hover:bg-slate-800/70'>
             <IconX color='#fff' size={17} />
-          </button>
+          </Button>
         </div>
       </div>
     </div>
