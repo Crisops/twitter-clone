139eaf88e3160581de59ee1d9651d6e26a1ad61b
Crisops <crialeperez1835@gmail.com>
2025-03-08 15:10:25 -0500

    refactor: replace manual file input with HeroUI Input in InputFileTweet
    - Removed `useId` and `refFile`, replacing them with React Hook Form's `register`.
    - Used `@heroui/react` `Input` component for better accessibility and styling.
    - Updated `handleFileChange` to integrate with both React Hook Form and custom handler.
    - Improved class names for a cleaner UI.

--- src/components/Home/InputFileTweet.tsx
+++ src/components/Home/InputFileTweet.tsx
@@ -1,44 +1,34 @@
-import { ChangeEvent, RefObject, useId } from 'react'
+import { ChangeEvent } from 'react'
 import { IconPhoto } from '@tabler/icons-react'
-import { StoreCreateTweet } from '@/types/store'
-import { StoreApi, UseBoundStore } from 'zustand'
+import { Input } from '@heroui/react'
+import { FormCreateTweet } from '@/types/store'
+import { UseFormRegisterReturn } from 'react-hook-form'
 
 interface InputFileTweetProps {
-  refFile: RefObject<HTMLInputElement>
-  useStoreHook: UseBoundStore<StoreApi<StoreCreateTweet>>
+  handleOnChange: (e: ChangeEvent<HTMLInputElement>) => void
+  register: (name: keyof FormCreateTweet) => UseFormRegisterReturn<keyof FormCreateTweet>
+  registerName: keyof FormCreateTweet
 }
 
-export default function InputFileTweet ({ refFile, useStoreHook }: InputFileTweetProps) {
-  const { initialForm, setFormCreateTweet } = useStoreHook(state => state)
-
-  const idFile = useId()
+export default function InputFileTweet ({ handleOnChange, register, registerName }: InputFileTweetProps) {
+  const { onChange, ...rest } = register(registerName)
 
   const handleFileChange = (e: ChangeEvent<HTMLInputElement>) => {
-    const { files } = e.target
-
-    if (files?.length) {
-      const [file] = files
-      const imageUrlPreview = file ? URL.createObjectURL(file) : null
-      setFormCreateTweet({ ...initialForm, file, imageUrlPreview })
-    }
+    onChange(e)
+    handleOnChange(e)
   }
 
   return (
     <>
-      <label
-        htmlFor={idFile}
-        title='Multimedia'
-        className='cursor-pointer'
-      >
-        <IconPhoto size={18} color='#1d9bf0' />
-      </label>
-      <input
-        id={idFile}
-        ref={refFile}
-        name='file'
+      <Input
         type='file'
-        className='hidden'
+        accept='image/*'
+        disableAnimation
+        className='w-12 h-12 cursor-pointer'
+        classNames={{ inputWrapper: ['bg-transparent data-[hover=true]:bg-transparent !cursor-pointer group-data-[focus=true]:bg-sky-500/5'], input: ['sr-only'], label: ['pe-0 top-1/2 left-1/2 -translate-x-1/2 translate-y-1/2 group-data-[filled-within=true]:scale-100 cursor-pointer'] }}
         onChange={handleFileChange}
+        label={<IconPhoto size={18} color='#1d9bf0' />}
+        {...rest}
       />
     </>
 
