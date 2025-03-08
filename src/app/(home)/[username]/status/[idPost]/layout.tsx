57a4131758f4bf721881e0e322fab74dbda8232e
Crisops <crialeperez1835@gmail.com>
2025-03-08 14:56:57 -0500

    feat: add ButtonModalComposePostServer to create a reply to an existing post

--- src/app/(home)/[username]/status/[idPost]/layout.tsx
+++ src/app/(home)/[username]/status/[idPost]/layout.tsx
@@ -1,13 +1,28 @@
 import { ReactNode } from 'react'
+import { Tables } from '@/types/database.types'
+import ButtonModalComposePostServer from '@/components/shared/ButtonModalComposePostServer'
 
 interface UsernameLayoutProps {
   children: ReactNode
+  params: {
+    idPost: Tables<'tweets'>['id']
+  }
 }
 
-export default async function StatusLayout ({ children }: UsernameLayoutProps) {
+export default async function StatusLayout ({ children, params: { idPost } }: UsernameLayoutProps) {
   return (
     <section className='h-full border-r border-zinc-700'>
       {children}
+      <div className='fixed bottom-20 right-6 min-[500px]:hidden'>
+        <ButtonModalComposePostServer
+          className='min-w-14 h-14 bg-sky-500 text-white p-1 shadow-default/50'
+          sizeModal='full'
+          variant='shadow'
+          placement='top-center'
+          loadingForm='comment-post'
+          idPost={idPost}
+        />
+      </div>
     </section>
   )
 }
