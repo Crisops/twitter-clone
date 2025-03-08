2a2cb1dbb4d9d067a4ee87e4de633185dfb2aafa
Crisops <crialeperez1835@gmail.com>
2025-03-08 15:05:36 -0500

    refactor:  added `className` prop to allow external styling.

--- src/components/Posts/FormPostServer.tsx
+++ src/components/Posts/FormPostServer.tsx
@@ -1,17 +1,19 @@
+import { HTMLProps } from 'react'
 import { Tables } from '@/types/database.types'
 import { getSessionAuth, getUserProfile } from '@/utils/supabase/getUser'
 import FormPostClient from '@/components/Posts/FormPostClient'
 import TweetImageUser from '@/components/Home/TweetImageUser'
 
 interface FormPostServerProps {
+  className: HTMLProps<HTMLElement>['className']
   idPost: Tables<'tweets'>['id']
 }
 
-export default async function FormPostServer ({ idPost }: FormPostServerProps) {
+export default async function FormPostServer ({ idPost, className }: FormPostServerProps) {
   const { id } = await getSessionAuth()
   const { name, username, avatar_url: avatar } = await getUserProfile({ id })
   return (
-    <FormPostClient idSession={id} idTweet={idPost}>
+    <FormPostClient idSession={id} idTweet={idPost} className={className}>
       <TweetImageUser avatar_url={avatar} name={name} username={username} />
     </FormPostClient>
   )
