fa7cbdffe1c4aab5e788aa6d91753e94593e40e0
Crisops <crialeperez1835@gmail.com>
2025-03-08 15:17:58 -0500

    feat: add idPost and loadingForm props to ButtonModalComposePostServer
    - Added `idPost` prop to support tweet identification.
    - Introduced `loadingForm` prop to distinguish between 'create-post' and 'comment-post'.
    - Updated `FormTweetServer` to receive these new props and improved styling.

--- src/components/shared/ButtonModalComposePostServer.tsx
+++ src/components/shared/ButtonModalComposePostServer.tsx
@@ -1,20 +1,23 @@
 import { ButtonVariantProps, ModalVariantProps } from '@heroui/react'
+import { Tables } from '@/types/database.types'
 import { ModalProvider } from '@/context/ModalComposeContext'
 import ButtonModalComposePostClient from '@/components/shared/ButtonModalComposePostClient'
 import FormTweetServer from '@/components/Home/FormTweetServer'
 
 interface ButtonModalComposePostServerProps {
   className: string
+  idPost?: Tables<'tweets'>['id']
   variant: ButtonVariantProps['variant']
   sizeModal: ModalVariantProps['size']
   placement: ModalVariantProps['placement']
+  loadingForm: 'create-post' | 'comment-post'
 }
 
-export default function ButtonModalComposePostServer ({ className, variant, sizeModal, placement }: ButtonModalComposePostServerProps) {
+export default function ButtonModalComposePostServer ({ className, idPost, variant, sizeModal, placement, loadingForm }: ButtonModalComposePostServerProps) {
   return (
     <ModalProvider>
       <ButtonModalComposePostClient className={className} variant={variant} sizeModal={sizeModal} placement={placement}>
-        <FormTweetServer viewModal='modal' />
+        <FormTweetServer className='flex flex-col w-full h-full px-6 pt-2' loadingForm={loadingForm} idPost={idPost} />
       </ButtonModalComposePostClient>
     </ModalProvider>
   )
