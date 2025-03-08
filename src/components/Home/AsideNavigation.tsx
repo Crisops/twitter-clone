f011b297075d1377494862fcb72d3546c1b7e20e
Crisops <crialeperez1835@gmail.com>
2025-03-08 15:09:27 -0500

    refactor: add prop loadingForm='create-post'` indicating which form to load

--- src/components/Home/AsideNavigation.tsx
+++ src/components/Home/AsideNavigation.tsx
@@ -17,7 +17,13 @@ export default function AsideNavigation ({ viewMovil, LinkProfile }: AsideNaviga
         <ul className='flex flex-col justify-start w-full gap-1'>
           <LinksSideNav links={preLoadLinks} viewMovil={viewMovil} LinkProfile={LinkProfile} />
           <li className='hidden min-[500px]:block min-[500px]:mt-2'>
-            <ButtonModalComposePostServer variant='solid' sizeModal='xl' placement='top' className='min-w-fit bg-white text-black py-2 px-3' />
+            <ButtonModalComposePostServer
+              variant='solid'
+              sizeModal='xl'
+              placement='top'
+              className='min-w-fit bg-white text-black py-2 px-3'
+              loadingForm='create-post'
+            />
           </li>
         </ul>
       </nav>
