1cbee403b99382ad3a72b0e1e6e270c45daf3720
Crisops <crialeperez1835@gmail.com>
2025-03-08 14:53:52 -0500

    feat(form): enhance useFormTweet hook with session-based defaults
    - Added `idSession` parameter to `useFormTweet` to associate form data with the current user session.
    - Imported `initialCreateTweetForm` from the configuration to set initial form values.
    - Extended `useForm` hook with `getValues`, `setValue`, and `reset` for better form control.
    - Ensured `defaultValues` include `user_id` derived from `idSession` for improved data consistency.
    
    This update enhances form handling by pre-populating user-specific fields and providing additional form manipulation utilities.

--- src/hooks/useFormTweet.ts
+++ src/hooks/useFormTweet.ts
@@ -1,9 +1,13 @@
 import { useForm } from 'react-hook-form'
 import { type FormCreateTweet } from '@/types/store'
 import { validationRulesTweet } from '@/lib/form-tweet'
+import { initialCreateTweetForm } from '@/config/fields-form'
+import { Tables } from '@/types/database.types'
 
-export const useFormTweet = () => {
-  const { register, handleSubmit, formState: { errors, isSubmitting }, trigger } = useForm<FormCreateTweet>()
+export const useFormTweet = ({ idSession }: {idSession: Tables<'users'>['id']}) => {
+  const { register, handleSubmit, formState: { errors, isSubmitting }, trigger, getValues, setValue, reset } = useForm<FormCreateTweet>({
+    defaultValues: { ...initialCreateTweetForm, user_id: idSession }
+  })
 
   const registerField = (name: keyof FormCreateTweet) => register(name, validationRulesTweet[name])
 
@@ -11,6 +15,9 @@ export const useFormTweet = () => {
     registerField,
     handleSubmit,
     trigger,
+    getValues,
+    setValue,
+    reset,
     errors,
     isSubmitting
   }
