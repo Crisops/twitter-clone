33b0032c7718146a11d6fa1b016add6d41745c7f
Crisops <crialeperez1835@gmail.com>
2025-03-19 13:12:00 -0500

    style: add border x in SectionTwittsMain

--- src/components/Home/SectionTwittsMain.tsx
+++ src/components/Home/SectionTwittsMain.tsx
@@ -7,13 +7,15 @@ import WriteTweet from '@/components/Home/WriteTweet'
 
 export default async function SectionTwittsMain () {
   return (
-    <section className='h-full min-[500px]:border-r min-[500px]:border-zinc-700'>
-      <ChooseViewTwitts />
-      <ChooseViewTweetsMoviel />
-      <WriteTweet />
-      <Suspense fallback={<LoadingSpinner />}>
-        <ViewAllTweets />
-      </Suspense>
+    <section className='h-full min-[500px]:border-x min-[500px]:border-zinc-700'>
+      <div className='flex flex-col w-full h-full'>
+        <ChooseViewTwitts />
+        <ChooseViewTweetsMoviel />
+        <WriteTweet />
+        <Suspense fallback={<LoadingSpinner />}>
+          <ViewAllTweets />
+        </Suspense>
+      </div>
     </section>
   )
 }
