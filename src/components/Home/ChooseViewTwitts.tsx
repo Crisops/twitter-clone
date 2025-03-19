88b674c1c7bd6e6950ab073f0d588e5229060473
Crisops <crialeperez1835@gmail.com>
2025-03-19 13:11:17 -0500

    refactor: change nav component a heroui

--- src/components/Home/ChooseViewTwitts.tsx
+++ src/components/Home/ChooseViewTwitts.tsx
@@ -1,15 +1,26 @@
+import { Navbar, NavbarContent, NavbarItem } from '@heroui/react'
+
 export default function ChooseViewTwitts () {
   return (
-    <div className='hidden min-[500px]:block sticky top-0 w-full h-14 border-b border-zinc-700 z-10'>
-      <div className='flex justify-between items-center h-full [&>div]:flex [&>div]:justify-center [&>div]:items-center transition-all duration-200 backdrop-blur-md bg-black/60'>
-        <div className='relative flex-grow h-full'>
+    <Navbar
+      classNames={
+        {
+          wrapper: 'px-0 gap-0 flex-col h-14 xl:border-l xl:border-zinc-700',
+          base: 'hidden min-[500px]:flex',
+          content: 'w-full gap-0',
+          item: 'flex-grow'
+        }
+      }
+    >
+      <NavbarContent>
+        <NavbarItem className='relative'>
+          <button className='w-full py-3 text-white text-base font-bold transition-colors duration-200 hover:bg-white/10 '>Para ti</button>
           <div className='absolute w-1/4 h-1 left-1/2 bottom-0 -translate-x-1/2 rounded-full bg-sky-500' />
-          <button className='w-full h-full text-white text-base font-bold transition-colors duration-200 hover:bg-white/10 '>Para ti</button>
-        </div>
-        <div className='flex-grow h-full'>
-          <button className='w-full h-full text-zinc-500 text-base font-medium transition-colors duration-200 hover:bg-white/10'>Siguiendo</button>
-        </div>
-      </div>
-    </div>
+        </NavbarItem>
+        <NavbarItem>
+          <button className='w-full py-3 text-zinc-500 text-base font-medium transition-colors duration-200 hover:bg-white/10'>Siguiendo</button>
+        </NavbarItem>
+      </NavbarContent>
+    </Navbar>
   )
 }
