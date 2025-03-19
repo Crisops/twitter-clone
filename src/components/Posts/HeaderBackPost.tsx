5e4f68e05be919f49830b85c7c4b71ba93008ea5
Crisops <crialeperez1835@gmail.com>
2025-03-19 14:12:30 -0500

    refactor: replace HeaderBackPost with Navbar component and update ButtonNavigationBack implementation

--- src/components/Posts/HeaderBackPost.tsx
+++ src/components/Posts/HeaderBackPost.tsx
@@ -1,16 +1,28 @@
+import { Navbar, NavbarContent, NavbarItem } from '@heroui/navbar'
+import { IconArrowNarrowLeft } from '@tabler/icons-react'
 import ButtonNavigationBack from '@/components/shared/ButtonNavigationBack'
 
 export default function HeaderBackPost () {
   return (
-    <div className='sticky top-0 left-0 w-full h-14 px-2 z-50 backdrop-blur-md bg-black/60'>
-      <div className='flex items-center h-full'>
-        <div className='flex items-center w-16 h-full'>
-          <ButtonNavigationBack />
-        </div>
-        <div className='flex-grow flex items-center h-full'>
-          <span className='text-xl font-bold text-gray-100 pb-1'>Post</span>
-        </div>
-      </div>
-    </div>
+    <Navbar
+      classNames={
+      {
+        wrapper: 'px-0 gap-0 flex-col h-14 xl:border-l xl:border-zinc-700',
+        content: 'w-full gap-0'
+      }
+    }
+    >
+      <NavbarContent>
+        <NavbarItem className='pl-2'>
+          <ButtonNavigationBack
+            className='bg-transparent px-1 group'
+            startContent={<IconArrowNarrowLeft size={30} className='p-1 group-hover:bg-white/10 group-hover:rounded-full' />}
+            isIconOnly={false}
+          >
+            <span className='text-xl font-bold text-gray-100 pb-1'>Post</span>
+          </ButtonNavigationBack>
+        </NavbarItem>
+      </NavbarContent>
+    </Navbar>
   )
 }
