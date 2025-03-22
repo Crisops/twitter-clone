5f200f3e6952d12808c2352beec1a1b9e16524b5
Crisops <crialeperez1835@gmail.com>
2025-03-21 19:10:51 -0500

    feat: replace ButtonInteractivity with Link in ViewInteractivity
    - The `ButtonInteractivity` component was removed and replaced with `Link` from `next/link`.
    
    - Improved accessibility and interaction by using `Link` instead of `button`.

--- src/components/Profile/ViewInteractivity.tsx
+++ src/components/Profile/ViewInteractivity.tsx
@@ -3,7 +3,7 @@
 import { ReactNode } from 'react'
 import { usePathname } from 'next/navigation'
 import { Tables } from '@/types/database.types'
-import ButtonInteractivity from '@/components/shared/ButtonInteractivity'
+import Link from 'next/link'
 
 interface ViewInteractivityProps {
   posts: ReactNode
@@ -17,9 +17,15 @@ function ViewInteractivity ({ username, posts, media }: ViewInteractivityProps) 
   return (
     <>
       <div className='w-full border-b border-zinc-700'>
-        <div className='flex [&>div]:flex-grow [&>div]:flex [&>div]:justify-center [&>div]:items-center [&>div>button]:w-full [&>div>button]:h-full'>
-          <ButtonInteractivity href={`/${username}`} text='Posts' />
-          <ButtonInteractivity href={`/${username}/media`} text='Multimedia' />
+        <div className='flex [&>div]:flex-grow [&>div]:flex [&>div]:justify-center [&>div]:items-center'>
+          <div className='relative'>
+            <Link className='w-full h-full py-3 text-center' href={`/${username}`}>Posts</Link>
+            <div className={`absolute w-3/4 h-1 left-1/2 bottom-0 -translate-x-1/2 rounded-full ${pathname === `/${username}` ? 'bg-sky-500' : ''}`} />
+          </div>
+          <div className='relative transition-colors duration-200 ease-in-out hover:bg-white/10'>
+            <Link className='w-full h-full py-3 text-center' href={`/${username}/media`}>Multimedia</Link>
+            <div className={`absolute w-3/4 h-1 left-1/2 bottom-0 -translate-x-1/2 rounded-full ${pathname === `/${username}/media` ? 'bg-sky-500' : ''}`} />
+          </div>
         </div>
       </div>
       {pathname === `/${username}` ? posts : media}
