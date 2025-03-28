02fb3c1c99b497ab264a2dec9a0bb8f7c9d6fa60
Crisops <crialeperez1835@gmail.com>
2025-03-28 17:11:36 -0500

    refactor(ButtonGoogle): replace div and button with reusable Button component

--- src/components/shared/ButtonGoogle.tsx
+++ src/components/shared/ButtonGoogle.tsx
@@ -1,17 +1,17 @@
 'use client'
 
 import { signup } from '@/actions/actions'
+import Button from '@/components/shared/Button'
 import { IconGoogle } from '@/components/Icons'
 
-export const ButtonGoogle = ({ textContent }: {textContent: string}) => {
+export const ButtonGoogle = () => {
   const handleSignUpGoogleAuth = async () => {
     await signup()
   }
 
   return (
-    <div className='relative bg-white flex items-center justify-center rounded-full gap-4'>
-      <IconGoogle />
-      <button onClick={handleSignUpGoogleAuth} className='text-sm text-zinc-700 py-2 pr-2 font-medium'>{textContent}</button>
-    </div>
+    <Button onPress={handleSignUpGoogleAuth} startContent={<IconGoogle />} className='text-zinc-700 bg-white w-full h-9 font-medium' variant='solid' radius='full'>
+      Registrarse con Google
+    </Button>
   )
 }
