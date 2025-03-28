e3d4ed3ef192a7c22f1ef558591ac216a9cddde7
Crisops <crialeperez1835@gmail.com>
2025-03-28 17:26:20 -0500

    refactor(DataContainerForm): simplify structure and replace native button with shared Button

--- src/components/Login/DataContainerForm.tsx
+++ src/components/Login/DataContainerForm.tsx
@@ -2,6 +2,7 @@ import { useId } from 'react'
 import { ButtonGoogle } from '@/components/shared/ButtonGoogle'
 import { InputForm } from '@/components/shared/InputForm'
 import Link from 'next/link'
+import Button from '@/components/shared/Button'
 
 interface DataContainerFormProps {
     handleNextConfirmData: () => void
@@ -17,23 +18,23 @@ export const DataContainerForm = ({ handleNextConfirmData }: DataContainerFormPr
           <h1 className='text-3xl text-white font-bold'>Inicia sesión en X</h1>
         </div>
         <form className='flex flex-col w-full'>
-          <ButtonGoogle textContent='Iniciar sesión con Google' />
+          <ButtonGoogle />
+          <div className='flex items-center gap-3 my-3'>
+            <div className='w-full h-px bg-zinc-700' />
+            <div className='text-white text-xs'>O</div>
+            <div className='w-full h-px bg-zinc-700' />
+          </div>
+          <InputForm
+            id={idEmail}
+            type='email'
+            registerName='email'
+            label='Correo electrónico'
+          />
         </form>
-        <div className='flex items-center gap-3 my-3'>
-          <div className='w-full h-px bg-zinc-700' />
-          <div className='text-white text-xs'>O</div>
-          <div className='w-full h-px bg-zinc-700' />
-        </div>
-        <InputForm
-          id={idEmail}
-          type='email'
-          registerName='email'
-          label='Correo electrónico'
-        />
-        <div className='mb-6'>
-          <button onClick={handleNextConfirmData} className=' text-black text-center w-full p-2 font-medium text-base rounded-full bg-white'>Siguiente</button>
-        </div>
-        <div>
+        <Button onPress={handleNextConfirmData} className='bg-white text-zinc-700 w-full font-medium' variant='solid' radius='full' size='md'>
+          Siguiente
+        </Button>
+        <div className='mt-6'>
           <p className='text-base text-zinc-500'>¿No tienes una cuenta? <Link href='/signup' className='text-sky-500 hover:underline'>Regístrate</Link></p>
         </div>
       </div>
