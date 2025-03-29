5e6f1d291a3c97319452fde399506b444f4d1f37
Crisops <crialeperez1835@gmail.com>
2025-03-28 20:31:45 -0500

    refactor: Improved login form handling.
    - Migrated from `useAuth` to `useFormAuth` for better validation handling.
    - Removed `useId` in inputs.
    - Replaced `InputForm` by `Input` with better style customization.
    - Implemented `trigger` in `onChange` for dynamic validation.
    - Improved password visibility toggle with `endContent` in `Input`.
    - Removed direct global state mutation on submit.”

--- src/components/Login/FormLogin.tsx
+++ src/components/Login/FormLogin.tsx
@@ -1,26 +1,30 @@
-import { FormEvent, useId, useState } from 'react'
-import { useAuth } from '@/hooks/useStore'
-import { initialFormAuth } from '@/config/fields-form'
 import { IconEye, IconEyeOff } from '@tabler/icons-react'
-import { InputForm } from '@/components/shared/InputForm'
 import Link from 'next/link'
+import Input from '@/components/shared/Input'
+import { useFormAuth } from '@/hooks/useFormAuth'
+import { type FormLogin as FormLoginType } from '@/types/store'
+import { ChangeEvent, useState } from 'react'
+import Button from '../shared/Button'
 
 export const FormLogin = () => {
-  const idEmail = useId()
-  const idPassword = useId()
-  const [view, setView] = useState<boolean>(false)
+  const { registerField, errors, trigger, handleSubmit } = useFormAuth<FormLoginType>()
+  const [viewPassword, setViewPassword] = useState<boolean>(false)
 
-  const { initialForm, setFormAuth } = useAuth(state => state)
+  const { onChange, ...rest } = registerField('password')
 
   const handleViewPassword = () => {
-    setView(!view)
+    setViewPassword(prev => !prev)
   }
 
-  const handleOnSubmit = (e: FormEvent<HTMLFormElement>) => {
-    e.preventDefault()
-    console.log(initialForm)
-    setFormAuth(initialFormAuth)
+  const handleOnChange = (e: ChangeEvent<HTMLInputElement>) => {
+    onChange(e)
+    trigger('password')
   }
+
+  const handleOnSubmit = handleSubmit(data => {
+    console.log('Formulario enviado')
+    console.log(data)
+  })
 
   return (
     <div className='flex items-center flex-col w-full'>
@@ -28,32 +32,33 @@ export const FormLogin = () => {
         <div className='my-7'>
           <h1 className='text-2xl md:text-3xl text-white font-semibold'>Introduce tu contraseña</h1>
         </div>
-        <form onSubmit={handleOnSubmit} className='flex flex-col w-full h-full'>
-          <InputForm
-            id={idEmail}
-            registerName='email'
+        <form onSubmit={handleOnSubmit} className='flex flex-col gap-y-6 w-full h-full'>
+          <Input
+            isReadOnly
+            classNames={{ inputWrapper: ['bg-[#202327]/50 ring-1 ring-[#202327] data-[hover=true]:bg-[#202327]/50'], label: ['text-zinc-500'] }} radius='none'
             label='Correo electrónico'
-            defaultValue={initialForm.email?.toLocaleLowerCase()}
-            type='email'
-            disabled
+            defaultValue='crialeperez1835@gmail.com'
+            {...registerField('email')}
           />
-          <InputForm
-            id={idPassword}
-            type={view ? 'text' : 'password'}
-            registerName='password'
+          <Input
+            type={viewPassword ? 'text' : 'password'}
+            onChange={handleOnChange}
+            endContent={
+              <Button onPress={handleViewPassword} className='w-fit h-auto bg-transparent data-[pressed=true]:scale-100' isIconOnly>
+                {viewPassword ? <IconEyeOff color='white' size={25} /> : <IconEye color='white' size={25} />}
+              </Button>
+            }
+            isInvalid={!!errors.password}
+            errorMessage={errors.password?.message}
+            classNames={{ inputWrapper: [`bg-transparent ring-1 data-[hover=true]:bg-transparent group-data-[focus=true]:bg-transparent  !bg-danger-transparent data-[hover=true]:!bg-danger-transparent group-data-[focus=true]:!bg-danger-transparent ${errors.password ? 'ring-[#f31260] group-data-[focus=true]:ring-[#f31260]' : 'ring-zinc-500 group-data-[focus=true]:ring-sky-500'}`], label: ['group-data-[filled-within=true]:text-sky-500 '] }} radius='none'
             label='Contraseña'
-            IconSvg={view ? <IconEyeOff color='white' size={25} /> : <IconEye color='white' size={25} />}
-            handleViewPassword={handleViewPassword}
+            {...rest}
           />
-          <div className='flex flex-col mb-10 h-full justify-end'>
-            <div>
-              <button type='submit' className='text-black text-base w-full rounded-full font-bold bg-white p-2 hover:bg-white'>Iniciar sesión</button>
-            </div>
-            <div className='mt-5'>
-              <p className='text-base text-zinc-500'>¿No tienes una cuenta? <Link href='/signup' className='text-sky-500 hover:underline'>Regístrate</Link></p>
-            </div>
-          </div>
+          <Button type='submit' className='bg-white text-black font-medium w-full text-medium' variant='solid' radius='full'>Iniciar sesión</Button>
         </form>
+        <div className='mt-5 w-full'>
+          <p className='text-base text-zinc-500'>¿No tienes una cuenta? <Link href='/signup' className='text-sky-500 hover:underline'>Regístrate</Link></p>
+        </div>
       </div>
     </div>
   )
