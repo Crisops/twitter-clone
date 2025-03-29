8a5a5ee2bda96f9cbcd19be57e355d0d98d6b159
Crisops <crialeperez1835@gmail.com>
2025-03-29 15:16:40 -0500

    refactor: Form Sign up - Implementation of useFormAuth for status handling and validations. - Improved error handling and dynamic styles. - Replaced InputForm with a reusable Input component. - Optimized password visibility handling. - Integration with the signup action for sending data.

--- src/components/SignUp/Form.tsx
+++ src/components/SignUp/Form.tsx
@@ -1,50 +1,73 @@
-import { FormEvent, useId, useState } from 'react'
+import { ChangeEvent, useState } from 'react'
+import { FormSignUp } from '@/types/store'
 import { IconEye, IconEyeOff } from '@tabler/icons-react'
+import { signup } from '@/actions/actions'
 import { useAuth } from '@/hooks/useStore'
-import { InputForm } from '@/components/shared/InputForm'
+import { useFormAuth } from '@/hooks/useFormAuth'
+import { FormAuth } from '@/lib/form-auth'
+import Input from '@/components/shared/Input'
+import Button from '@/components/shared/Button'
 
 export const Form = () => {
-  const idUserName = useId()
-  const idPassword = useId()
   const { initialForm } = useAuth(state => state)
+  const { registerField, errors, trigger, handleSubmit } = useFormAuth<FormSignUp>({ initialForm })
+  const [viewPassword, setViewPassword] = useState<boolean>(false)
 
-  const [view, setView] = useState<boolean>(false)
+  const { onChange: onChangeUsername, ...restUsername } = registerField('username')
+  const { onChange: onChangePassword, ...restPassword } = registerField('password')
 
   const handleViewPassword = () => {
-    setView(!view)
+    setViewPassword(prev => !prev)
   }
 
-  const handleOnSubmit = (e: FormEvent<HTMLFormElement>) => {
-    e.preventDefault()
-    console.log(initialForm)
+  const handleOnChange = (e: ChangeEvent<HTMLInputElement>) => {
+    onChangePassword(e)
+    onChangeUsername(e)
+    trigger(e.target.name as keyof FormAuth)
   }
+
+  const handleOnSubmit = handleSubmit(async (data) => {
+    console.log(data)
+    await signup({ provider: 'email', data })
+  })
 
   return (
-    <div className='flex items-center flex-col w-full h-[calc(100%-4rem)]'>
-      <div className='flex flex-col max-w-72 sm:max-w-md w-full h-full'>
+    <div className='flex items-center flex-col w-full'>
+      <div className='flex flex-col max-w-72 sm:max-w-md lg:max-w-lg w-full h-full'>
         <div className='my-7'>
-          <h1 className='text-3xl text-white font-semibold'>Crea tu cuenta</h1>
+          <h1 className='text-2xl md:text-3xl text-white font-semibold'>Crea tu cuenta</h1>
         </div>
-        <form onSubmit={handleOnSubmit} className='flex flex-col w-full h-full'>
-          <InputForm
-            id={idUserName}
+        <form onSubmit={handleOnSubmit} className='flex flex-col gap-y-6 w-full h-full'>
+          <Input
+            isReadOnly
+            classNames={{ inputWrapper: ['bg-[#202327]/50 ring-1 ring-[#202327] data-[hover=true]:bg-[#202327]/50'], label: ['text-zinc-500'] }} radius='none'
+            label='Correo electrónico'
+            defaultValue={initialForm.email}
+            {...registerField('email')}
+          />
+          <Input
+            onChange={handleOnChange}
+            isInvalid={!!errors.username}
+            errorMessage={errors.username?.message}
+            classNames={{ inputWrapper: [`bg-transparent ring-1 data-[hover=true]:bg-transparent group-data-[focus=true]:bg-transparent  !bg-danger-transparent data-[hover=true]:!bg-danger-transparent group-data-[focus=true]:!bg-danger-transparent ${errors.username ? 'ring-[#f31260] group-data-[focus=true]:ring-[#f31260]' : 'ring-zinc-500 group-data-[focus=true]:ring-sky-500'}`], label: ['group-data-[filled-within=true]:text-sky-500 '] }} radius='none'
             label='Nombre de usuario'
-            type='text'
-            registerName='username'
+            {...restUsername}
           />
-          <InputForm
-            id={idPassword}
-            registerName='password'
-            type={view ? 'text' : 'password'}
+          <Input
+            type={viewPassword ? 'text' : 'password'}
+            onChange={handleOnChange}
+            endContent={
+              <Button onPress={handleViewPassword} className='w-fit h-auto bg-transparent data-[pressed=true]:scale-100' isIconOnly>
+                {viewPassword ? <IconEyeOff color='white' size={25} /> : <IconEye color='white' size={25} />}
+              </Button>
+            }
+            isInvalid={!!errors.password}
+            errorMessage={errors.password?.message}
+            classNames={{ inputWrapper: [`bg-transparent ring-1 data-[hover=true]:bg-transparent group-data-[focus=true]:bg-transparent  !bg-danger-transparent data-[hover=true]:!bg-danger-transparent group-data-[focus=true]:!bg-danger-transparent ${errors.password ? 'ring-[#f31260] group-data-[focus=true]:ring-[#f31260]' : 'ring-zinc-500 group-data-[focus=true]:ring-sky-500'}`], label: ['group-data-[filled-within=true]:text-sky-500 '] }} radius='none'
             label='Contraseña'
-            IconSvg={view ? <IconEyeOff color='white' size={25} /> : <IconEye color='white' size={25} />}
-            handleViewPassword={handleViewPassword}
+            {...restPassword}
           />
-          <div className='flex mb-10 h-full'>
-            <div className='flex-grow self-end'>
-              <button type='submit' className='text-black text-lg w-full rounded-full font-medium bg-white p-3 hover:bg-white/90'>Crear cuenta</button>
-            </div>
-          </div>
+          <Button type='submit' className='bg-white text-black font-medium w-full text-medium h-12' variant='solid' radius='full'>Crear cuenta</Button>
         </form>
       </div>
     </div>
