7be9af01d18a5c1b94c50dda764fd84be6fd12cb
Crisops <crialeperez1835@gmail.com>
2025-03-28 20:30:41 -0500

    refactor(DataContainerForm): integrate useFormAuth for better validation
    - Replaced useId with useFormAuth for better form handling and validation.
    - Implemented handleSubmit from useFormAuth to manage form submission.
    - Used trigger to validate the email field on change.
    - Updated InputForm to Input and passed validation-related props (isInvalid, errorMessage).
    - Improved class management for better UI feedback on validation errors.
    - Wrapped the form submission logic inside handleOnSubmit.

--- src/components/Login/DataContainerForm.tsx
+++ src/components/Login/DataContainerForm.tsx
@@ -1,15 +1,30 @@
-import { useId } from 'react'
-import { ButtonGoogle } from '@/components/shared/ButtonGoogle'
-import { InputForm } from '@/components/shared/InputForm'
-import Link from 'next/link'
+'use client'
+
+import { ChangeEvent } from 'react'
+import { FormLogin } from '@/types/store'
+import { useFormAuth } from '@/hooks/useFormAuth'
 import Button from '@/components/shared/Button'
+import { ButtonGoogle } from '@/components/shared/ButtonGoogle'
+import Input from '@/components/shared/Input'
+import Link from 'next/link'
 
 interface DataContainerFormProps {
     handleNextConfirmData: () => void
 }
 
 export const DataContainerForm = ({ handleNextConfirmData }: DataContainerFormProps) => {
-  const idEmail = useId()
+  const { registerField, errors, trigger, handleSubmit } = useFormAuth<FormLogin>()
+
+  const { onChange, ...rest } = registerField('email')
+
+  const handleOnChange = (e: ChangeEvent<HTMLInputElement>) => {
+    onChange(e)
+    trigger('email')
+  }
+
+  const handleOnSubmit = handleSubmit(data => {
+    console.log(data)
+  })
 
   return (
     <div className='flex items-center flex-col w-full'>
@@ -17,26 +32,28 @@ export const DataContainerForm = ({ handleNextConfirmData }: DataContainerFormPr
         <div className='my-6'>
           <h1 className='text-3xl text-white font-bold'>Inicia sesión en X</h1>
         </div>
-        <form className='flex flex-col w-full'>
+        <form onSubmit={handleOnSubmit} className='flex flex-col w-full mb-6 gap-3'>
           <ButtonGoogle />
-          <div className='flex items-center gap-3 my-3'>
+          <div className='flex items-center gap-3'>
             <div className='w-full h-px bg-zinc-700' />
             <div className='text-white text-xs'>O</div>
             <div className='w-full h-px bg-zinc-700' />
           </div>
-          <InputForm
-            id={idEmail}
+          <Input
             type='email'
-            registerName='email'
+            autoComplete='email'
+            onChange={handleOnChange}
+            isInvalid={!!errors.email}
+            errorMessage={errors.email?.message}
+            classNames={{ inputWrapper: [`bg-transparent ring-1 data-[hover=true]:bg-transparent group-data-[focus=true]:bg-transparent !bg-danger-transparent data-[hover=true]:!bg-danger-transparent group-data-[focus=true]:!bg-danger-transparent ${errors.email ? 'ring-[#f31260] group-data-[focus=true]:ring-[#f31260]' : 'ring-zinc-500 group-data-[focus=true]:ring-sky-500'}`], label: ['group-data-[filled-within=true]:text-sky-500 '] }} radius='none'
             label='Correo electrónico'
+            {...rest}
           />
+          <Button type='submit' className='bg-white text-zinc-700 w-full font-medium' variant='solid' radius='full' size='md'>
+            Siguiente
+          </Button>
         </form>
-        <Button onPress={handleNextConfirmData} className='bg-white text-zinc-700 w-full font-medium' variant='solid' radius='full' size='md'>
-          Siguiente
-        </Button>
-        <div className='mt-6'>
-          <p className='text-base text-zinc-500'>¿No tienes una cuenta? <Link href='/signup' className='text-sky-500 hover:underline'>Regístrate</Link></p>
-        </div>
+        <p className='text-base text-zinc-500'>¿No tienes una cuenta? <Link href='/signup' className='text-sky-500 hover:underline'>Regístrate</Link></p>
       </div>
     </div>
   )
