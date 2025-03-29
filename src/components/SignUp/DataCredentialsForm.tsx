83dd5d4893698e955d765b7f410cb5ddf7e9e856
Crisops <crialeperez1835@gmail.com>
2025-03-29 15:17:58 -0500

    refactor: DataCredentialsForm
    - Integration of useAuth and useFormAuth for status management and validations.
    - Migration of InputForm to a new Input component with dynamic error handling.
    - Implementation of a centralized handler for updating values and validations.
    - Replacement of the standard button by the reusable Button component.
    - Conversion of the form to a <form> with onSubmit handled by useFormAuth.”

--- src/components/SignUp/DataCredentialsForm.tsx
+++ src/components/SignUp/DataCredentialsForm.tsx
@@ -1,6 +1,10 @@
-import { useId } from 'react'
+import { ChangeEvent } from 'react'
+import { FormSignUp } from '@/types/store'
+import { useAuth } from '@/hooks/useStore'
+import { useFormAuth } from '@/hooks/useFormAuth'
 import { months, days, years } from '@/lib/data-date'
-import { InputForm } from '@/components/shared/InputForm'
+import Button from '@/components/shared/Button'
+import Input from '@/components/shared/Input'
 import { SelectedBirthday } from '@/components/SignUp/SelectedBirthDay'
 
 interface DataCredentialsFormProps {
@@ -8,8 +12,23 @@ interface DataCredentialsFormProps {
 }
 
 export const DataCredentialsForm = ({ handleNextData }: DataCredentialsFormProps) => {
-  const idNameUser = useId()
-  const idEmailUser = useId()
+  const { initialForm, setFormAuth } = useAuth(state => state)
+  const { registerField, trigger, errors, handleSubmit } = useFormAuth<FormSignUp>({ initialForm })
+
+  const { onChange: onChangeFullName, ...restFullName } = registerField('fullName')
+  const { onChange: onChangeEmail, ...restEmail } = registerField('email')
+
+  const handleOnChange = (e: ChangeEvent<HTMLInputElement | HTMLSelectElement>) => {
+    onChangeFullName(e)
+    onChangeEmail(e)
+    trigger(e.target.name as keyof FormSignUp)
+  }
+
+  const handleOnSubmit = handleSubmit(data => {
+    console.log(data)
+    setFormAuth(data)
+    handleNextData()
+  })
 
   return (
     <div className='flex items-center flex-col w-full h-[calc(100%-3.5rem)]'>
@@ -17,34 +36,34 @@ export const DataCredentialsForm = ({ handleNextData }: DataCredentialsFormProps
         <div className='my-7'>
           <h1 className='text-3xl text-white font-semibold'>Crea tu cuenta</h1>
         </div>
-        <section className='flex flex-col w-full h-full'>
-          <InputForm
-            id={idNameUser}
+        <form onSubmit={handleOnSubmit} className='flex flex-col w-full h-full gap-6'>
+          <Input
+            onChange={handleOnChange}
+            isInvalid={!!errors.fullName}
+            errorMessage={errors.fullName?.message}
+            classNames={{ inputWrapper: [`bg-transparent ring-1 data-[hover=true]:bg-transparent group-data-[focus=true]:bg-transparent !bg-danger-transparent data-[hover=true]:!bg-danger-transparent group-data-[focus=true]:!bg-danger-transparent ${errors.fullName ? 'ring-[#f31260] group-data-[focus=true]:ring-[#f31260]' : 'ring-zinc-500 group-data-[focus=true]:ring-sky-500'}`], label: ['group-data-[filled-within=true]:text-sky-500 '] }} radius='none'
             label='Nombre'
-            type='text'
-            registerName='fullName'
+            {...restFullName}
           />
-          <InputForm
-            id={idEmailUser}
-            registerName='email'
+          <Input
+            onChange={handleOnChange}
+            isInvalid={!!errors.email}
+            errorMessage={errors.email?.message}
+            classNames={{ inputWrapper: [`bg-transparent ring-1 data-[hover=true]:bg-transparent group-data-[focus=true]:bg-transparent !bg-danger-transparent data-[hover=true]:!bg-danger-transparent group-data-[focus=true]:!bg-danger-transparent ${errors.email ? 'ring-[#f31260] group-data-[focus=true]:ring-[#f31260]' : 'ring-zinc-500 group-data-[focus=true]:ring-sky-500'}`], label: ['group-data-[filled-within=true]:text-sky-500 '] }} radius='none'
             label='Correo electrónico'
-            type='email'
+            {...restEmail}
           />
           <div>
             <h3 className='text-white text-base font-medium'>Fecha de nacimiento</h3>
             <p className='text-zinc-500 text-sm'>Esta información no será pública. Confirma tu propia edad, incluso si esta cuenta es para una empresa, una mascota u otra cosa.</p>
           </div>
           <div className='flex gap-4 mt-5'>
-            <SelectedBirthday label='Mes' name='month' date={months} />
-            <SelectedBirthday label='Día' name='day' date={days} />
-            <SelectedBirthday label='Año' name='year' date={years} />
+            <SelectedBirthday label='Mes' date={months} handleOnChange={handleOnChange} registerField={registerField('birthday.month')} />
+            <SelectedBirthday label='Día' date={days} handleOnChange={handleOnChange} registerField={registerField('birthday.day')} />
+            <SelectedBirthday label='Año' date={years} handleOnChange={handleOnChange} registerField={registerField('birthday.year')} />
           </div>
-        </section>
-        <div className='mb-10'>
-          <div>
-            <button onClick={handleNextData} type='button' className='text-black bg-white text-lg w-full rounded-full font-medium p-3'>Siguiente</button>
-          </div>
-        </div>
+          <Button type='submit' className='bg-white font-medium w-full text-black h-12' variant='solid' radius='full'>Siguiente</Button>
+        </form>
       </div>
     </div>
   )
