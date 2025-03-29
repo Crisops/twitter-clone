723bbf383a5e9226b4acdf20b6cbdde27163bb1c
Crisops <crialeperez1835@gmail.com>
2025-03-29 15:09:42 -0500

    refactor(hook): add support for initial form values in useFormAuth
    - Introduced `initialForm` prop to `useFormAuth` for setting default form values.
    - Updated `useForm` hook to use `defaultValues` from the provided `initialForm`.
    - Ensured backward compatibility while improving flexibility.

--- src/hooks/useFormAuth.ts
+++ src/hooks/useFormAuth.ts
@@ -1,8 +1,12 @@
-import { Path, useForm } from 'react-hook-form'
+import { DefaultValues, Path, useForm } from 'react-hook-form'
 import { validationRules } from '@/lib/form-auth'
 
-export const useFormAuth = <T extends Partial<Record<keyof typeof validationRules, any>>>() => {
-  const { register, handleSubmit, trigger, formState: { errors } } = useForm<T>()
+interface useFormAuthProps<T> {
+  initialForm: DefaultValues<T>
+}
+
+export const useFormAuth = <T extends Partial<Record<keyof typeof validationRules, any>>>({ initialForm }: useFormAuthProps<T>) => {
+  const { register, handleSubmit, trigger, formState: { errors } } = useForm<T>({ defaultValues: initialForm })
 
   const registerField = (name: Path<T>) => {
     const rules = validationRules as Partial<Record<Path<T>, any>>
