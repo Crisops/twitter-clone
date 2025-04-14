c70ba1ea902e480aa8a0ff5d519449a5565e066c
Crisops <crialeperez1835@gmail.com>
2025-04-14 10:49:47 -0500

    refactor: simplify AsideBarMenu and AsideNavigation components, improve layout and structure

--- src/components/Home/AsideBarMenu.tsx
+++ src/components/Home/AsideBarMenu.tsx
@@ -1,29 +1,13 @@
-import { IconTwitter } from '@/components/Icons'
-import AsideInformationUserServer from '@/components/Home/AsideInformationUserServer'
 import AsideNavigation from '@/components/Home//AsideNavigation'
 import AsideNavigationDesktop from '@/components/Home/AsideNavigationDesktop'
 
 export default function AsideBarMenu () {
   return (
-    <aside className='hidden min-[500px]:block sticky top-0 h-screen'>
-      <div className='h-full px-1'>
-        <div className='flex flex-col justify-between h-full'>
-          <div className='flex flex-col w-full h-full'>
-            <header className='flex justify-end xl:block p-3'>
-              <IconTwitter size='size-7' />
-            </header>
-            <div className='h-full'>
-              <div className='min-[500px]:hidden h-full'>
-                <AsideNavigation />
-              </div>
-              <div className='hidden min-[500px]:block h-full'>
-                <AsideNavigationDesktop />
-              </div>
-            </div>
-          </div>
-          <AsideInformationUserServer />
-        </div>
+    <aside className='hidden min-[500px]:block top-0 h-full'>
+      <div className='relative min-[500px]:hidden h-full'>
+        <AsideNavigation />
       </div>
+      <AsideNavigationDesktop />
     </aside>
   )
 }
--- src/components/Home/AsideNavigation.tsx
+++ src/components/Home/AsideNavigation.tsx
@@ -1,7 +1,9 @@
 import { ReactNode } from 'react'
-import { IconFeather } from '@/components/Icons'
+import { IconFeather, IconTwitter } from '@/components/Icons'
 import { linksDesktop, linksMovileDrawer } from '@/lib/links-navbar'
+import AsideInformationUserServer from '@/components/Home/AsideInformationUserServer'
 import ButtonModalComposePostServer from '@/components/shared/ButtonModalComposePostServer'
+import { Navbar, NavbarContent, NavbarItem } from '@heroui/navbar'
 import LinksSideNav from '@/components/Home/LinksSideNav'
 
 interface AsideNavigationProps {
@@ -13,24 +15,32 @@ export default function AsideNavigation ({ viewMovil, LinkProfile }: AsideNaviga
   const preLoadLinks = viewMovil ? linksMovileDrawer : linksDesktop
 
   return (
-    <>
-      <nav className='flex'>
-        <ul className='flex flex-col justify-start w-full gap-1'>
-          <LinksSideNav links={preLoadLinks} viewMovil={viewMovil} LinkProfile={LinkProfile} />
-          <li className='hidden min-[500px]:block min-[500px]:mt-2'>
-            <ButtonModalComposePostServer
-              variant='solid'
-              sizeModal='xl'
-              placement='top'
-              className='min-w-fit bg-white text-black py-2 px-3 font-semibold text-medium xl:w-4/5'
-              loadingForm='create-post'
-            >
-              <span className='block xl:hidden'><IconFeather className='fill-current size-6' /></span>
-              <span className='hidden xl:block text-current'>Postear</span>
-            </ButtonModalComposePostServer>
-          </li>
-        </ul>
-      </nav>
-    </>
+    <Navbar classNames={{ base: ['items-start [&>header]:px-3 [&>header]:h-full h-screen'], content: ['flex flex-col h-full items-start'] }}>
+      <NavbarContent justify='start'>
+        <NavbarItem className='p-3'>
+          <IconTwitter size='size-7' />
+        </NavbarItem>
+        {preLoadLinks.map(({ href, icon, text }, index) => (
+          <NavbarItem key={index}>
+            <LinksSideNav viewMovil={viewMovil} href={href} icon={icon} text={text} />
+          </NavbarItem>
+        ))}
+        <NavbarItem className='w-full'>
+          <ButtonModalComposePostServer
+            variant='solid'
+            sizeModal='xl'
+            placement='top'
+            className='w-full bg-white text-black py-2 px-3 font-semibold text-medium xl:w-4/5'
+            loadingForm='create-post'
+          >
+            <span className='block xl:hidden'><IconFeather className='fill-current size-6' /></span>
+            <span className='hidden xl:block text-current'>Postear</span>
+          </ButtonModalComposePostServer>
+        </NavbarItem>
+        <NavbarItem className='grow flex w-full h-full items-end'>
+          <AsideInformationUserServer />
+        </NavbarItem>
+      </NavbarContent>
+    </Navbar>
   )
 }
