acf94cb39722981a05c12ea55fcf87b2815154a9
Crisops <crialeperez1835@gmail.com>
2025-04-26 19:05:30 -0500

    feat: enhance bookmarks page with UI updates and add NotBookmarks component

--- src/app/(home)/bookmarks/page.tsx
+++ src/app/(home)/bookmarks/page.tsx
@@ -1,5 +1,23 @@
-export default function BookmarksPage () {
+import { getBookMarks } from '@/utils/supabase/getInterations'
+import { getSessionAuth } from '@/utils/supabase/getUser'
+import HeaderBackPost from '@/components/Posts/HeaderBackPost'
+import NotBookmarks from '@/components/bookmarks/not-bookmarks'
+import Tweet from '@/components/Home/Tweet'
+
+export default async function BookmarksPage () {
+  const { id } = await getSessionAuth()
+  const bookmarks = await getBookMarks({ userId: id })
+
   return (
-    <div>BookmarksPage</div>
+    <div className='w-full h-full border-x border-zinc-700'>
+      <HeaderBackPost title='Guardados' />
+      <section className='w-full h-full flex flex-col'>
+        {
+          bookmarks.length === 0
+            ? <div className='w-full h-full flex justify-center text-zinc-600'><NotBookmarks /></div>
+            : bookmarks.map(post => (<Tweet key={post?.tweet?.id} idUser={post?.tweet?.creator?.id} tweet={post?.tweet} />))
+        }
+      </section>
+    </div>
   )
 }
