7d82a125b62ff5a88daa9c5891db5bf936d8981a
Crisops <crialeperez1835@gmail.com>
2025-05-06 19:18:11 -0500

    refactor: remove unused SITE_URL declaration from FormComposePost and add it to Form component

--- src/components/Login/Form.tsx
+++ src/components/Login/Form.tsx
@@ -3,9 +3,14 @@
 import { useState } from 'react'
 import { DataContainerForm } from '@/components/Login/DataContainerForm'
 import { FormLogin } from '@/components/Login/FormLogin'
+import { EnvConfig } from '@/config/env.config'
 
 export const Form = () => {
   const [nextData, setNextData] = useState<boolean>(false)
+
+  const SITE_URL = EnvConfig().NEXT_PUBLIC_SITE_URL ?? 'http://localhost:3000'
+
+  console.log(SITE_URL)
 
   const handleNextConfirmData = () => {
     setNextData(true)
