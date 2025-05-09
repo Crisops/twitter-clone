a119808814edda584cbf59ae7ba73850f23ede13
Crisops <crialeperez1835@gmail.com>
2025-05-08 19:10:53 -0500

    refactor: implement logout confirmation modal in SignOutUser component and improve button accessibility

--- src/components/Home/SignOutUser.tsx
+++ src/components/Home/SignOutUser.tsx
@@ -1,21 +1,44 @@
 'use client'
 
+import { useState } from 'react'
+import { Button } from '@heroui/react'
+import ShowModal from '@/components/shared/ShowModal'
+import ValidateSignOut from '@/components/Home/validate-sign-out'
 import { signOut } from '@/actions/actions'
-
 export default function SignOutUser ({ username }: {username: string}) {
-  const handleSignOut = async () => {
+  const [isModalOpen, setIsModalOpen] = useState<boolean>(false)
+
+  const handleOpenModal = (): void => {
+    setIsModalOpen(true)
+  }
+
+  const handleSignOut = async (): Promise<void> => {
     await signOut()
   }
 
+  const handleCloseModal = (): void => {
+    setIsModalOpen(false)
+  }
+
   return (
-    <div className='relative top-0 left-0 w-72 h-24 flex items-end py-2'>
-      <div className='hover:bg-slate-600/10 cursor-pointer flex-grow'>
-        <div onClick={handleSignOut} role='button' className='py-3 px-4'>
-          <div className='max-w-36'>
-            <span className='text-white text-base font-medium'>Cerrar la sesión de @{username}</span>
-          </div>
-        </div>
-      </div>
+    <div className='w-72 h-24 py-2'>
+      <Button
+        onPress={handleOpenModal}
+        className='w-full h-full data-[hover=true]:bg-slate-600/10 rounded-none'
+        variant='light'
+      >
+        <span className='text-white text-medium font-medium'>
+          Cerrar la sesión de @{username}
+        </span>
+      </Button>
+      <ShowModal
+        isOpen={isModalOpen}
+        placement='top-center'
+        size='xs'
+        hideCloseButton
+        handleClose={handleCloseModal}
+        content={<ValidateSignOut handleSignOut={handleSignOut} handleCloseModal={handleCloseModal} />}
+      />
     </div>
   )
 }
